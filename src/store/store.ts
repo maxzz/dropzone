47369937c44884031f4bf5bf3565351e6f611e5b
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-09 17:30:27 -0700

    WIP. load cache. files as array of atoms failed to update atom

--- src/store/store.ts
+++ src/store/store.ts
@@ -36,3 +36,56 @@ export type FileCache = {
 export type FileUsAtom = Atom<FileUs>;
 
 export const filesAtom = atom<FileUsAtom[]>([]);
+
+function textFileReader(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        const aborted = () => reject(`file (${file.name}) reading was aborted`);
+        reader.onabort = aborted;
+        reader.onerror = aborted;
+        reader.onload = () => resolve(reader.result?.toString() || '');
+        reader.readAsText(file);
+    });
+}
+
+const updateCacheAtom = atom(
+    null,
+    async (get, set) => {
+        const files = get(filesAtom);
+
+        for (let fileAtom of files) {
+            try {
+                const file = get(fileAtom);
+
+                if (file.file && !file.cnt) {
+                    const cnt = await textFileReader(file.file);
+                    const newAtom = {
+                        ...file,
+                        cnt,
+                    }
+                    set(fileAtom, atom(newAtom));
+                }
+
+                file.file && cache.push(
+                    { id: file.id, cnt: await textFileReader(file.file), }
+                );
+            } catch (error) {
+                console.log('error', error);
+            }
+        }
+
+
+    }
+);
+
+async function laodCache(acceptedFiles: FileUs[]) {
+    const cache: FileCache[] = [];
+    for (let file of acceptedFiles) {
+        try {
+            file.file && cache.push({ id: file.id, cnt: await textFileReader(file.file), });
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+    return cache;
+}
