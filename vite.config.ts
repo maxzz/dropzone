2b7119813fddd9370bee037f88a9bdda92d902a4
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-10 16:05:58 -0700

    Add visualization.html and inline images during production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { visualizer } from 'rollup-plugin-visualizer';
+import url from '@rollup/plugin-url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()]
+    plugins: [
+        reactRefresh(),
+
+        {
+            ...url(
+                    {
+                    include: ['**/*.svg'],
+                    limit: 15000,
+                }
+            ),
+            enforce: 'pre',
+        },
+        visualizer({
+            filename: 'visualization.html',
+            template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
+            gzipSize: true,
+            brotliSize: true,
+        }),
+
+    ]
 });
