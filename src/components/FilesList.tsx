56ac602246fbf3026158a94c529c0eda44b1f458
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-12 17:03:28 -0700

    WIP. login card fields. add show table fields recursively

--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -99,15 +99,22 @@ function isObject(value: any): boolean {
     return value && typeof value === 'object';
 }
 
-function ObjectTable({ obj = {} }: { obj?: any; }) {
+function ObjectTable({ obj = {} }: { obj?: any; }): JSX.Element {
     const values = Object.entries(obj);
     return (
         <div className="grid grid-cols-[auto,1fr] gap-x-1 text-xs">
             {values.map((pair) => {
-                return (<React.Fragment key={pair[0]}>
-                    <div className="">{pair[0]}</div>
-                    <div className="border-l border-gray-500 pl-1 smallscroll overflow-x-auto whitespace-nowrap overflow-ellipsis">{`${pair[1]}`}</div>
-                </React.Fragment>)
+                if (isObject(pair[1])) {
+                    return (<React.Fragment key={pair[0]}>
+                        <div className="">field {pair[0]}</div>
+                        {ObjectTable({obj: pair[1]})}
+                    </React.Fragment>);
+                } else {
+                    return (<React.Fragment key={pair[0]}>
+                        <div className="">{pair[0]}</div>
+                        <div className="border-l border-gray-500 pl-1 smallscroll overflow-x-auto whitespace-nowrap overflow-ellipsis">{`${pair[1]}`}</div>
+                    </React.Fragment>);
+                }
             })}
         </div>
     );
