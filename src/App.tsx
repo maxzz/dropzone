74ed574977a876854a0c091b4362ee8620dfff25
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-14 02:26:59 -0700

    WIP. Filters. remove old buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,6 @@ import Toaster from './components/Toaster';
 import FilesList from './components/FilesList';
 import LabeledSwitch from './components/Switch';
 
-function Button({children, ...rest}: {children?: React.ReactNode, rest?: React.HtmlHTMLAttributes<HTMLElement>}) {
-    return (
-        <button className="h-8 p-2 flex items-center rounded border shadow-inner"
-        style={{
-            boxShadow: '#0000005e 1px 1px 4px 0px inset, #bcbcbc99 -1px -1px 2px 0px inset',
-            background: '#b0b0b026',
-        }}
-        >
-            {children}
-        </button>
-    );
-}
-
 function App() {
     return (
         <React.Fragment>
@@ -26,16 +13,8 @@ function App() {
                 <header className="pb-0">
                     <DropzoneArea>
                         <div className="mr-4 flex items-center space-x-2">
-                            <Button>Auto</Button>
                             <LabeledSwitch label="Normal" />
                             <LabeledSwitch label="Manual" />
-                            <button className="h-8 p-2 flex items-center rounded border shadow-inner"
-                            style={{
-                                boxShadow: '#0000005e 1px 1px 4px 0px inset, #bcbcbc99 -1px -1px 2px 0px inset',
-                                background: '#b0b0b026',
-                            }}
-                            >Auto</button>
-                            <button className="h-8 p-2 flex items-center rounded border">Manual</button>
                         </div>
                     </DropzoneArea>
                 </header>
