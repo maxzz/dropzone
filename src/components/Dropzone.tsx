e0782b9409af3c0214fc9757514b4db53443bd8f
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-14 03:07:28 -0700

    WIP. add dropzone re-work. 2

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -16,7 +16,7 @@ function nameLengthValidator(file: File) {
     return null;
 }
 
-export function DropzoneBase({className}: React.HTMLAttributes<HTMLDivElement>) {
+export function DropzoneBase({className, children}: React.HTMLAttributes<HTMLDivElement>) {
     const setFiles = useUpdateAtom(SetFilesAtom);
 
     const onDrop = useCallback((accepterFiles: File[]) => setFiles(accepterFiles), []);
@@ -29,9 +29,7 @@ export function DropzoneBase({className}: React.HTMLAttributes<HTMLDivElement>) 
     return (
         <div {...getRootProps()} className={`${className} ${isDragActive ? 'bg-green-600' : ''}`}>
             <input {...getInputProps()} />
-            <div>
-                <p>Drag 'n' drop files here, or click to select files</p>
-            </div>
+            {children}
         </div>
     );
 }
@@ -48,7 +46,11 @@ export function DropzoneArea({ children }: { children?: React.ReactNode; }) {
 
     return (
         <div className="flex justify-between bg-gray-700 text-gray-100 ring-2 ring-gray-50 rounded-md">
-            <DropzoneBase className="px-4 py-2 my-1 mr-4 rounded bg-gray-900 ml-4" />
+            <DropzoneBase className="px-4 py-2 my-1 mr-4 rounded bg-gray-900 ml-4">
+                <div>
+                    <p>Drag 'n' drop files here, or click to select files</p>
+                </div>
+            </DropzoneBase>
             <div className="flex items-center justify-between">
                 {children}
                 <div className="w-7 h-7 mr-8" onClick={(event) => { event.stopPropagation(); toast('again'); }}>
