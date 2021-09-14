55a46d3749af5515cbf7577b91b692578b74ed9f
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-14 03:06:07 -0700

    WIP. add dropzone re-work

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -16,6 +16,26 @@ function nameLengthValidator(file: File) {
     return null;
 }
 
+export function DropzoneBase({className}: React.HTMLAttributes<HTMLDivElement>) {
+    const setFiles = useUpdateAtom(SetFilesAtom);
+
+    const onDrop = useCallback((accepterFiles: File[]) => setFiles(accepterFiles), []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        validator: nameLengthValidator,
+    });
+
+    return (
+        <div {...getRootProps()} className={`${className} ${isDragActive ? 'bg-green-600' : ''}`}>
+            <input {...getInputProps()} />
+            <div>
+                <p>Drag 'n' drop files here, or click to select files</p>
+            </div>
+        </div>
+    );
+}
+
 export function DropzoneArea({ children }: { children?: React.ReactNode; }) {
     const setFiles = useUpdateAtom(SetFilesAtom);
 
@@ -28,18 +48,7 @@ export function DropzoneArea({ children }: { children?: React.ReactNode; }) {
 
     return (
         <div className="flex justify-between bg-gray-700 text-gray-100 ring-2 ring-gray-50 rounded-md">
-            <div {...getRootProps()} className={`px-4 py-2 my-1 mr-4 rounded bg-gray-900 ml-4 ${isDragActive ? 'bg-green-600' : ''}`}>
-                <input {...getInputProps()} className="" />
-                <div className="">
-                    <p>Drag 'n' drop files here, or click to select files</p>
-                </div>
-                {/* <div className="">
-                    {isDragActive ?
-                        <p>Drop the files here ...</p> :
-                        <p>Drag 'n' drop files here, or click to select files</p>
-                    }
-                </div> */}
-            </div>
+            <DropzoneBase className="px-4 py-2 my-1 mr-4 rounded bg-gray-900 ml-4" />
             <div className="flex items-center justify-between">
                 {children}
                 <div className="w-7 h-7 mr-8" onClick={(event) => { event.stopPropagation(); toast('again'); }}>
