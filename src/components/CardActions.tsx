3c13cc4899349c73316aa2ac5dad7ded1065ae16
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-17 00:07:29 -0700

    WIP. per card actions. remove unused classes

--- src/components/CardActions.tsx
+++ src/components/CardActions.tsx
@@ -9,45 +9,6 @@ import { violet, mauve, blackA } from '@radix-ui/colors';
 // } from '@radix-ui/react-icons';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
-const slideUpAndFade = keyframes({
-    '0%': { opacity: 0, transform: 'translateY(2px)' },
-    '100%': { opacity: 1, transform: 'translateY(0)' },
-});
-
-const slideRightAndFade = keyframes({
-    '0%': { opacity: 0, transform: 'translateX(-2px)' },
-    '100%': { opacity: 1, transform: 'translateX(0)' },
-});
-
-const slideDownAndFade = keyframes({
-    '0%': { opacity: 0, transform: 'translateY(-2px)' },
-    '100%': { opacity: 1, transform: 'translateY(0)' },
-});
-
-const slideLeftAndFade = keyframes({
-    '0%': { opacity: 0, transform: 'translateX(2px)' },
-    '100%': { opacity: 1, transform: 'translateX(0)' },
-});
-
-const StyledContent = styled(DropdownMenuPrimitive.Content, {
-    minWidth: 220,
-    backgroundColor: 'white',
-    borderRadius: 6,
-    padding: 5,
-    boxShadow: '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
-    '@media (prefers-reduced-motion: no-preference)': {
-        animationDuration: '400ms',
-        animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
-        willChange: 'transform, opacity',
-        '&[data-state="open"]': {
-            '&[data-side="top"]': { animationName: slideDownAndFade },
-            '&[data-side="right"]': { animationName: slideLeftAndFade },
-            '&[data-side="bottom"]': { animationName: slideUpAndFade },
-            '&[data-side="left"]': { animationName: slideRightAndFade },
-        },
-    },
-});
-
 const itemStyles = {
     all: 'unset',
     fontSize: 13,
@@ -91,11 +52,11 @@ const StyledItem = styled(DropdownMenuPrimitive.Item, { ...itemStyles });
 //     color: mauve.mauve11,
 // });
 
-// const StyledSeparator = styled(DropdownMenuPrimitive.Separator, {
-//     height: 1,
-//     backgroundColor: violet.violet6,
-//     margin: 5,
-// });
+const StyledSeparator = styled(DropdownMenuPrimitive.Separator, {
+    height: 1,
+    backgroundColor: violet.violet6,
+    margin: 5,
+});
 
 // const StyledItemIndicator = styled(DropdownMenuPrimitive.ItemIndicator, {
 //     position: 'absolute',
@@ -110,23 +71,53 @@ const StyledItem = styled(DropdownMenuPrimitive.Item, { ...itemStyles });
 //     fill: 'white',
 // });
 
-// Exports
-export const DropdownMenu = DropdownMenuPrimitive.Root;
-export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
-export const DropdownMenuContent = StyledContent;
-export const DropdownMenuItem = StyledItem;
+const slideUpAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateY(2px)' },
+    '100%': { opacity: 1, transform: 'translateY(0)' },
+});
 
-// export const DropdownMenuCheckboxItem = StyledCheckboxItem;
-// export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
-// export const DropdownMenuRadioItem = StyledRadioItem;
-// export const DropdownMenuItemIndicator = StyledItemIndicator;
-// export const DropdownMenuTriggerItem = StyledTriggerItem;
-// export const DropdownMenuLabel = StyledLabel;
-// export const DropdownMenuSeparator = StyledSeparator;
-// export const DropdownMenuArrow = StyledArrow;
+const slideRightAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateX(-2px)' },
+    '100%': { opacity: 1, transform: 'translateX(0)' },
+});
 
-// Your app...
-const Box = styled('div', {});
+const slideDownAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateY(-2px)' },
+    '100%': { opacity: 1, transform: 'translateY(0)' },
+});
+
+const slideLeftAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateX(2px)' },
+    '100%': { opacity: 1, transform: 'translateX(0)' },
+});
+
+const StyledContent = styled(DropdownMenuPrimitive.Content, {
+    minWidth: 220,
+    backgroundColor: 'white',
+    borderRadius: 6,
+    padding: 5,
+    boxShadow: '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+    '@media (prefers-reduced-motion: no-preference)': {
+        animationDuration: '400ms',
+        animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+        willChange: 'transform, opacity',
+        '&[data-state="open"]': {
+            '&[data-side="top"]': { animationName: slideDownAndFade },
+            '&[data-side="right"]': { animationName: slideLeftAndFade },
+            '&[data-side="bottom"]': { animationName: slideUpAndFade },
+            '&[data-side="left"]': { animationName: slideRightAndFade },
+        },
+        '&[data-state="closed"]': {
+            outline: 'none',
+        },
+    },
+});
+
+const DropdownMenu = DropdownMenuPrimitive.Root;
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+const DropdownMenuContent = StyledContent;
+const DropdownMenuItem = StyledItem;
+const DropdownMenuSeparator = StyledSeparator;
 
 export const CardActions = ({ icon }: { icon: React.ReactNode; }) => {
     return (
@@ -137,6 +128,7 @@ export const CardActions = ({ icon }: { icon: React.ReactNode; }) => {
 
             <DropdownMenuContent sideOffset={5}>
                 <DropdownMenuItem>Convert 1</DropdownMenuItem>
+                <DropdownMenuSeparator />
                 <DropdownMenuItem>Convert 2</DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
