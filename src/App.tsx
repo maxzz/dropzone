4232953495994e9eaf82f1b45c27eaf37a976846
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-17 15:16:03 -0700

    Bug wrong scrollbars. OK but on wrong element

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import Toaster from './components/UI/UiToaster';
 import Header from './components/Header';
-import FilesList from './components/ManiList';
+import FilesList from './components/FilesList';
 
 function App() {
     return (
@@ -10,7 +10,8 @@ function App() {
             {/* <div className="p-4 grid grid-rows-[auto,1fr] h-screen"> */}
             <div className="p-4 flex flex-col h-screen">
                 <Header />
-                <div className="flex-1 mt-4 -mr-2 max-h-full overflow-y-hidden">
+                <div className="flex-1 mt-4 -mr-2 max-h-full overflow-y-auto mb-8">
+                {/* <div className="flex-1 mt-4 -mr-2 max-h-full overflow-y-hidden"> */}
                     <FilesList />
                 </div>
             </div>
---
+++ src/components/FilesList.tsx
@@ -0,0 +1,29 @@
+import { useAtom } from 'jotai';
+import React from 'react';
+import { filteredAtom } from '../store/store';
+import ManifestCard from './Card/Card';
+
+function FilesList() {
+    const [files] = useAtom(filteredAtom);
+    return (
+        <div className="ml-[2rem] h-full">
+            <div className="overflow-y-auto" style={{width: 'calc(400px - 4rem)'}}>
+                <div className="h-full mx-auto max-w-[585px]"
+                // <div className="h-full overflow-y-auto mx-auto max-w-[585px] ml-[2rem]"
+                
+                // style={{width: 'calc(100% - 2rem)'}}
+                >
+                    <div className="grid grid-flow-row gap-4 text-sm"
+                    // style={{gridTemplateColumns: 'repeat(auto-fit, minmax(0,1fr))'}}
+                    >
+                        {/* smallscroll smallscroll-light */}
+                        {files.map((atom) =>
+                            <ManifestCard atom={atom} key={`${atom}`} />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default FilesList;
