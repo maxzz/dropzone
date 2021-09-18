d012630b5494369385650320c71385cf5d6e8646
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-18 00:53:03 -0700

    WIP. simplebar-react. add scrollbar as a separated component

--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -2,21 +2,19 @@ import { useAtom } from 'jotai';
 import React from 'react';
 import { filteredAtom } from '../store/store';
 import ManifestCard from './Card/Card';
-
-import SimpleBar from 'simplebar-react';
-import './UI/simplebar.css';
+import UISimpleBar from './UI/UIScrollbar';
 
 function FilesList() {
     const [files] = useAtom(filteredAtom);
     return (
         <div className="h-full w-full">
-            <SimpleBar className="h-full text-gray-500">
+            <UISimpleBar className="h-full text-gray-500">
                 <div className="grid grid-flow-row gap-4 text-sm">
                     {files.map((atom) =>
                         <ManifestCard atom={atom} key={`${atom}`} />
                     )}
                 </div>
-            </SimpleBar>
+            </UISimpleBar>
         </div>
     );
 }
