92229e20536b4f068d812de2ecbcd07b97f0ad03
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-18 18:36:06 -0700

    WIP. App layout. one possible animation of the left panel

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,16 @@ function App() {
         <React.Fragment>
             <div className="h-screen p-4 grid grid-rows-[auto,1fr] grid-cols-[minmax(450px,550px),1fr] gap-y-4 sm:gap-4">
                 <Header className="col-span-full" />
-                <div className="w-full h-full flex-auto overflow-y-hidden bg-green-500">
+                <div className="w-full h-full flex-auto overflow-y-hidden"
+                    style={{
+                        transition: 'transform .2s ease',
+                        transform: 'scaleX(.0001)',
+                        transformOrigin: 'left top',
+                    }}
+                >
                     <FilesList />
                 </div>
-                <RightPanel />
+                <RightPanel className="bg-red-500" />
             </div>
             <Toaster />
         </React.Fragment>
