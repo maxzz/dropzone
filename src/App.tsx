6e776db9fa3586a2cb06f6a4988c5b53b49d0807
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-19 02:06:07 -0700

    WIP. App layout. custom scrollbars much better

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,17 @@ function App() {
             {/* <div className={`h-screen p-4 grid grid-rows-[auto,1fr] ${open ? 'grid-cols-[minmax(450px,550px),1fr]' : 'grid-cols-[0px,1fr]'} gap-y-4 sm:gap-4 sm:--one:1`}> */}
             <div className="h-screen p-4 flex flex-col">
 
-                <Header className="mb-4" />
+                <Header className="flex-none mb-4" />
 
-                <div className="flex-auto h-full" // style={{ transition: 'transform .2s ease', transform: 'scaleX(.0001)', transformOrigin: 'left top', }}
+                <div className="flex-auto flex overflow-y-hidden" // style={{ transition: 'transform .2s ease', transform: 'scaleX(.0001)', transformOrigin: 'left top', }}
                 >
-                    <div className="flex h-full">
+                    {/* <div className="flex h-full"> */}
 
                         <FilesList />
                         <RightPanel className="bg-red-500" />
                         <button className="bg-yellow-600" onClick={() => setOpen((v) => !v)}>Do</button>
 
-                    </div>
+                    {/* </div> */}
                 </div>
 
             </div>
