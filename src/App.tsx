8c35849fe8d098833a76e4b12c0d6322959144b7
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-19 01:18:49 -0700

    WIP. App layout. standard scrollbars OK

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,22 @@ function App() {
     return (
         <React.Fragment>
             {/* <div className="h-screen p-4 grid grid-rows-[auto,1fr] grid-cols-[minmax(450px,550px),1fr] gap-y-4 sm:gap-4 sm:--one:1"> */}
-            <div className={`h-screen p-4 grid grid-rows-[auto,1fr] ${open ? 'grid-cols-[minmax(450px,550px),1fr]' : 'grid-cols-[0px,1fr]'} gap-y-4 sm:gap-4 sm:--one:1`}>
-                <Header className="col-span-full" />
-                <div className="w-full h-full flex-auto overflow-y-hidden"
-                    // style={{
-                    //     transition: 'transform .2s ease',
-                    //     transform: 'scaleX(.0001)',
-                    //     transformOrigin: 'left top',
-                    // }}
+            {/* <div className={`h-screen p-4 grid grid-rows-[auto,1fr] ${open ? 'grid-cols-[minmax(450px,550px),1fr]' : 'grid-cols-[0px,1fr]'} gap-y-4 sm:gap-4 sm:--one:1`}> */}
+            <div className="h-screen p-4 flex flex-col">
+
+                <Header className="mb-4" />
+
+                <div className="flex-auto" // style={{ transition: 'transform .2s ease', transform: 'scaleX(.0001)', transformOrigin: 'left top', }}
                 >
-                    <FilesList />
+                    <div className="flex">
+
+                        <FilesList />
+                        <RightPanel className="bg-red-500" />
+                        <button className="bg-yellow-600" onClick={() => setOpen((v) => !v)}>Do</button>
+
+                    </div>
                 </div>
-                <RightPanel className="bg-red-500" />
-                <button className="col-span-full bg-yellow-600" onClick={() => setOpen((v) => !v)}>Do</button>
+
             </div>
             <Toaster />
         </React.Fragment>
--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -7,11 +7,14 @@ import UISimpleBar from './UI/UIScrollbar';
 function FilesList() { //TODO: add compact view
     const [files] = useAtom(filteredAtom);
     return (
-        <UISimpleBar className="w-full h-full text-gray-500">
-            <div className="grid grid-flow-row gap-4 text-sm">
-                {files.map((atom) => <Card atom={atom} className="" key={`${atom}`} />)}
-            </div>
-        </UISimpleBar>
+        <div className="w-full h-[400px] overflow-y-auto">
+            <UISimpleBar className="text-gray-500">
+                <div className="grid grid-flow-row gap-4 text-sm">
+                    {files.map((atom) => <Card atom={atom} className="" key={`${atom}`} />)}
+                    {/* <div className="w-96 h-[2000px] bg-green-400"></div> */}
+                </div>
+            </UISimpleBar>
+        </div>
     );
 }
 
