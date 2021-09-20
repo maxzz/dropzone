2146fc2226518539e5b55dd1be94e5ef9b97734d
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-19 20:13:46 -0700

    WIP. add right panel. failed attempt to connect simplebar scrollbars

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -3,9 +3,10 @@ import { useAtom } from 'jotai';
 import { rightPanelAtom, rightPanelValueAtom } from '../store/store';
 import { IconBack } from './UI/UiIcons';
 import { useUpdateAtom } from 'jotai/utils';
+import UISimpleBar from './UI/UIScrollbar';
 
 function RightPanel(props: React.HTMLAttributes<HTMLDivElement>) {
-    const {className, ...rest} = props;
+    const { className, ...rest } = props;
     const [rightPanelValue] = useAtom(rightPanelValueAtom);
     const setRightPanel = useUpdateAtom(rightPanelAtom);
     return (
@@ -14,10 +15,12 @@ function RightPanel(props: React.HTMLAttributes<HTMLDivElement>) {
                 <>
                     {/* <IconBack className="w-12 h-12 p-2 bg-green-500" onClick={() => setRightPanel(undefined)} /> */}
 
-                    <div className="my-2 text-xs bg-gray-800 opacity-50 border-4 border-gray-800 shadow-md">
-                        {/* overflow-auto smallscroll */}
-                        <pre>{rightPanelValue.raw}</pre>
-                    </div>
+                    <UISimpleBar className={`text-gray-500 ${className}`} autoHide={false}>
+                        <div className="my-2 overflow-auto text-xs bg-gray-800 opacity-50 border-4 border-gray-800 shadow-md">
+                            {/* overflow-auto smallscroll */}
+                            <pre>{rightPanelValue.raw}</pre>
+                        </div>
+                    </UISimpleBar>
                 </>
             }
         </div>
