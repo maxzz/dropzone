d7ef208f5b487f1a1bb31023289fab7cecbc9f01
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-22 19:04:13 -0700

    WIP. overscrollbars. add scrollbars; somehow working

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -4,26 +4,36 @@ import { rightPanelAtom, rightPanelValueAtom } from '../store/store';
 import { IconBack } from './UI/UiIcons';
 import { useUpdateAtom } from 'jotai/utils';
 import UISimpleBar from './UI/UIScrollbar';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 
 function RightPanel(props: React.HTMLAttributes<HTMLDivElement>) {
     const { className, ...rest } = props;
     const [rightPanelValue] = useAtom(rightPanelValueAtom);
     const setRightPanel = useUpdateAtom(rightPanelAtom);
     return (
-        <div className={`bg-gray-900 text-gray-100 ${className}`} {...rest}>
+        <OverlayScrollbarsComponent>
+        <div className={`bg-gray-900 text-gray-100 w-auto h-auto ${className}`} {...rest}>
             {rightPanelValue &&
                 <>
                     {/* <IconBack className="w-12 h-12 p-2 bg-green-500" onClick={() => setRightPanel(undefined)} /> */}
 
-                    <UISimpleBar className={`text-gray-500 ${className}`} autoHide={false}>
-                        <div className="my-2 text-xs bg-gray-800 opacity-50 border-4 border-gray-800 shadow-md">
-                            {/* overflow-auto smallscroll */}
-                            <pre>{rightPanelValue.raw}</pre>
-                        </div>
-                    </UISimpleBar>
+                    {/* <OverlayScrollbarsComponent> */}
+                        {/* options={{ scrollbars: { autoHide: 'scroll' } }} */}
+                        {/* <UISimpleBar className={`text-gray-500 ${className}`} autoHide={false}> */}
+
+                        {/* <div className="w-full h-full"> */}
+                            <div className="my-2 text-xs bg-gray-800 opacity-50 border-4 border-gray-800 shadow-md">
+                                {/* overflow-auto smallscroll */}
+                                <pre>{rightPanelValue.raw}</pre>
+                            </div>
+                        {/* </div> */}
+                        {/* </UISimpleBar> */}
+                    {/* </OverlayScrollbarsComponent> */}
                 </>
             }
         </div>
+        </OverlayScrollbarsComponent>
     );
 }
 
