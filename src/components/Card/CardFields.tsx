84bc7095227061af5188c6a6173b7ab33d667f3e
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-26 15:00:42 -0700

    WIP. card fields. add react portal per preview

--- src/components/Card/CardFields.tsx
+++ src/components/Card/CardFields.tsx
@@ -66,6 +66,56 @@ function ButtonWithChildren({ name, children, toggle }: { name: string | undefin
     );
 }
 
+function ButtonWithChildrenPortal({ name, children, toggle }: { name: string | undefined; children: React.ReactNode; toggle?: React.ReactNode; }) {
+    const [open, setOpen] = React.useState(false);
+    const buttonRef = React.useRef<HTMLButtonElement>(null);
+    const containerRef = React.useRef<HTMLDivElement>(null);
+    useClickAway(containerRef, (event) => event.target !== containerRef.current && !buttonRef.current?.contains(event.target as HTMLElement) && setOpen(false));
+    if (!name) {
+        return null;
+    }
+    return (
+        <>
+            {toggle
+                ? <button
+                    ref={buttonRef}
+                    onClick={() => setOpen((v) => !v)}
+                >
+                    {toggle}
+                </button>
+                : <button
+                    ref={buttonRef}
+                    className={`pl-2 pr-1 text-xs border border-gray-500 rounded ${open ? 'bg-gray-300' : ''} flex items-center`}
+                    onClick={() => setOpen((v) => !v)}
+                >
+                    <div className="pb-1 mr-1">{name}</div>
+                    {open ? <IconChevronUp className="w-4 h-4" /> : <IconChevronDown className="list-owner w-4 h-4" />}
+                </button>
+            }
+
+            {/* <button
+                ref={buttonRef}
+                className={`pl-2 pr-1 text-xs border border-gray-500 rounded ${open ? 'bg-gray-300' : ''} flex items-center`}
+                onClick={() => setOpen((v) => !v)}
+            >
+                <div className="pb-1 mr-1">{name}</div>
+                {open ? <IconChevronUp className="w-4 h-4" /> : <IconChevronDown className="list-owner w-4 h-4" />}
+            </button> */}
+            {open &&
+                <div ref={containerRef} className="absolute top-[110%] left-0 right-0 z-10 px-2 border border-gray-500 rounded bg-gray-300 text-xs">
+                    {children}
+                </div>
+            }
+            
+            {/* {open &&
+                <div ref={containerRef} className="absolute top-[110%] left-0 right-0 z-10 px-2 border border-gray-500 rounded bg-gray-300 text-xs">
+                    {children}
+                </div>
+            } */}
+        </>
+    );
+}
+
 // Form parts
 
 function OptionLockFields({ lockfields }: { lockfields: string | undefined; }) {
@@ -298,13 +348,15 @@ function TableField({ metaForm, field }: { metaForm: Meta.Form; field: Meta.Fiel
                                         <IconPreview className="w-[14px] h-[14px]" />
                                     </div> */}
 
-                                    {ReactDOM.createPortal(<div className="flex items-center">
+                                    <div className="flex items-center">
                                         <ButtonWithChildren name="preview" toggle={
                                             <IconPreview className="w-[14px] h-[14px]" />
                                         }>
-                                            <FieldPreview form={metaForm} field={field} />
+                                            {ReactDOM.createPortal(
+                                                <FieldPreview form={metaForm} field={field} />, document.getElementById('portal')!
+                                            )}
                                         </ButtonWithChildren>
-                                    </div>, document.getElementById('portal')!)}
+                                    </div>
 
                                     {/* <div className="flex items-center">
                                         <ButtonWithChildren name="preview" toggle={
