d56ac08f27ef254793789fde624b7ba37ecbeecb
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-27 15:45:55 -0700

    WIP. card fields. add useElementClickAway hook. 2. fix typescript complains

--- src/hooks/useElementClickAway.ts
+++ src/hooks/useElementClickAway.ts
@@ -3,19 +3,18 @@ import { on, off } from 'react-use/esm/misc/util';
 
 const defaultEvents = ['mousedown', 'touchstart'];
 
-export const useClickAway = <E extends Event = Event>(
-    ref: RefObject<HTMLElement | null>,
-    onClickAway: (event: E) => void,
-    events: string[] = defaultEvents
-) => {
-    const savedCallback = useRef(onClickAway);
+export const useClickAway = <E extends Event = Event>
+    (ref: RefObject<HTMLElement | null>, onClickAway: (event: E) => void, events: string[] = defaultEvents) => {
+
+    const savedCallback = useRef<(event: E) => void>(onClickAway);
     useEffect(() => {
         savedCallback.current = onClickAway;
     }, [onClickAway]);
+
     useEffect(() => {
-        const handler = (event: Event) => {
+        const handler = (event: E) => {
             const { current: el } = ref;
-            el && !el.contains(event.target) && savedCallback.current(event);
+            el && !el.contains(event.target as Node) && savedCallback.current(event);
         };
         for (const eventName of events) {
             on(document, eventName, handler);
@@ -28,19 +27,18 @@ export const useClickAway = <E extends Event = Event>(
     }, [events, ref]);
 };
 
-export const useElementClickAway = <E extends Event = Event>(
-    ref: RefObject<HTMLElement | null>,
-    onClickAway: (event: E) => void,
-    events: string[] = defaultEvents
-) => {
+export const useElementClickAway = <E extends Event = Event>
+    (ref: RefObject<HTMLElement | null>, onClickAway: (event: E) => void, events: string[] = defaultEvents) => {
+
     const savedCallback = useRef(onClickAway);
     useEffect(() => {
         savedCallback.current = onClickAway;
     }, [onClickAway]);
+
     useEffect(() => {
-        const handler = (event) => {
+        const handler = (event: E) => {
             const { current: el } = ref;
-            el && !el.contains(event.target) && savedCallback.current(event);
+            el && !el.contains(event.target as Node) && savedCallback.current(event);
         };
         for (const eventName of events) {
             on(document, eventName, handler);
