32aa48be1c92e1107f43313e7cd439fb80d6cd9a
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-28 01:33:10 -0700

    WIP. card fields. update CardFieldPreview size

--- src/components/Card/CardFieldPreview.tsx
+++ src/components/Card/CardFieldPreview.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { FieldPath } from '../../store/manifest/mani-functions';
 
-function maxRect(rects: MPath.Chunk_loc[]) {
+function maxRect(rects: MPath.Chunk_loc[]): { x: number; y: number; w: number; h: number; } {
+    let x = 0;
+    let y = 0;
     let w = 0;
     let h = 0;
     rects.forEach(rect => {
+        if (rect.x > x) {
+            x = rect.x;
+        }
+        if (rect.y > y) {
+            y = rect.y;
+        }
         if (rect.x + rect.w > w) {
             w = rect.x + rect.w;
         }
@@ -12,12 +20,14 @@ function maxRect(rects: MPath.Chunk_loc[]) {
             h = rect.y + rect.h;
         }
     });
-    return { w, h };
+    return { x, y, w, h };
 }
 
 export function FieldPreview({ form, field }: { form: Meta.Form; field: Meta.Field; }): JSX.Element {
-    let maxSize = maxRect(form.rects);
+    let size = maxRect(form.rects);
     let thisRects = [...form.rects];
+    console.log('rect', thisRects);
+    
 
     let fieldLocs = FieldPath.PathLocations.pathItem_loc2items(field.path.loc || '');
     if (fieldLocs.length) {
@@ -26,7 +36,7 @@ export function FieldPreview({ form, field }: { form: Meta.Form; field: Meta.Fie
 
     return (
         <div className="rects">
-            <svg viewBox={`0 0 ${maxSize.w} ${maxSize.h}`}>
+            <svg viewBox={`0 0 ${size.w} ${size.h}`}>
                 {thisRects.map((item, idx) => (
                     <rect x={item.x} y={item.y} width={item.w} height={item.h} key={idx} className={`${item.f ? 'last-field' : ''}`}>
                         <title>{idx}</title>
