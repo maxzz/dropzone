59d18a413f8659df5db4e95f7a96d04a4ffa4457
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-28 02:10:33 -0700

    WIP. card fields. update CardFieldPreview all svg styles in js

--- src/components/Card/CardFieldPreview.tsx
+++ src/components/Card/CardFieldPreview.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from '../../stitches.config';
 import { FieldPath } from '../../store/manifest/mani-functions';
 
 function maxRect(rects: MPath.Chunk_loc[]): { x: number; y: number; w: number; h: number; } {
@@ -23,6 +24,19 @@ function maxRect(rects: MPath.Chunk_loc[]): { x: number; y: number; w: number; h
     return { x, y, w, h };
 }
 
+const stylesSvg = css({
+    '--size-div': 4,
+    width: 'calc(1920px / var(--size-div))',
+    height: 'calc(1200px / var(--size-div))',
+    backgroundColor: 'red', //#f4f4f4
+});
+
+const styleRect = css({
+    fill: '#e8e8e8',
+    strokeWidth: 'calc(10 - var(--size-div))',
+    stroke: '#d7a9ff',
+})
+
 export function FieldPreview({ form, field }: { form: Meta.Form; field: Meta.Field; }): JSX.Element {
     let size = maxRect(form.rects);
     let thisRects = [...form.rects];
@@ -36,10 +50,10 @@ export function FieldPreview({ form, field }: { form: Meta.Form; field: Meta.Fie
 
     return (
         <div className="rects">
-            <svg viewBox={`0 0 ${size.w} ${size.h}`}>
+            <svg viewBox={`0 0 ${size.w} ${size.h}`} className={stylesSvg()}>
                 {thisRects.map((item, idx) => (
                     <rect x={item.x} y={item.y} width={item.w} height={item.h} key={idx}
-                        className={`${item.f ? 'last-field' : ''} last:fill-[red]`}
+                        className={`${styleRect()} ${item.f ? '!fill-[#f0ddb0]' : ''} last:fill-[red]`}
                     >
                         <title>{idx}</title>
                     </rect>
