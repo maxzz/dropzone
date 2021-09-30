fbf2ecbe9eefcc0e5d65d9e9edc6504d8f7a5fbb
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-30 16:14:43 -0700

    Add react-split-pane

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import Toaster from './components/UI/UiToaster';
 import Header from './components/Header';
 import FilesList from './components/FilesList';
 import RightPanel from './components/RightPanel';
+import SplitPane from 'react-split-pane';
 
 function App() {
     return (
         <React.Fragment>
             <div className="h-screen p-4 flex flex-col overflow-hidden">
                 <Header className="flex-none mb-4" />
-                <div className="flex-1 flex min-w-0 min-h-0">
+
+                <div className="flex-1">
+                    <SplitPane className="w-full h-full" split="vertical" minSize={350}>
+                        <FilesList className="" />
+                        <RightPanel className="" />
+                    </SplitPane>
+                </div>
+
+                {/* <div className="flex-1 flex min-w-0 min-h-0">
                     <FilesList className="flex-auto w-1/3 flex-shrink-0" />
                     <RightPanel className="ml-4" />
                 </div>
+ */}
             </div>
             <Toaster />
         </React.Fragment>
