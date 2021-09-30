30f98c15e759a00b5cb4a609b3110aec4efa2421
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-30 14:36:56 -0700

    Fix Card code duplication

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,24 +7,12 @@ import FormOptions from './Form/FormOptions';
 import FormFields from './Form/FormFields';
 import UICardFormButton from './UICardFormButton';
 
-// Forms
-
-function FormContentLogin({ cardData }: { cardData: CardDatum; }) {
+function FormContent({ cardData, formIndex }: { cardData: CardDatum; formIndex: number; }) {
     return (
         <div className="">
-            <div className="pt-2 font-bold border-b border-gray-500">Login form</div>
-            <FormOptions cardData={cardData} formIndex={0} />
-            <FormFields cardData={cardData} formIndex={0} />
-        </div>
-    );
-}
-
-function FormContentCpass({ cardData }: { cardData: CardDatum; }) {
-    return (
-        <div className="">
-            <div className="pt-2 font-bold border-b border-gray-500">Password change form</div>
-            <FormOptions cardData={cardData} formIndex={1} />
-            <FormFields cardData={cardData} formIndex={1} />
+            <div className="pt-2 font-bold border-b border-gray-500">{formIndex === 0 ? "Login form" : "Password change form"}</div>
+            <FormOptions cardData={cardData} formIndex={formIndex} />
+            <FormFields cardData={cardData} formIndex={formIndex} />
         </div>
     );
 }
@@ -35,13 +23,13 @@ function CardBodyTopButtons({ cardData }: { cardData: CardDatum; }) {
     return (
         <div className="p-2 bg-gray-200 text-gray-800">
             <div className="flex items-center space-x-2 text-sm">
-                {/* Login form button */}
+                {/* Login */}
                 {cardData.hasLogin && <UICardFormButton cardData={cardData} form={0} opened={open1} onClick={() => setOpen1((v) => !v)} />}
-                {/* Cpass form button */}
+                {/* Password change */}
                 {cardData.hasCpass && <UICardFormButton cardData={cardData} form={1} opened={open2} onClick={() => setOpen2((v) => !v)} />}
             </div>
-            {open1 && (<FormContentLogin cardData={cardData} />)}
-            {open2 && (<FormContentCpass cardData={cardData} />)}
+            {open1 && (<FormContent cardData={cardData} formIndex={0} />)}
+            {open2 && (<FormContent cardData={cardData} formIndex={1} />)}
         </div>
     );
 }
@@ -62,5 +50,4 @@ export default Card;
 
 //TODO: add card index of total
 //TODO: compact view
-
 //TODO: some IE forms have no detection section: but we can check IE_Server and presences of locations
