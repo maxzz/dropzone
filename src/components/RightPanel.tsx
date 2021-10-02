de282b944811713b041bbaca5d2d8ec642883a1b
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-01 18:57:14 -0700

    Add RightPanel bottom separator

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -14,14 +14,15 @@ function RightPanel(props: React.HTMLAttributes<HTMLDivElement>) {
         <>
             {/* <IconBack className="w-12 h-12 p-2 bg-green-500" onClick={() => setRightPanel(undefined)} /> */}
 
-            <div className={`px-2 pt-4 pb-2 flex-auto overflow-hidden w-full h-full bg-gray-900 ${className}`} {...rest}>
+            <div className={`pt-4 pb-2 flex-auto overflow-hidden w-full h-full bg-gray-900 ${className}`} {...rest}>
                 {rightPanelValue &&
-                    <div className="w-full h-full flex flex-col">
-                        <div className="my-2 text-gray-100 bg-gray-900">
+                    <div className="w-full h-full flex flex-col space-y-2">
+                        {/* Card title */}
+                        <div className="p-2 text-gray-100 bg-gray-900 border-b-[0.5px] border-gray-600">
                             <CardTitleText atom={rightPanelValueAtom as FileUsAtom} />
                         </div>
-                        <UISimpleBar className={`overflow-auto w-full h-full text-gray-500 cursor-default`}>
-                            {/* <CardTitle /> */}
+                        {/* Raw data preview (+ codemirror?) */}
+                        <UISimpleBar className={`px-2 overflow-auto w-full h-full text-gray-500 cursor-default`}>
                             <div className="text-xs bg-gray-800 text-gray-100 opacity-50">
                                 <pre>{rightPanelValue.raw}</pre>
                             </div>
