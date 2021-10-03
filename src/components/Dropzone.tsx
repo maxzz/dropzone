f1c3b284258bc004581b3918679e8be5cb1a5868
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-03 16:21:35 -0700

    Fix. conflict dropzone and dropdown menu events

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -48,35 +48,38 @@ export function DropzoneArea({ children }: { children?: React.ReactNode; }) {
     const [files, setFiles] = useAtom(filesAtom);
     return (
         <div className="min-h-[40px] flex justify-between bg-gray-700 text-gray-100 ring-2 ring-gray-50 rounded-md">
-            <DropzoneBase
-                className={`m-0.5 rounded-l flex items-stretch ${files.length ? 'bg-gray-600' : 'bg-gray-900'} cursor-pointer`}
-                stylesActive={{ backgroundColor: '#059669' }} // {/* bg-green-600: classNameActive is not good for tailwind parser */}
-            >
-                {files.length
-                    ?
-                    <div className="flex items-center">
-                        <div className="mr-4 my-2 uppercase text-xs flex items-center">
-                            <IconDocumentsAccepted className="w-6 h-6 ml-2 mr-1" />
-                            {files.length} file{files.length === 1 ? '' : 's'}
+            
+            <div className="flex my-0.5">
+                <DropzoneBase
+                    className={`ml-0.5 rounded-l flex items-stretch ${files.length ? 'bg-gray-600' : 'bg-gray-900'} cursor-pointer`}
+                    stylesActive={{ backgroundColor: '#059669' }} // {/* bg-green-600: classNameActive is not good for tailwind parser */}
+                >
+                    {files.length
+                        ?
+                        <div className="flex items-center">
+                            <div className="mr-4 my-2 uppercase text-xs flex items-center">
+                                <IconDocumentsAccepted className="w-6 h-6 ml-2 mr-1" />
+                                {files.length} file{files.length === 1 ? '' : 's'}
+                            </div>
                         </div>
-
-                        <div className="px-2 self-stretch border-l rounded-none border-gray-500 flex items-center justify-center">
-                            <TopMenu icon={
-                                <IconMenuHamburger className="p-1 w-8 h-8 rounded hover:bg-gray-700" />
-                            }
-                            />
+                        :
+                        <div className="px-4 py-2">
+                            Drag 'n' drop files here, or click to select files
                         </div>
-
-                        <button className="px-2 self-stretch border-l rounded-none border-gray-500 flex items-center justify-center">
+                    }
+                </DropzoneBase>
+                {!!files.length &&
+                    <>
+                        <div className="px-2 self-stretch border-l rounded-none border-gray-500 bg-gray-600 flex items-center justify-center">
+                            <TopMenu icon={ <IconMenuHamburger className="p-1 w-8 h-8 rounded hover:bg-gray-700" /> } />
+                        </div>
+                        <button className="px-2 self-stretch border-l rounded-none border-gray-500 bg-gray-600 flex items-center justify-center">
                             <IconTrash className="w-8 h-8 p-2 rounded hover:bg-red-500 active:scale-[.97]" onClick={(event) => { event.stopPropagation(); setFiles([]); }} />
                         </button>
-                    </div>
-                    :
-                    <div className="px-4 py-2">
-                        Drag 'n' drop files here, or click to select files
-                    </div>
+                    </>
                 }
-            </DropzoneBase>
+            </div>
+
             <div className="flex items-center justify-between">
                 {children}
                 <div className="w-7 h-7 mx-4" onClick={(event) => { event.stopPropagation(); toast('again'); }}>
