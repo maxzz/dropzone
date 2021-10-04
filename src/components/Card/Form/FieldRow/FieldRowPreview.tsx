e926fd11028f6c798c02d301d95516d31e4417c2
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-03 18:50:00 -0700

    WIP. preview w/ selection. defined small prop

--- src/components/Card/Form/FieldRow/FieldRowPreview.tsx
+++ src/components/Card/Form/FieldRow/FieldRowPreview.tsx
@@ -3,10 +3,10 @@ import { css } from '../../../../stitches.config';
 import { FieldPath } from '../../../../store/manifest/mani-functions';
 
 const stylesSvg = css({
-    '--size-div': 4,
+    // '--size-div': 4,
     // width: 'calc(1920px / var(--size-div))',
     // height: 'calc(1200px / var(--size-div))',
-    background: 'radial-gradient(circle, #679dff 0%, #3478f4 100%)',
+    //background: 'radial-gradient(circle, #679dff 0%, #3478f4 100%)',
     stroke: '#d1d1d1',
     strokeWidth: 1,
 });
@@ -32,10 +32,12 @@ type PreviewProps = {
 
 function FieldRowPreview({ form, highlight, small, ...attrs }: PreviewProps): JSX.Element | null {
     const view = form.view;
-    if (!view) {
+    if (!view || !view.rects.length) {
         return null;
     }
     let { rects, bounds } = view;
+    console.log({ view });
+
 
     const asbPos = true;
     if (asbPos) {
@@ -44,15 +46,18 @@ function FieldRowPreview({ form, highlight, small, ...attrs }: PreviewProps): JS
         bounds = FieldPath.loc.utils.rectsBoundaries(rects);
     }
 
-    const {className, ...rest} = attrs;
+    const { className, ...rest } = attrs;
+    const styles: React.CSSProperties = {
+        background: small ? 'transparent': 'radial-gradient(circle, #679dff 0%, #3478f4 100%)'
+    };
 
     return (
-        <svg viewBox={`0 0 ${bounds.x2} ${bounds.y2}`} className={`${stylesSvg()} ${className}`} {...rest}>
+        <svg viewBox={`0 0 ${bounds.x2} ${bounds.y2}`} className={`${stylesSvg()} ${className}`} style={styles} {...rest}>
             {rects.map((rect, idx) => (
                 <rect x={rect.x} y={rect.y} width={rect.w} height={rect.h} key={idx}
-                    className={`${idx === highlight ? 'fill-[#00ff62]' : rect.f ? 'fill-[#454545]' : 'fill-[#0008] hover:fill-[beige]'}`}
+                    className={`${idx === highlight ? 'fill-[#00ff62]' : rect.f ? 'fill-[#454545]' : `fill-[#0008] ${small ? '' : 'hover:fill-[beige]'}`}`}
                 >
-                    <title>{`xy: ${rect.x},${rect.y} wh: ${rect.w} x ${rect.h}`}</title>
+                    {!small && <title>{`xy: ${rect.x},${rect.y} wh: ${rect.w} x ${rect.h}`}</title>}
                 </rect>
             ))}
         </svg>
