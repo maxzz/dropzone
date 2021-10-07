b50a89d2f39732fd4cd9c7f834ecc345fae896ad
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-07 01:58:33 -0700

    Code clean up. remove old FieldRow

--- src/components/Card/Form/FieldRow/FieldRow.tsx
+++ src/components/Card/Form/FieldRow/FieldRow.tsx
@@ -2,75 +2,9 @@ import React from 'react';
 import { IconInOut, IconInputFieldChk, IconInputFieldChkEmpty, IconPreview } from '../../../UI/UiIcons';
 import FieldRowPreview from './FieldRowPreview';
 import FormRowTypeIcon from './FieldRowTypeIcon';
-import { FieldFirstCol, FieldSecondCol } from '../../UICard/UITableFromObject';
 import UIToggleWithPortal from '../../UICard/UIToggleWithPortal';
 import { useAtom } from 'jotai';
 import { SelectRowAtoms } from '../../../../store/store';
-
-export function FieldRowOld({ metaForm, field }: { metaForm: Meta.Form; field: Meta.Field; }): JSX.Element {
-    const { displayname, type, dbname, path_ext, rfield, rfieldindex, password, useit, } = field.mani;
-    const toShow = {
-        ...(displayname && { displayname }),
-        ...(type && { type }),
-        ...(dbname && { id: dbname }),
-        ...(path_ext && { path: path_ext }),
-        ...(rfield && { rfield }),
-        ...(rfieldindex && { rfieldindex }),
-        ...(password && { password }),
-        ...(useit && { useit }),
-    };
-    const values = Object.entries(toShow);
-    return (
-        <div className="grid grid-cols-[minmax(7rem,auto),1fr] items-center text-xs">
-            {values.map(([key, val], idx) => {
-                if (key === 'displayname' || key === 'password' || key === 'useit') {
-                    return;
-                }
-                if (key === 'type') {
-                    return (
-                        <React.Fragment key={`${key || idx}`}>
-                            <FieldFirstCol className="bg-gray-300 relative">
-                                <div className="flex items-center justify-between pr-1">
-                                    <FormRowTypeIcon field={toShow} />
-                                    <div className="flex-1">{`${password ? 'psw' : val}`}</div>
-
-                                    {/* <ButtonWithChildrenPortal name="preview" toggle={<IconPreview className="w-[14px] h-[14px]" />}>
-                                        <FieldPreview form={metaForm} field={field} />
-                                    </ButtonWithChildrenPortal> */}
-
-                                    {/* <ToggleWithPortal toggle={<div className="w-[14px] h-[14px]">text</div>}> */}
-                                    <UIToggleWithPortal toggle={<IconPreview className="w-[14px] h-[14px]" />}>
-                                        <FieldRowPreview form={metaForm} selected={field.ridx} small={false} />
-                                    </UIToggleWithPortal>
-
-                                    {/* <div className="flex items-center">
-                                        <ButtonWithChildren name="preview" toggle={ <IconPreview className="w-[14px] h-[14px]" /> }>
-                                            <FieldPreview form={metaForm} field={field} />
-                                        </ButtonWithChildren>
-                                    </div> */}
-
-                                    {useit
-                                        ? <IconInputFieldChk className="w-4 h-4" fill="#38a00040" />
-                                        : <IconInputFieldChkEmpty className="w-4 h-4" />
-                                    }
-                                </div>
-                            </FieldFirstCol>
-                            <FieldSecondCol className="bg-gray-300">
-                                {toShow.displayname}
-                            </FieldSecondCol>
-                        </React.Fragment>);
-                }
-                return (
-                    <React.Fragment key={`${key || idx}`}>
-                        <FieldFirstCol>{key}</FieldFirstCol>
-                        <FieldSecondCol>
-                            <div className="flex items-center">{`${val}`}</div>
-                        </FieldSecondCol>
-                    </React.Fragment>);
-            })}
-        </div>
-    );
-}
 
 type FieldRowProps = {
     form: Meta.Form;
@@ -83,6 +17,7 @@ function FieldRow({ form, field, selectRowAtoms }: FieldRowProps): JSX.Element {
     const selectInFormAtom = form.type === 0 ? selectRowAtoms.loginAtom : selectRowAtoms.cpassAtom;
     const [selectedRow, setSelectedRow] = useAtom(selectInFormAtom);
     const isSelected = form.view?.rects.length && field.ridx === selectedRow;
+    const isScript = !!field.path.loc;
 
     const disp = type !== 'text'
         ?
@@ -97,9 +32,9 @@ function FieldRow({ form, field, selectRowAtoms }: FieldRowProps): JSX.Element {
                 patern
             </div>
         </div>;
-    const isScript = !!field.path.loc;
+
     return (
-        <div className={`flex items-center text-xs h-6 space-x-1 overflow-hidden ${useit?'bg-[#bbffdf42]':''} ${isSelected ? '!bg-blue-200' : ''}`}
+        <div className={`flex items-center text-xs h-6 space-x-1 overflow-hidden ${useit ? 'bg-[#bbffdf42]' : ''} ${isSelected ? '!bg-blue-200' : ''}`}
             onClick={() => {
                 setSelectedRow(isSelected ? -1 : field.ridx);
             }}
