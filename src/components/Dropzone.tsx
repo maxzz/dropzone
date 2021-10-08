ffc3d4346cac8ca5dfe163f42939f36864626fc5
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-08 15:49:00 -0700

    Add notes on a dropped folder data structure

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -26,7 +26,10 @@ type DropzoneBaseProps = React.HTMLAttributes<HTMLDivElement> & {
 export function DropzoneBase({ className, classNameActive, stylesActive = {}, children }: DropzoneBaseProps) {
     const setFiles = useUpdateAtom(SetFilesAtom);
 
-    const onDrop = useCallback((accepterFiles: File[]) => setFiles(accepterFiles), []);
+    const onDrop = useCallback((accepterFiles: File[]) => {
+        //console.log('files', accepterFiles);
+        setFiles(accepterFiles);
+    }, []);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         onDrop,
@@ -67,7 +70,7 @@ export function DropzoneArea({ children }: { children?: React.ReactNode; }) {
                         </div>
                     }
                 </DropzoneBase>
-                
+
                 {!!total &&
                     <>
                         <div className="px-2 self-stretch border-l rounded-none border-gray-500 bg-gray-600 flex items-center justify-center cursor-pointer">
@@ -89,3 +92,18 @@ export function DropzoneArea({ children }: { children?: React.ReactNode; }) {
         </div>
     );
 }
+
+//TODO: if we drop a folder then we have a relative path from this folder as a root
+/*
+(10) [File, File, File, File, File, File, File, File, File, File]
+0: File {path: '/DI Admin Test/field_catalog.dpn', name: 'field_catalog.dpn', lastModified: 1585844956000, lastModifiedDate: Thu Apr 02 2020 09:29:16 GMT-0700 (Pacific Daylight Time), webkitRelativePath: '', …}
+1: File {path: '/DI Admin Test/OLD DNA Lockout/{8a5a0d28-3d14-436c-913d-f638140f3f8c}.dpm', name: '{8a5a0d28-3d14-436c-913d-f638140f3f8c}.dpm', lastModified: 1606161064000, lastModifiedDate: Mon Nov 23 2020 11:51:04 GMT-0800 (Pacific Standard Time), webkitRelativePath: '', …}
+2: File {path: '/DI Admin Test/OLD DNA Prod/{c64c064e-e23a-46dd-b3a6-75a9c53f42a9}.dpm', name: '{c64c064e-e23a-46dd-b3a6-75a9c53f42a9}.dpm', lastModified: 1606172264000, lastModifiedDate: Mon Nov 23 2020 14:57:44 GMT-0800 (Pacific Standard Time), webkitRelativePath: '', …}
+3: File {path: '/DI Admin Test/OLD DNA Prod/{cfbbce14-f9bf-4a2a-bf23-ee3e92d79dd1}.dpm', name: '{cfbbce14-f9bf-4a2a-bf23-ee3e92d79dd1}.dpm', lastModified: 1606162860000, lastModifiedDate: Mon Nov 23 2020 12:21:00 GMT-0800 (Pacific Standard Time), webkitRelativePath: '', …}
+4: File {path: '/DI Admin Test/OLD DNA Prod/{e923da1c-f705-49cc-a678-992599ca331e}.dpm', name: '{e923da1c-f705-49cc-a678-992599ca331e}.dpm', lastModified: 1606082640000, lastModifiedDate: Sun Nov 22 2020 14:04:00 GMT-0800 (Pacific Standard Time), webkitRelativePath: '', …}
+5: File {path: '/DI Admin Test/Old DNA Train/{1c685ee7-6f0c-41f6-8ec0-6fcb6e8fefd1}.dpm', name: '{1c685ee7-6f0c-41f6-8ec0-6fcb6e8fefd1}.dpm', lastModified: 1606766152000, lastModifiedDate: Mon Nov 30 2020 11:55:52 GMT-0800 (Pacific Standard Time), webkitRelativePath: '', …}
+6: File {path: '/DI Admin Test/Old DNA Train/{d057704e-f02e-4415-8d82-ec002c7bb468}.dpm', name: '{d057704e-f02e-4415-8d82-ec002c7bb468}.dpm', lastModified: 1569526384000, lastModifiedDate: Thu Sep 26 2019 12:33:04 GMT-0700 (Pacific Daylight Time), webkitRelativePath: '', …}
+7: File {path: '/DI Admin Test/{059dcff8-69f0-4c72-8db4-163a39507fdf}.dpm', name: '{059dcff8-69f0-4c72-8db4-163a39507fdf}.dpm', lastModified: 1585678674000, lastModifiedDate: Tue Mar 31 2020 11:17:54 GMT-0700 (Pacific Daylight Time), webkitRelativePath: '', …}
+8: File {path: '/DI Admin Test/{25297e6a-27e0-414d-8d98-07b8e02e67ac}.dpm', name: '{25297e6a-27e0-414d-8d98-07b8e02e67ac}.dpm', lastModified: 1585847570000, lastModifiedDate: Thu Apr 02 2020 10:12:50 GMT-0700 (Pacific Daylight Time), webkitRelativePath: '', …}
+9: File {path: '/DI Admin Test/{bee51e93-e397-416d-912a-04dcacfded72}.dpm', name: '{bee51e93-e397-416d-912a-04dcacfded72}.dpm', lastModified: 1595626694000, lastModifiedDate: Fri Jul 24 2020 14:38:14 GMT-0700 (Pacific Daylight Time), webkitRelativePath: '', …}
+*/
