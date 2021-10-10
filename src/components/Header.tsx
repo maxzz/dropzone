8bbbd4cb2ccbefe5067c97047c902f3fd4a1f606
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-10 14:17:01 -0700

    WIP. busy indicator. fix unnecessary counters re-renders

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,50 @@
 import React from 'react';
-import { useAtom } from 'jotai';
+import { PrimitiveAtom, useAtom } from 'jotai';
 import { showEmptyManiAtom, showManualManiAtom, showNormalManiAtom, totalEmptyManiAtom, totalManualManiAtom, totalNormalManiAtom } from '../store/store';
 import LabeledSwitch from './UI/UiSwitch';
 import { DropzoneArea } from './Dropzone';
 
-function LabeWithNumber({ label, value }: { label: string; value: number; }) {
+function LabeWithNumber({ label, atomCnt }: { label: string; atomCnt: PrimitiveAtom<number>; }) {
+    const [total] = useAtom(atomCnt);
     return (
         <div className="ml-2 flex items-center">
             <div className="inline-block">{label}</div>
-            <div className="inline-block ml-1 pb-3">{value}</div>
+            <div className="inline-block ml-1 pb-3">{total}</div>
         </div>
     );
 }
 
-function AppFilters() {
-    const [showNormalMani, setShowNormalMani] = useAtom(showNormalManiAtom);
-    const [showManualMani, setShowManualMani] = useAtom(showManualManiAtom);
-    const [showEmptyMani, setShowEmptyMani] = useAtom(showEmptyManiAtom);
-
-    const [totalNormalMani] = useAtom(totalNormalManiAtom);
-    const [totalManualMani] = useAtom(totalManualManiAtom);
-    const [totalEmptyMani] = useAtom(totalEmptyManiAtom);
-    return (<>
-        <LabeledSwitch label={<LabeWithNumber label={'Normal'} value={totalNormalMani} />} value={showNormalMani} onChange={() => setShowNormalMani(!showNormalMani)} title="Show normal mode manifests" />
-        <LabeledSwitch label={<LabeWithNumber label={'Manual'} value={totalManualMani} />} value={showManualMani} onChange={() => setShowManualMani(!showManualMani)} title="Show manual mode manifests" />
-        <LabeledSwitch label={<LabeWithNumber label={'Empty'} value={totalEmptyMani} />} value={showEmptyMani} onChange={() => setShowEmptyMani(!showEmptyMani)} title="Show excluded manifests" />
-    </>);
+function AppFilter({ atomShow, atomCnt, label, title }: { atomShow: PrimitiveAtom<boolean>, atomCnt: PrimitiveAtom<number>, label: string, title: string; }) {
+    const [show, setShow] = useAtom(atomShow);
+    return (
+        <LabeledSwitch label={<LabeWithNumber label={label} atomCnt={atomCnt} />} value={show} onChange={() => setShow(!show)} title={title} />
+    );
 }
+
+// function AppFilters() {
+//     const [showNormalMani, setShowNormalMani] = useAtom(showNormalManiAtom);
+//     const [showManualMani, setShowManualMani] = useAtom(showManualManiAtom);
+//     const [showEmptyMani, setShowEmptyMani] = useAtom(showEmptyManiAtom);
+
+//     const [totalNormalMani] = useAtom(totalNormalManiAtom);
+//     const [totalManualMani] = useAtom(totalManualManiAtom);
+//     const [totalEmptyMani] = useAtom(totalEmptyManiAtom);
+//     return (<>
+//         <LabeledSwitch label={<LabeWithNumber label={'Normal'} value={totalNormalMani} />} value={showNormalMani} onChange={() => setShowNormalMani(!showNormalMani)} title="Show normal mode manifests" />
+//         <LabeledSwitch label={<LabeWithNumber label={'Manual'} value={totalManualMani} />} value={showManualMani} onChange={() => setShowManualMani(!showManualMani)} title="Show manual mode manifests" />
+//         <LabeledSwitch label={<LabeWithNumber label={'Empty'} value={totalEmptyMani} />} value={showEmptyMani} onChange={() => setShowEmptyMani(!showEmptyMani)} title="Show excluded manifests" />
+//     </>);
+// }
 
 function Header(props: React.HTMLAttributes<HTMLElement>) {
     return (
         <header className="" {...props}>
             <DropzoneArea>
                 <div className="p-2 sm:p-0 flex flex-col sm:flex-row items-end sm:items-center space-x-2 space-y-2 sm:space-y-0 text-sm text-gray-200">
-                    <AppFilters />
+                    {/* <AppFilters /> */}
+                    <AppFilter  atomShow={showNormalManiAtom} atomCnt={totalNormalManiAtom} label="Normal" title="Show normal mode manifests" />
+                    <AppFilter  atomShow={showManualManiAtom} atomCnt={totalManualManiAtom} label="Manual" title="Show manual mode manifests" />
+                    <AppFilter  atomShow={showEmptyManiAtom} atomCnt={totalEmptyManiAtom} label="Empty" title="Show excluded manifests" />
                 </div>
             </DropzoneArea>
         </header>
