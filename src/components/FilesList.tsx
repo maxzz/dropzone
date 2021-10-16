530521eb508e5e5fdca86bdf7451638f94fc1033
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-16 16:33:20 -0700

    WIP. test 179. bug w/ delay. new menu code clean up

--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -4,6 +4,18 @@ import { filteredAtom } from '../store/store';
 import Card from './Card/Card';
 import UISimpleBar from './UI/UIScrollbar';
 
+function FilesListOld(props: React.HTMLAttributes<HTMLElement>) { //TODO: add compact view
+    const {className, ...rest} = props;
+    const [files] = useAtom(filteredAtom);
+    return (
+        <UISimpleBar className={`p-3 text-gray-500 bg-gray-700 ${className} overflow-auto w-full h-full`} {...rest}>
+            <div className="grid grid-flow-row gap-4 text-sm">
+                {files.map((atom) => <Card fileUsAtom={atom} className="" key={`${atom}`} />)}
+            </div>
+        </UISimpleBar>
+    );
+}
+
 function FilesList(props: React.HTMLAttributes<HTMLElement>) { //TODO: add compact view
     const {className, ...rest} = props;
     const [files] = useAtom(filteredAtom);
