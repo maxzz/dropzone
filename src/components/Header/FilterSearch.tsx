fe34099b4d5ee5f7c070edd970198c3b4ded1fb0
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-20 14:21:56 -0700

    WIP. filters. add search UI implemention

--- src/components/Header/FilterSearch.tsx
+++ src/components/Header/FilterSearch.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { IconSearch } from '../UI/UiIcons';
+import { IconClose, IconSearch } from '../UI/UiIcons';
 
 function FilterSearch() {
+    const [filter, setFilter] = React.useState('');
+    const isEmpty = !filter;
     return (
-        <div className="flex-1 max-w-[40rem] ml-2 md:ml-4 sm:self-stretch md:self-end md:pb-2 lg:pb-0 lg:self-auto">
-            <div className="w-full h-8 px-1 border-2 rounded flex items-center bg-gray-600">
-                <input className="w-full text-sm text-gray-200 bg-gray-600" />
-                <IconSearch className="w-4 h-4" />
+        <div className="flex-1 max-w-[40rem] ml-2 md:ml-4 sm:self-stretch md:self-end md:pb-2 lg:pb-0 lg:self-auto flex justify-end">
+            <div className={`${isEmpty?'w-12 rounded-full':'w-full rounded'} h-8 px-2 border-2 flex items-center bg-gray-600`}>
+                <input className="w-full h-6 text-sm text-gray-200 bg-gray-600 focus:outline-none" value={filter} onChange={(event) => setFilter(event.target.value)} />
+                {isEmpty
+                    ? <IconSearch className="w-4 h-4 flex-none" />
+                    : <IconClose onClick={() => setFilter('')} className="w-6 h-6 p-0.5 cursor-pointer" />
+                }
             </div>
         </div>
     );
