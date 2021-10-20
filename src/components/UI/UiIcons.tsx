d89e54ea84562c70b456965a077e88a05378066f
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-20 14:02:35 -0700

    WIP. filters. add cross-mark icon

--- src/components/UI/UiIcons.tsx
+++ src/components/UI/UiIcons.tsx
@@ -362,3 +362,11 @@ export function IconSearch(props: SVGProps<SVGSVGElement>) {
         </svg>
     );
 }
+
+export function IconClose(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+            <path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29l-4.3 4.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4.29-4.3l4.29 4.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42z" />
+        </svg>
+    );
+}
