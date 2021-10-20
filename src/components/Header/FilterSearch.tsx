26b2c04c9f3e7d4e498216d144bc1b314cfbbdd4
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-19 17:29:37 -0700

    WIP. filters. fix flex on search filter for sm size

--- src/components/Header/FilterSearch.tsx
+++ src/components/Header/FilterSearch.tsx
@@ -3,7 +3,7 @@ import { IconSearch } from '../UI/UiIcons';
 
 function FilterSearch() {
     return (
-        <div className="flex-1 max-w-[40rem] ml-4 sm:self-stretch md:self-end md:pb-2 lg:pb-0 lg:self-auto">
+        <div className="flex-1 max-w-[40rem] ml-2 md:ml-4 sm:self-stretch md:self-end md:pb-2 lg:pb-0 lg:self-auto">
             <div className="w-full h-8 px-1 border-2 rounded flex items-center bg-gray-600">
                 <input className="w-full text-sm text-gray-200 bg-gray-600" />
                 <IconSearch className="w-4 h-4" />
