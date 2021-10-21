71ab9094707db4cc95e3eba3b0f3d084db78d8f3
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-21 01:42:56 -0700

    WIP. filters. fix Ctrl+K hint activation

--- src/components/Header/FilterSearch.tsx
+++ src/components/Header/FilterSearch.tsx
@@ -20,22 +20,22 @@ function ToggleCaseSensitive() {
 function FilterSearch() {
     const [filter, setFilter] = useAtom(searchFilterAtom);
     const keyboardRef = React.useRef<HTMLInputElement>(null);
+    const [active, setActive] = React.useState(false);
     useKey('Escape', () => setFilter(''), { target: keyboardRef.current });
-    useKey((event) => {
-        return event.ctrlKey && event.key === 'd';
-    }, (event) => {
-        event.preventDefault();
-        keyboardRef.current && keyboardRef.current.focus();
-    });
+    useKey((event) => event.ctrlKey && event.key === 'd', (event) => { event.preventDefault(); keyboardRef.current && keyboardRef.current.focus(); });
     const isEmpty = !filter;
     return (
         <div className="flex-1 max-w-[40rem] ml-2 md:ml-4 sm:self-stretch md:self-end md:pb-2 lg:pb-0 lg:self-auto flex justify-end">
             <div
                 className={`h-8 px-2 flex items-center bg-gray-700 focus-within:bg-gray-600 border-2 ${isEmpty ? 'w-12 rounded-full' : 'w-full rounded-md'}`}
                 title="Search (Ctrl+D). win: to show only Windows apps; web: to show only web apps"
-            >
+                onFocus={() => setActive(true)}
+                onBlur={() => setActive(false)}
+                onClick={() => keyboardRef.current && keyboardRef.current.focus()}
+        >
                 <input
-                    className="w-full h-6 text-sm text-gray-200 bg-transparent focus:outline-none group"
+                    className="w-full h-6 text-sm text-gray-200 bg-transparent focus:outline-none"
+                    spellCheck="false"
                     ref={keyboardRef}
                     value={filter}
                     onChange={(event) => setFilter(event.target.value)}
@@ -44,10 +44,10 @@ function FilterSearch() {
                     ?
                     // Ctrl+D and Search icon
                     <div className="flex-none relative">
-                        <div className="absolute -left-3.5 -top-0.5 flex flex-col items-center text-gray-500 pointer-events-none group-focus:bg-red-600">
+                        {!active && <div className="absolute -left-3.5 -top-0.5 flex flex-col items-center text-gray-400 pointer-events-none">
                             <IconCtrl className="w-3 h-3" />
                             <div className="text-[.5rem] leading-[.5rem]">+D</div>
-                        </div>
+                        </div>}
                         <IconSearch className="w-4 h-4" />
                     </div>
                     : <>
@@ -63,3 +63,4 @@ function FilterSearch() {
 export default FilterSearch;
 
 //TODO: add history to UI and store history to the localStorage
+//TODO: win: and web: prefixes
