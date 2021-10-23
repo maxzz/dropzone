19d6e6d0e52bc88341156423eb38c7faabba3d65
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-22 19:03:23 -0700

    Feature. svg font spy as a component final tweaks

--- src/utils/SvgFontSpy.tsx
+++ src/utils/SvgFontSpy.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-function SvgFontSpy() {
+function SvgFontSpy({ fontID = 'svgfont' }: { fontID?: string; }) {
     const [ids, setIds] = React.useState<string[]>([]);
     React.useEffect(() => {
-        const font = document.querySelector('#svgfont > defs')?.children;
+        const font = document.querySelector(`#${fontID} > defs`)?.children;
         const ids = (font ? [...font] : []).map(item => item.id);
         setIds(ids);
-
-        console.log({ svgs: ids });
     }, []);
-    return (
-        <div className="w-[80vw] mx-auto mt-4 grid grid-cols-[repeat(auto-fill,minmax(0,64px))] gap-4">
+    return (<>
+        {!!ids.length && <div className="w-[80vw] mx-auto mt-4 grid grid-cols-[repeat(auto-fill,minmax(0,64px))] gap-4">
             {ids.map((id, idx) =>
                 <div className="w-16 h-16 border-4 border-gray-100 bg-gray-300 shadow-lg">
                     <svg fill="#c3b2d3" stroke="black" strokeWidth={.5} className={`w-full h-full`} key={idx}>
@@ -18,9 +16,11 @@ function SvgFontSpy() {
                         <use xlinkHref={`#${id}`} />
                     </svg>
                 </div>
-            )}
-        </div>
-    );
+            )
+            }
+        </div >
+        }
+    </>);
 }
 
 export default SvgFontSpy;
