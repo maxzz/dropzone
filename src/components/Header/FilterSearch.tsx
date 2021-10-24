53898e5dab61722dfda3a704ea911f5fcbadd88f
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-24 16:17:29 -0700

    Feature. add seach by caption and classname. simplify tooltip

--- src/components/Header/FilterSearch.tsx
+++ src/components/Header/FilterSearch.tsx
@@ -74,11 +74,11 @@ function FilterSearch() {
                 <div className="text-sm py-1 px-1">
                     <div className="font-bold">Search (Ctrl+D)</div>
                     <div className="pb-1">Use the search prefix to dispay only:</div>
-                    <div className=""><IconDot className="w-3 h-3 inline" /><span className="inline-block font-bold font-mono tracking-tight w-8">win:</span> logins for Windows apps</div>
-                    <div className=""><IconDot className="w-3 h-3 inline" /><span className="inline-block font-bold font-mono tracking-tight w-8">web:</span> logins for web apps</div>
-                    <div className=""><IconDot className="w-3 h-3 inline" /><span className="inline-block font-bold font-mono tracking-tight w-8">why:</span> logins with problems to check why</div>
-                    <div className=""><IconDot className="w-3 h-3 inline" /><span className="inline-block font-bold font-mono tracking-tight w-8">cap:</span> logins with window caption</div>
-                    <div className=""><IconDot className="w-3 h-3 inline" /><span className="inline-block font-bold font-mono tracking-tight w-8">cls:</span> logins with window classname</div>
+                    <div className=""><IconDot className="w-3 h-3 inline" fill="none" stroke="black" /><span className="inline-block font-bold font-mono tracking-tight w-8">win:</span> logins for Windows apps</div>
+                    <div className=""><IconDot className="w-3 h-3 inline" fill="none" stroke="black" /><span className="inline-block font-bold font-mono tracking-tight w-8">web:</span> logins for web apps</div>
+                    <div className=""><IconDot className="w-3 h-3 inline" fill="none" stroke="black" /><span className="inline-block font-bold font-mono tracking-tight w-8">why:</span> logins with problems to check why</div>
+                    <div className=""><IconDot className="w-3 h-3 inline" fill="none" stroke="black" /><span className="inline-block font-bold font-mono tracking-tight w-8">cap:</span> logins with window caption</div>
+                    <div className=""><IconDot className="w-3 h-3 inline" fill="none" stroke="black" /><span className="inline-block font-bold font-mono tracking-tight w-8">cls:</span> logins with window classname</div>
                 </div>
             </UITooltip>
         </div>
