15100eecb533e639619038394df2afb8e965be01
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-26 02:07:23 -0700

    Feature. build version. add @rollup/plugin-replace

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { visualizer } from 'rollup-plugin-visualizer';
 import url from '@rollup/plugin-url';
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,14 +11,17 @@ export default defineConfig({
         reactRefresh(),
 
         {
-            ...url(
-                    {
-                    include: ['**/*.svg'],
-                    limit: 15000,
-                }
-            ),
+            ...url({
+                include: ['**/*.svg'],
+                limit: 15000,
+            }),
             enforce: 'pre',
         },
+
+        replace({
+            __DATE__: new Date().toISOString(),
+        }),
+
         visualizer({
             filename: 'visualization.html',
             template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
