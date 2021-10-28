ff89e061cb9e868bad5d2a8aa89096b3c2a3512d
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-27 19:29:19 -0700

    WIP. file actions. add XML illegal characters escape

--- src/store/manifest/xml-to-js.ts
+++ src/store/manifest/xml-to-js.ts
@@ -3,6 +3,19 @@ import { parse, j2xParser } from 'fast-xml-parser';
 import { parseOptions } from './mani-io';
 import { fileDownload } from '../../utils/file-download';
 
+function escapeXml(unsafe: string) {
+    return unsafe.replace(/[<>&'"]/g, function(c: string): string {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+            default: return c;
+        }
+    });
+}
+
 export function convertToXml(fileUs: FileUs): { err: string; res?: undefined; } | { res: string; err?: undefined; } {
 
     //console.log('raw', fileUs.raw);
@@ -19,7 +32,7 @@ export function convertToXml(fileUs: FileUs): { err: string; res?: undefined; } 
         console.log('obj\n', obj);
         ((obj.manifest as Mani.Manifest).descriptor as any)._attributes.id += '<>><'; // <- fast-xml-parser can read it, but notepad complains on illegal characters.
 
-        xml = (new j2xParser({ ...parseOptions, format: true, indentBy: '\t' })).parse(obj);
+        xml = (new j2xParser({ ...parseOptions, format: true, indentBy: '\t', attrValueProcessor: escapeXml })).parse(obj);
         xml = `<?xml version="1.0" encoding="UTF-8"?>\n${xml}`;
         console.log('raw\n', xml);
 
