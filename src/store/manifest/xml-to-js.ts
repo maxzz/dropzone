9b154f67c9bed7787ef26ba50231c95461aa001b
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-27 18:29:15 -0700

    WIP. file actions. test for illigal characters

--- src/store/manifest/xml-to-js.ts
+++ src/store/manifest/xml-to-js.ts
@@ -16,14 +16,17 @@ export function convertToXml(fileUs: FileUs): { err: string; res?: undefined; } 
     let xml = '';
     try {
         const obj = parse(fileUs.raw, parseOptions); //console.log('%craw', 'color: green', JSON.stringify(obj, null, 4));
+        console.log('obj\n', obj);
+        ((obj.manifest as Mani.Manifest).descriptor as any)._attributes.id += '<>><'
+
         xml = (new j2xParser({ ...parseOptions, format: true, indentBy: '\t' })).parse(obj);
         xml = `<?xml version="1.0" encoding="UTF-8"?>\n${xml}`;
-        console.log('raw', xml);
+        console.log('raw\n', xml);
 
         fileDownload({ data: xml, filename: 'test.txt', mime: 'text/plain;charset=utf-8' });
 
     } catch (error) {
-
+        console.log({error});
     }
 
     return {
