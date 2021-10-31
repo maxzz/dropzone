79dd2a21da84a0508b55a3144257211c6bae8551
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-30 19:37:17 -0700

    WIP. edit-manifest. done w/ fields convertion

--- src/store/manifest/xml-to-js.ts
+++ src/store/manifest/xml-to-js.ts
@@ -15,12 +15,15 @@ function manifestToJsonForXml(mani: Mani.Manifest) {
         manifest: {}
     };
 
+    // 1. Customization
+
     if (mani.options) {
         let processes;
         let rest: any = {};
         for (const kv of Object.entries(mani.options) as Entries<Mani.Customization.Options>) {
             if (kv[0] === 'processes') {
-                processes = kv[1]?.length && kv[1].map((process) => ({ [attributes]: { ...process } }));
+                const maniProcesses: Mani.Customization.Process[] = kv[1];
+                processes = maniProcesses?.length && maniProcesses.map((process) => ({ [attributes]: { ...process } }));
             } else {
                 rest[kv[0]] = kv[1];
             }
@@ -31,24 +34,43 @@ function manifestToJsonForXml(mani: Mani.Manifest) {
         };
     }
 
+    // 2. Manifest descriptor
+
     if (!isEmptyObject(mani.descriptor)) {
         rv.manifest.descriptor = { [attributes]: { ...mani.descriptor } };
     }
+
+    // 3. Manifest forms
 
     if (mani.forms?.length) {
         rv.manifest.forms = {};
         rv.manifest.forms.form = mani.forms.map((form) => {
             let newForm: any = {};
+
+            // 3.1. Form detection
+            if (!isEmptyObject(form.fcontext)) {
+                newForm.fcontext = { [attributes]: { ...form.fcontext } };
+            }
+
+            // 3.2. Form detection
             if (!isEmptyObject(form.detection)) {
                 newForm.detection = { [attributes]: { ...form.detection } };
             }
+
+            // 3.3. Form options
             if (!isEmptyObject(form.options)) {
                 newForm.options = { [attributes]: { ...form.options } };
             }
+
+            // 3.4. Form fields
+            if (form.fields.length) {
+                newForm.fields = { field: form.fields.map((field) => ({ [attributes]: { ...field } })) };
+            }
+
             return newForm;
         });
+    }//3.
 
-    }
     return rv;
 }
 
@@ -66,15 +88,18 @@ export function convertToXml(fileUs: FileUs): { err: string; res?: undefined; } 
         // 2.
         let rv = fileUs.mani && manifestToJsonForXml(fileUs.mani) || '';
 
-        console.log('%c---------internal mani---------', 'color: green', `\n${JSON.stringify(fileUs.mani || 'undefiend', null, 4)}`);
-        console.log('%c---------js from xml---------', 'color: green', `\n${JSON.stringify(jsFromXml, null, 4)}`);
-        console.log('%c---------mani for Xml---------', 'color: yellow', `\n${JSON.stringify(rv, null, 4)}`);
+        // console.log('%c---------internal mani---------', 'color: green', `\n${JSON.stringify(fileUs.mani || 'undefiend', null, 4)}`);
+        // console.log('%c---------js from xml---------', 'color: green', `\n${JSON.stringify(jsFromXml, null, 4)}`);
+        // console.log('%c---------mani for xml---------', 'color: yellow', `\n${JSON.stringify(rv, null, 4)}`);
 
         // 3.
-        xml = (new J2xParser({ ...parseOptions, format: true, indentBy: '    ', })).parse(rv);
         // xml = (new J2xParser({ ...parseOptions, format: true, indentBy: '    ', })).parse(jsFromXml);
+        // xml = `<?xml version="1.0" encoding="UTF-8"?>\n${xml}`;
+        //console.log('%c---------new xml from---------', 'color: green', `\n${xml}`);
+
+        xml = (new J2xParser({ ...parseOptions, format: true, indentBy: '    ', })).parse(rv);
         xml = `<?xml version="1.0" encoding="UTF-8"?>\n${xml}`;
-        console.log('%c---------new xml---------', 'color: green', `\n${xml}`);
+        console.log('%c---------new xml from converted---------', 'color: green', `\n${xml}`);
 
         // 4.
         //fileDownload({ data: xml, filename: fileUs.fname, mime: 'text/plain;charset=utf-8' });
