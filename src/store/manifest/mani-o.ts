b733185f5db11e7bd21028a8d717d8711c98ed43
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-31 02:09:05 -0700

    WIP. edit-manifest. code clean up

--- src/store/manifest/mani-o.ts
+++ src/store/manifest/mani-o.ts
@@ -11,23 +11,6 @@ export function manifestToJsonForXml(mani: Mani.Manifest) {
 
     // 1. Customization
     if (mani.options) {
-        /*
-        let processes;
-        let rest: any = {};
-        for (const kv of Object.entries(mani.options) as Entries<Mani.Customization.Options>) {
-            if (kv[0] === 'processes') {
-                const maniProcesses: Mani.Customization.Process[] = kv[1];
-                processes = maniProcesses?.length && maniProcesses.map((process) => ({ [attributes]: { ...process } }));
-            } else {
-                rest[kv[0]] = kv[1];
-            }
-        }
-        rv.manifest.options = {
-            ...(processes && { processes: { process: processes } }),
-            ...rest,
-        };
-        */
-
         const { processes, ...rest } = mani.options;
         const xmlProcesses = processes?.length && processes.map((process) => ({ [attributes]: { ...process } }));
         rv.manifest.options = {
@@ -45,40 +28,14 @@ export function manifestToJsonForXml(mani: Mani.Manifest) {
     if (mani.forms?.length) {
         rv.manifest.forms = {};
         rv.manifest.forms.form = mani.forms.map((form) => {
-            //let newForm: any = {};
-
-            const { fcontext, detection, options, fields } = form;
-
-            let newForm: any = {
+            const { fcontext, detection, options, fields, ...rest } = form;
+            return {
                 ...(!isEmptyObject(fcontext) && { fcontext: { [attributes]: { ...form.fcontext } } }),
                 ...(!isEmptyObject(detection) && { detection: { [attributes]: { ...form.detection } } }),
                 ...(!isEmptyObject(options) && { options: { [attributes]: { ...form.options } } }),
                 ...(fields?.length && { fields: { field: form.fields.map((field) => ({ [attributes]: { ...field } })) } }),
+                ...rest,
             };
-
-            /*
-            // 3.1. Form detection
-            if (!isEmptyObject(form.fcontext)) {
-                newForm.fcontext = { [attributes]: { ...form.fcontext } };
-            }
-
-            // 3.2. Form detection
-            if (!isEmptyObject(form.detection)) {
-                newForm.detection = { [attributes]: { ...form.detection } };
-            }
-
-            // 3.3. Form options
-            if (!isEmptyObject(form.options)) {
-                newForm.options = { [attributes]: { ...form.options } };
-            }
-
-            // 3.4. Form fields
-            if (form.fields.length) {
-                newForm.fields = { field: form.fields.map((field) => ({ [attributes]: { ...field } })) };
-            }
-            */
-
-            return newForm;
         });
     }
 
