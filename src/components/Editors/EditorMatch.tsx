cc4e4d933118558b4efef93edadec0b785b0e2ef
Max Zakharzhevskiy <maxzz@msn.com>
2021-10-31 23:42:28 -0700

    WIP. edit matching. add simple dialog w/ style

--- src/components/Editors/EditorMatch.tsx
+++ src/components/Editors/EditorMatch.tsx
@@ -1,17 +1,58 @@
 import React from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { Cross1Icon } from '@radix-ui/react-icons';
+import { keyframes } from '@stitches/react';
+import { styled } from '../../stitches.config';
+import { blackA } from '@radix-ui/colors';
 
-export function Dialog({ children, ...props }: {children: React.ReactNode}) {
+const overlayShow = keyframes({
+    '0%': { opacity: 0 },
+    '100%': { opacity: 1 },
+});
+
+const contentShow = keyframes({
+    '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
+    '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+});
+
+const StyledOverlay = styled(DialogPrimitive.Overlay, {
+    backgroundColor: blackA.blackA9,
+    position: 'fixed',
+    inset: 0,
+    '@media (prefers-reduced-motion: no-preference)': {
+        animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+    },
+});
+
+const StyledContent = styled(DialogPrimitive.Content, {
+    backgroundColor: 'white',
+    borderRadius: 6,
+    boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '90vw',
+    maxWidth: '450px',
+    maxHeight: '85vh',
+    padding: 25,
+    '@media (prefers-reduced-motion: no-preference)': {
+        animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+        willChange: 'transform',
+    },
+    '&:focus': { outline: 'none' },
+});
+
+export function Dialog({ children, ...props }: { children: React.ReactNode; }) {
     return (
         <DialogPrimitive.Root {...props}>
-            <DialogPrimitive.Overlay />
+            <StyledOverlay />
             {children}
         </DialogPrimitive.Root>
     );
 }
 
-export const DialogContent = React.forwardRef<HTMLDivElement, {children: React.ReactNode}>(({ children, ...props }, forwardedRef) => (
+export const DialogContent = React.forwardRef<HTMLDivElement, { children: React.ReactNode; } | React.HTMLAttributes<HTMLDivElement>>(({ children, ...props }, forwardedRef) => (
     <DialogPrimitive.Content {...props} ref={forwardedRef}>
         {children}
         <DialogPrimitive.Close>
@@ -29,8 +70,15 @@ function EditorMatch() {
     return (
         <div>
             <Dialog>
-                <DialogTrigger>Dialog trigger</DialogTrigger>
-                <DialogContent>Dialog Content</DialogContent>
+                <DialogTrigger>
+                    <div className="bg-purple-400">Dialog trigger</div>
+                </DialogTrigger>
+                <StyledContent className="fixed inset-0 grid place-items-center">
+                    <div className="">
+                        <div className="bg-red-700">Caption</div>
+                        <div className="bg-red-500 ">Dialog Content</div>
+                    </div>
+                </StyledContent>
             </Dialog>
         </div>
     );
