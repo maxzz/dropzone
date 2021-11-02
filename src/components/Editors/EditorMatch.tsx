a8d1092400d9f98289a0bb32bc6c552603c3e8f8
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-01 17:56:48 -0700

    WIP. edit matching. update layout

--- src/components/Editors/EditorMatch.tsx
+++ src/components/Editors/EditorMatch.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { styled } from '../../stitches.config';
 import Modal from 'react-overlays/Modal';
 
 function EditorMatch() {
@@ -27,13 +26,18 @@ function EditorMatch() {
                 aria-labelledby="modal-label"
                 container={portalRef}
             >
-                <div className="py-4">
-                    <h4 className="px-2 py-2 font-bold">URL matching</h4>
+                <div className="py-4 text-sm">
+                    <h4 className="px-4 py-2 text-base font-bold">URL matching</h4>
 
-                    <div className="px-2 flex flex-col space-y-2">
-                        <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
-                        <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
-                        <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
+                    <div className="px-4">
+                        <div className="flex flex-col space-y-1">
+                            <div className="">Matching</div>
+                            <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
+                            <div className="">Quicklink</div>
+                            <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
+                            <div className="">Original</div>
+                            <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
+                        </div>
 
                         <div className="!mt-4 flex justify-end space-x-2">
                             <button className="px-4 py-2 min-w-[5rem] h-8 leading-4 text-gray-200 bg-gray-600 rounded" onClick={() => setShow(false)}>OK</button>
