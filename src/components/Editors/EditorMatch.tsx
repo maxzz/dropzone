e299823dcc952a9f50107d3b73d922ca9850a254
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-03 16:53:15 -0700

    WIP. edit matching. code clean up

--- src/components/Editors/EditorMatch.tsx
+++ src/components/Editors/EditorMatch.tsx
@@ -53,14 +53,12 @@ export function EditorMatch({ atom, setShow = (v: boolean) => { } }: { atom: num
     );
 }
 
+export const RenderBackdrop = (props: any) => <div className="fixed inset-0 z-[1040] bg-black opacity-40" {...props} />;
+
 function Dialog({ children, allowClickOutside }: { children: JSX.Element; allowClickOutside?: boolean; }) {
     const [show, setShow] = React.useState(false);
-
-    const renderBackdrop = (props: any) => <div {...props} className="fixed inset-0 z-[1040] bg-black opacity-40" />;
-
     const portalRef = React.useRef<HTMLElement | null>(null);
     React.useEffect(() => { portalRef.current = document.getElementById('portal'); }, []);
-
     return (
         <div className="modal-example">
             <button type="button" className="px-2 py-1 text-gray-200 bg-gray-600 rounded" onClick={() => setShow(true)}>
@@ -70,11 +68,9 @@ function Dialog({ children, allowClickOutside }: { children: JSX.Element; allowC
             <Modal
                 className="fixed w-[400px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[1040] bg-gray-200 rounded border border-gray-100 shadow"
                 show={show}
-                onHide={() => {
-                    allowClickOutside && setShow(false);
-                }}
-                onEscapeKeyDown={(e: KeyboardEvent) => setShow(false)}
-                renderBackdrop={renderBackdrop}
+                onHide={() => { allowClickOutside && setShow(false); }}
+                onEscapeKeyDown={() => setShow(false)}
+                renderBackdrop={RenderBackdrop}
                 aria-labelledby="modal-label"
                 container={portalRef}
             >
