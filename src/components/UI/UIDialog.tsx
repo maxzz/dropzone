5088e4b05d2b6e01d4b182f82a786ef6ca67cae4
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-03 17:42:52 -0700

    WIP. edit matching. add trigger prop to Dialog

--- src/components/UI/UIDialog.tsx
+++ src/components/UI/UIDialog.tsx
@@ -3,17 +3,37 @@ import Modal from 'react-overlays/Modal';
 
 export const RenderBackdrop = (props: any) => <div className="fixed inset-0 z-[1040] bg-black opacity-40" {...props} />;
 
-const className="fixed w-[400px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[1040] bg-gray-200 rounded border border-gray-100 shadow";
+const className = "fixed w-[400px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[1040] bg-gray-200 rounded border border-gray-100 shadow";
 
-function Dialog({ children, allowClickOutside }: { children: JSX.Element; allowClickOutside?: boolean; }) {
+function ButtonTrigger(props: React.HTMLAttributes<HTMLElement>) {
+    return (
+        <button type="button" className="px-2 py-1 text-gray-200 bg-gray-600 rounded" {...props}>
+            Open Modal
+        </button>
+    )
+}
+
+// <button type="button" className="px-2 py-1 text-gray-200 bg-gray-600 rounded" onClick={() => setShow(true)}>
+// Open Modal
+// </button>
+
+type DialogProps = {
+    children: JSX.Element;
+    allowClickOutside?: boolean;
+    trigger?: JSX.Element;
+};
+
+function Dialog({ children, allowClickOutside, trigger }: DialogProps) {
     const [show, setShow] = React.useState(false);
     const portalRef = React.useRef<HTMLElement | null>(null);
     React.useEffect(() => { portalRef.current = document.getElementById('portal'); }, []);
     return (
         <>
-            <button type="button" className="px-2 py-1 text-gray-200 bg-gray-600 rounded" onClick={() => setShow(true)}>
+            {trigger ? trigger : <ButtonTrigger onClick={() => setShow(true)} />}
+
+            {/* <button type="button" className="px-2 py-1 text-gray-200 bg-gray-600 rounded" onClick={() => setShow(true)}>
                 Open Modal
-            </button>
+            </button> */}
 
             {show && <Modal
                 className={className}
