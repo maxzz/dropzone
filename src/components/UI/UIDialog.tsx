d45cd56c00bb400244c0ca7804c4d98a08e53773
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-03 17:08:02 -0700

    WIP. edit matching. tialwind can handle external class name, but w/ only specific name

--- src/components/UI/UIDialog.tsx
+++ src/components/UI/UIDialog.tsx
@@ -3,18 +3,20 @@ import Modal from 'react-overlays/Modal';
 
 export const RenderBackdrop = (props: any) => <div className="fixed inset-0 z-[1040] bg-black opacity-40" {...props} />;
 
+const className="fixed w-[400px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[1040] bg-gray-200 rounded border border-gray-100 shadow";
+
 function Dialog({ children, allowClickOutside }: { children: JSX.Element; allowClickOutside?: boolean; }) {
     const [show, setShow] = React.useState(false);
     const portalRef = React.useRef<HTMLElement | null>(null);
     React.useEffect(() => { portalRef.current = document.getElementById('portal'); }, []);
     return (
-        <div className="modal-example">
+        <>
             <button type="button" className="px-2 py-1 text-gray-200 bg-gray-600 rounded" onClick={() => setShow(true)}>
                 Open Modal
             </button>
 
             <Modal
-                className="fixed w-[400px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[1040] bg-gray-200 rounded border border-gray-100 shadow"
+                className={className}
                 show={show}
                 onHide={() => { allowClickOutside && setShow(false); }}
                 onEscapeKeyDown={() => setShow(false)}
@@ -24,7 +26,7 @@ function Dialog({ children, allowClickOutside }: { children: JSX.Element; allowC
             >
                 {React.cloneElement(children, { setShow })}
             </Modal>
-        </div>
+        </>
     );
 }
 
