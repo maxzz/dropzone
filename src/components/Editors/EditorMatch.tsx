51162e5a12a6fb79cdec12c157dc938f60ad3e7f
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-04 00:32:52 -0700

    WIP. edit matching. checkbox options code clean up

--- src/components/Editors/EditorMatch.tsx
+++ src/components/Editors/EditorMatch.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-function MatchRow({ label, type = "radio", group, val = 0, checked }:
-    { label: string; type?: string; group?: string; val?: number; checked: boolean; }) {
+function RadioButton({ label, type = "radio", group, val = 0, checked }: { label: string; type?: string; group?: string; val?: number; checked: boolean; }) {
     return (
         <label className="h-6 flex items-center space-x-1">
             <input type={type} {...(group && { name: group })} value={val} defaultChecked={checked} />
@@ -10,15 +9,15 @@ function MatchRow({ label, type = "radio", group, val = 0, checked }:
     );
 }
 
-function MatchTo() {
+function RadioGroup() {
     const [value, setValue] = React.useState(3);
     return (
         <div className="flex flex-col" onChange={(v: React.ChangeEvent<HTMLInputElement>) => setValue(+v.target.value)}>
-            <MatchRow group={"how"} val={1} checked={value === 1} label="Do not match" />
-            <MatchRow group={"how"} val={2} checked={value === 2} label="String match" />
-            <MatchRow group={"how"} val={3} checked={value === 3} label="Wildcard match" />
-            <MatchRow group={"how"} val={4} checked={value === 4} label="Regular expresssion" />
-            <MatchRow group={"how"} val={5} checked={value === 5} label="No domain match" />
+            <RadioButton group={"how"} val={1} checked={value === 1} label="Do not match" />
+            <RadioButton group={"how"} val={2} checked={value === 2} label="String match" />
+            <RadioButton group={"how"} val={3} checked={value === 3} label="Wildcard match" />
+            <RadioButton group={"how"} val={4} checked={value === 4} label="Regular expresssion" />
+            <RadioButton group={"how"} val={5} checked={value === 5} label="No domain match" />
         </div>
     );
 }
@@ -26,9 +25,8 @@ function MatchTo() {
 export default function EditorMatch({ atom, setShow = (v: boolean) => { } }: { atom: number; setShow?: (v: boolean) => void; }) {
     const firstFocusRef = React.useRef<HTMLInputElement>(null);
     React.useEffect(() => { firstFocusRef.current?.focus(); }, []);
+
     const [checked, setChecked] = React.useState(true);
-    console.log({checked});
-    
     return (
         <div className="py-4 text-sm">
             <h4 className="px-4 py-2 text-base font-bold">URL matching</h4>
@@ -38,13 +36,12 @@ export default function EditorMatch({ atom, setShow = (v: boolean) => { } }: { a
                     <div className="">Matching URL</div>
                     <input ref={firstFocusRef} className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
 
-                    <MatchTo />
-                    <div className="flex flex-col" onChange={(v: React.ChangeEvent<HTMLInputElement>) => {
-                        setChecked(!!v.target.value)
-                        console.log({v: v.target.value});
-                    }}>
-                        <MatchRow label="Case sensitive" type="checkbox" checked={checked} />
-                    </div>
+                    <RadioGroup />
+
+                    <label className="h-6 flex items-center space-x-1">
+                        <input type="checkbox" checked={checked} onChange={(event) => setChecked(event.target.checked)} />
+                        <div>Case sensitive</div>
+                    </label>
 
                     <div className="">Quicklink URL</div>
                     <input className="px-2 py-1 w-full border border-gray-400 rounded shadow-inner" />
