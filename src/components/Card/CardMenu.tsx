d225253262e80ed51e2526970f0b0ea90cdda281
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-05 15:03:37 -0700

    WIP. edit matching. react-overlays. new try. simple

--- src/components/Card/CardMenu.tsx
+++ src/components/Card/CardMenu.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useDropdownMenu } from "react-overlays";
+
+const MenuItem = ({ children }: { children: React.ReactNode; }) => (
+    <div className="">
+        {children}
+    </div>
+);
+
+const NavMenu = () => {
+    const [props, meta] = useDropdownMenu();
+    console.log({ props, meta });
+
+    return (
+        <div className="w-32 inline-block bg-white border rounded shadow" role="menu" {...props}>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Docs</MenuItem>
+            <MenuItem>About</MenuItem>
+        </div>
+    );
+};
 
 function CardMenu() {
     return (
-        <div>
+        <div className="relative">
+            btn
+            <NavMenu />
         </div>
     );
 }
