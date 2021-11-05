77ccfa938de6f242922894c53081109083a3b05e
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-05 01:15:26 -0700

    WIP. edit matching. react-overlays/dropdown remove all attempts

--- src/components/Card/CardMenu.tsx
+++ src/components/Card/CardMenu.tsx
@@ -1,41 +1,8 @@
 import React from 'react';
-import Dropdown, { Placement } from '../UI/DropdownOverlay';
-
-const Menu = () => {
-    return (
-        <Dropdown.Menu>
-            <Dropdown.Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-                    1st menu item
-                </a>
-            </Dropdown.Menu.Item>
-        </Dropdown.Menu>
-    );
-};
-
-const placementArr: Placement[] = [
-    "up",
-    "upEnd",
-    "down",
-    "downEnd",
-    "left",
-    "leftEnd",
-    "right",
-    "rightEnd",
-];
 
 function CardMenu() {
     return (
         <div>
-            {placementArr.map((placement) => {
-                return (
-                    <Dropdown key={placement} overlay={<Menu />} placement={placement}>
-                        <button style={{ marginLeft: "100px", marginBottom: "10px" }} onClick={(e) => e.preventDefault()}>
-                            {placement}
-                        </button>
-                    </Dropdown>
-                );
-            })}
         </div>
     );
 }
