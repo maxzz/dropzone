7cdaeec253b2733e9661c40f463307abb8b99b1d
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-05 15:51:56 -0700

    WIP. edit matching. react-overlays. new try. second working simple test. fix typescript

--- src/components/Card/CardMenu.tsx
+++ src/components/Card/CardMenu.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Dropdown, useDropdownMenu, useDropdownToggle } from "react-overlays";
 import { DropdownProps } from 'react-overlays/cjs/Dropdown';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 
 const Menu = ({ role }: { role: string; }) => {
     const [props, { toggle = () => { }, show }] = useDropdownMenu({ flip: true, offset: [0, 8], });
     const display = show ? "flex" : "none";
     return (
         <div className={`${display} w-48 py-2 flex-col shadow-lg border-gray-200 bg-white z-10 rounded`} role={role} {...props}>
-            <button className="text-left hover:bg-brand-100 px-6 py-2" onClick={() => toggle(false)}>
+            <button className="text-left focus:bg-blue-300 hover:bg-gray-300 px-6 py-2" onClick={() => toggle(false)}>
                 Item 1
             </button>
-            <button className="text-left hover:bg-brand-100 px-6 py-2" onClick={() => toggle(false)}>
+            <button className="text-left focus:bg-blue-300 hover:bg-gray-300 px-6 py-2" onClick={() => toggle(false)}>
                 Item 2
             </button>
         </div>
@@ -21,6 +21,7 @@ const Menu = ({ role }: { role: string; }) => {
 const Toggle = ({ id, children }: { id: string, children: React.ReactNode; }) => {
     const [props, { show, toggle }] = useDropdownToggle();
     return (
+        // <button type="button" className="btn" {...props}>
         <button type="button" className="btn" id={id} {...props}>
             {children}
         </button>
@@ -34,32 +35,32 @@ const DropdownButton = ({
     alignEnd,
     title,
     role = 'menu',
-}: Omit<DropdownProps, 'children'> & { title: string; role?: string; }) => (
-    <Dropdown show={show} onToggle={onToggle} drop={drop} alignEnd={alignEnd} itemSelector="button:not(:disabled)">
+}: Partial<DropdownProps> & { title: string; role?: string; }) => (
+    <Dropdown show={show} onToggle={onToggle!} drop={drop} alignEnd={alignEnd} itemSelector="button:not(:disabled)">
         <span>
-            <Toggle id="example-toggle">{title}</Toggle>
+            <Toggle id={`example-toggle-${title}`}>{title}</Toggle>
             <Menu role={role} />
         </span>
     </Dropdown>
 );
 
-const ButtonToolbar = styled("div")`
-    & > * + * {
-      margin-left: 12px;
-    }
-  `;
+// const ButtonToolbar = styled("div")`
+//     & > * + * {
+//       margin-left: 12px;
+//     }
+//   `;
 
 function DropdownExample() {
     const [show, setShow] = React.useState(false);
 
     return (
-        <ButtonToolbar className="dropdown-example">
+        <div className="space-x-2">
             <DropdownButton show={show} onToggle={(nextShow) => setShow(nextShow)} title={`${show ? "Close" : "Open"} Dropdown`} />
             <DropdownButton alignEnd title="Align right" />
 
             <DropdownButton drop="up" title="Drop up" />
             <DropdownButton role="menu" title="Role 'menu'" />
-        </ButtonToolbar>
+        </div>
     );
 }
 
