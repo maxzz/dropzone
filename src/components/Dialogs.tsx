0486649d3cdc806c322699739a8e2b4e274d0cd6
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-06 18:53:52 -0700

    WIP. edit matching. remove transition so far

--- src/components/Dialogs.tsx
+++ src/components/Dialogs.tsx
@@ -3,7 +3,6 @@ import { useAtom } from 'jotai';
 import { detectionEditorAtom } from '../store/store';
 import { PortalModal } from './UI/UIDialog';
 import EditorMatch from './Editors/EditorMatch';
-import { Transition } from '@headlessui/react';
 
 function DetectionGroupEditor() {
     const [editorAtom, setEditorAtom] = useAtom(detectionEditorAtom);
@@ -11,21 +10,9 @@ function DetectionGroupEditor() {
     return (
         <>
             {show &&
-                        <PortalModal show={show} setShow={(v: boolean) => !v && setEditorAtom(undefined)}>
-                            <Transition
-                                show={show}
-                                enter="transition duration-[12000] ease-out"
-                                enterFrom="transform scale-[.5] opacity-0"
-                                enterTo="transform scale-25 opacity-100"
-                                leave="transition duration-1000 ease-out"
-                                leaveFrom="transform scale-100 opacity-100"
-                                leaveTo="transform scale-95 opacity-0"
-                            >
-                    {/* <div className="w-96 h-96"> */}
-                            <EditorMatch atom={editorAtom} />
-                    {/* </div> */}
-                </Transition>
-                        </PortalModal>
+                <PortalModal show={show} setShow={(v: boolean) => !v && setEditorAtom(undefined)}>
+                    <EditorMatch atom={editorAtom} />
+                </PortalModal>
             }
         </>
     );
