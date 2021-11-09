7a5c70eb494d362bfc34dd2e26ca883f7837d59c
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-09 01:51:39 -0800

    WIP. edit matching. add access to real data

--- src/components/Editors/EditorMatch.tsx
+++ src/components/Editors/EditorMatch.tsx
@@ -10,8 +10,6 @@ type RadioButtonProps = {
     checked: boolean;
 } & React.HTMLAttributes<HTMLLabelElement>;
 
-//background-image: url(data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3ccircle cx='8' cy='8' r='3'/%3e%3c/svg%3e);
-
 function RadioButton({ label, type = "radio", group, val = 0, checked, ...rest }: RadioButtonProps) {
     return (
         <label className="h-6 flex items-center space-x-1.5" {...rest}>
@@ -43,18 +41,24 @@ function RadioGroup() {
     );
 }
 
-export function MatchWeb({ setShow = (v: boolean) => { } }: { setShow?: (v: boolean) => void; }) {
-    const [editorAtom, setEditorAtom] = useAtom(formEditorDataAtom);
+export function MatchWeb() {
     const firstFocusRef = React.useRef<HTMLInputElement>(null);
     // React.useEffect(() => { firstFocusRef.current?.focus(); }, []);
 
     const [checked, setChecked] = React.useState(true);
+
+    const [editorData, setEditorData] = useAtom(formEditorDataAtom);
+    if (!editorData) {
+        return null;
+    }
+    const [fileUs, setFileUs] = useAtom(editorData.fileUsAtom);
+    const detection = fileUs.meta?.[editorData.formIdx]?.mani?.detection;
+
     return (
         <div className="p-4">
-
             <div className="flex flex-col">
                 <div className="mb-1">Website url to match</div>
-                <input ref={firstFocusRef} className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner" />
+                <input ref={firstFocusRef} className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner" value={detection?.web_murl} readOnly />
 
                 <RadioGroup />
 
@@ -66,21 +70,27 @@ export function MatchWeb({ setShow = (v: boolean) => { } }: { setShow?: (v: bool
                 <div className="mt-2 mb-4 w-full border-t border-gray-300" />
 
                 <div className="mt-2 mb-1">Website original url</div>
-                <input className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner" />
+                <input className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner" value={detection?.web_ourl} readOnly />
 
                 <div className="mt-2 mb-1">Quicklink url</div>
-                <input className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner" />
-
+                <input className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner" value={detection?.web_qurl} readOnly />
             </div>
         </div>
     );
 }
 
-export function MatchWindows({ setShow = (v: boolean) => { } }: { setShow?: (v: boolean) => void; }) {
+export function MatchWindows() {
     const [editorAtom, setEditorAtom] = useAtom(formEditorDataAtom);
+    if (!editorAtom) {
+        return null;
+    }
     return (
-        <div className="p-4">Windows match is comming soon...</div>
+        <div className="p-4">
+            Windows match is comming soon...
+        </div>
     );
 }
 
+//TODO: RadioButton: background-image: url(data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3ccircle cx='8' cy='8' r='3'/%3e%3c/svg%3e);
 //TODO: add checks for other urls: the same as original url
+//test on: {cd2a9c44-f588-4010-ba99-9d3e58bf69cb}.dpm
