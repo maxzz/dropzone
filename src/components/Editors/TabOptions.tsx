ef6812821e7a9c648939a34afcd6a2a075cd7ae6
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-15 14:46:40 -0800

    WIP. dialog move. update packages

--- src/components/Editors/TabOptions.tsx
+++ src/components/Editors/TabOptions.tsx
@@ -5,7 +5,20 @@ import { EditorData } from '../../store/store';
 export function TabOptions({ editorData }: { editorData: EditorData; }) {
     return (
         <div className="p-4">
-            Windows match is comming soon...
+            <div className="mb-1 font-bold text-gray-600">Original url</div>
+            <input
+                className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner"
+                spellCheck={false}
+                value={''} readOnly
+            />
+
+            <div className="mb-1 font-bold text-gray-600">Original url</div>
+            <input
+                className="px-2 py-1.5 w-full border border-gray-400 rounded shadow-inner"
+                spellCheck={false}
+                value={''} readOnly
+            />
+
         </div>
     );
 }
