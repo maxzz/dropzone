382cfe7b1b1af715256e82a70bbdc7a470afe9ec
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-18 00:31:24 -0800

    WIP. created and modified. file time formatting

--- src/store/manifest/mani-functions.ts
+++ src/store/manifest/mani-functions.ts
@@ -22,11 +22,12 @@ export function dpTimeToShow(fileTime?: number | string): string {
     const d = fileTimeToDate(fileTime).toLocaleString();
     const m = /^(\d\d??)\/(\d\d??)\/(\d\d\d\d), (\d\d??):(\d\d??):(\d\d?)([\s\S]*$)/.exec(d);
     if (m) {
-        m[1] = zeros(m[1], 2);
-        m[2] = zeros(m[2], 2);
-        m[4] = zeros(m[4], 2);
-        m[5] = zeros(m[5], 2);
-        m[6] = zeros(m[6], 2);
+        [1,2,4,5,6].forEach((idx) => m[idx] = zeros(m[idx], 2));
+        // m[1] = zeros(m[1], 2);
+        // m[2] = zeros(m[2], 2);
+        // m[4] = zeros(m[4], 2);
+        // m[5] = zeros(m[5], 2);
+        // m[6] = zeros(m[6], 2);
 
         console.log('d', d, '\nm', m);
     }
