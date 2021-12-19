faf15e2362c0d6e22ddd695bc7497ea96990609a
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-18 18:16:26 -0800

    Fix. make atomWithCallback update value callable

--- src/hooks/atomsX.ts
+++ src/hooks/atomsX.ts
@@ -1,11 +1,19 @@
-import { atom, Getter, Setter, WritableAtom } from 'jotai';
+import { atom, Getter, SetStateAction, Setter, WritableAtom } from 'jotai';
 
-export default function atomWithCallback<Value>(initialValue: Value, onValueChange: ({ get, set, nextValue }: { get: Getter, set: Setter, nextValue: Value; }) => void): WritableAtom<Value, Value> {
+type OnValueChange<Value> = ({ get, set, nextValue }: {
+    get: Getter;
+    set: Setter;
+    nextValue: Value;
+}) => void;
+
+export default function atomWithCallback<Value>(initialValue: Value, onValueChange: OnValueChange<Value>): WritableAtom<Value, SetStateAction<Value>> {
     const baseAtom = atom(initialValue);
-    const derivedAtom = atom<Value, Value>(
+    const derivedAtom = atom<Value, SetStateAction<Value>>(
         (get) => get(baseAtom),
-        (get, set, update) => {
-            const nextValue = typeof update === 'function' ? update(get(baseAtom)) : update;
+        (get, set, update: SetStateAction<Value>) => {
+            const nextValue = typeof update === 'function'
+                ? (update as (prev: Value) => Value)(get(baseAtom))
+                : update;
             set(baseAtom, nextValue);
             onValueChange({ get, set, nextValue });
         }
