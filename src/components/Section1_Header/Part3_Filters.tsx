9de1f6efa0da48e8743992511204b229a977f8ae
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-27 18:18:53 -0700

    WIP. Part3_Filters. add show animation with opacity and position

--- src/components/Section1_Header/Part3_Filters.tsx
+++ src/components/Section1_Header/Part3_Filters.tsx
@@ -4,6 +4,7 @@ import { hasFilesAtom, showMani, totalMani } from '@/store';
 import { LabeledSwitch } from '@ui/UiSwitch';
 import { Part2_FilterSearch } from './Part2_FilterSearch';
 import { classNames } from '@/utils/classnames';
+import { a, useSpring } from '@react-spring/web';
 
 function LabeWithNumber({ label, counterAtom }: { label: string; counterAtom: PrimitiveAtom<number>; }) {
     const total = useAtomValue(counterAtom);
@@ -31,15 +32,18 @@ function ManiFilter({ showAtom, counterAtom, label, title }: { showAtom: Primiti
 
 export function Part3_Filters() {
     const hasFiles = useAtomValue(hasFilesAtom);
+    const styles = useSpring({ opacity: hasFiles ? 1 : 0, x: hasFiles ? 0 : 300 });
     return (
-        <div className={classNames(
-            "flex-1 p-2 md:p-0 flex flex-col md:flex-row items-end justify-end md:items-center space-x-2 space-y-2 md:space-y-0 text-sm text-gray-200",
-            hasFiles ? "opacity-100" : "opacity-0",
-        )}>
-            <Part2_FilterSearch />
-            <ManiFilter showAtom={showMani.normalAtom} counterAtom={totalMani.normalAtom} label="Normal" title="Show normal mode manifests" />
-            <ManiFilter showAtom={showMani.manualAtom} counterAtom={totalMani.manualAtom} label="Manual" title="Show manual mode manifests" />
-            <ManiFilter showAtom={showMani.emptyAtom} counterAtom={totalMani.emptyAtom} label="Empty" title="Show excluded manifests" />
+        <div className="overflow-hidden">
+            <a.div style={styles} className={classNames(
+                "flex-1 p-2 md:p-0 flex flex-col md:flex-row items-end justify-end md:items-center space-x-2 space-y-2 md:space-y-0 text-sm text-gray-200",
+                //hasFiles ? "opacity-100" : "opacity-0",
+            )}>
+                <Part2_FilterSearch />
+                <ManiFilter showAtom={showMani.normalAtom} counterAtom={totalMani.normalAtom} label="Normal" title="Show normal mode manifests" />
+                <ManiFilter showAtom={showMani.manualAtom} counterAtom={totalMani.manualAtom} label="Manual" title="Show manual mode manifests" />
+                <ManiFilter showAtom={showMani.emptyAtom} counterAtom={totalMani.emptyAtom} label="Empty" title="Show excluded manifests" />
+            </a.div>
         </div>
     );
 }
