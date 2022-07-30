8be91547938d08494a48733f1671cc71525ac5f0
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-29 18:30:13 -0700

    WIP. app. provide solid icons for titles and the rest outlined

--- src/components/Section2_Main/Panel1_FilesList/Card/Part1_CardTitle/CardTitleIcon.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part1_CardTitle/CardTitleIcon.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { FileUsStats } from "@/store";
-import { IconAppWebChrome, IconAppWebIE, IconAppWindows, IconCatalog } from "@ui/UIIconSymbols";
+import { IconAppWebChrome, IconAppWebIE, IconAppWebIESolid, IconAppWindows, IconCatalog } from "@ui/UIIconSymbols";
 
 export function CardTitleIcon({ stats: { isWeb, isChrome, isFCat, isCustomization } }: { stats: FileUsStats; }) {
     if (isFCat) {
         return (
             <div className="w-6 h-6 flex items-center justify-center">
-                <IconCatalog className="w-5 h-5 text-primary-300" title="Field catalog" />
+                <IconCatalog className="w-5 h-5 fill-primary-300 stroke-primary-900" title="Field catalog" />
+                {/* <IconCatalog className="w-5 h-5 text-primary-300" title="Field catalog" /> */}
             </div>
         );
     }
@@ -14,11 +15,12 @@ export function CardTitleIcon({ stats: { isWeb, isChrome, isFCat, isCustomizatio
     const icon = isChrome
         ? <IconAppWebChrome className="w-5 h-5 text-primary-300" />
         : isWeb
-            ? <IconAppWebIE className="w-5 h-5 text-primary-300" />
-            : <IconAppWindows className="w-5 h-5 text-primary-300" />;
+            ? <IconAppWebIESolid className="w-5 h-5 text-primary-300" /> // good to make outline: fill-transparent stroke-slate-200
+            : <IconAppWindows className="w-5 h-5 fill-primary-300 stroke-transparent" />;
+            // : <IconAppWindows className="w-5 h-5 text-primary-300" />;
 
     const title = isChrome ? 'Webiste trained with Chrome' : isWeb ? 'Webiste trained with IE' : 'Windows application';
-    
+
     return (
         <div title={`${title} `}> {/* TODO: avoid this div; set size on icon */}
             {icon}
--- src/components/Section2_Main/Panel1_FilesList/Card/Part4_CardUI/UICardFormButton.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part4_CardUI/UICardFormButton.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { IconAutoMode, IconFormChangePsw, IconFormLogin } from '@ui/UIIcons';
-import { IconAppWebChrome, IconAppWebIE, IconAppWindows, IconManualMode } from '@ui/UIIconSymbols';
+import { IconAppWebChrome, IconAppWebIE, IconAppWebIESolid, IconAppWindows, IconManualMode } from '@ui/UIIconSymbols';
 
 const TagWinApp =     <IconAppWindows    key="TagWinApp"     title="Windows application"                  className="w-5 h-5 ml-2 opacity-75" />      ;
-const TagWebIe =      <IconAppWebIE      key="TagWebIe"      title="Webiste trained with IE"              className="w-5 h-5 ml-2" strokeWidth={.9} />;
+// const TagWebIe =      <IconAppWebIE      key="TagWebIe"      title="Webiste trained with IE"              className="w-4 h-4 ml-2" strokeWidth={.9} />;
+const TagWebIe =      <IconAppWebIESolid key="TagWebIe"      title="Webiste trained with IE"              className="w-5 h-5 ml-2 fill-transparent stroke-primary-900" />;
 const TagWeb =        <IconAppWebChrome  key="TagWeb"        title="Webiste trained with Chrome/Firefox"  className="w-5 h-5 ml-2" strokeWidth={.9} />;
 const TagModeNormal = <IconAutoMode      key="TagModeNormal" title="Normal mode"                          className="w-5 h-5 ml-2 opacity-75" />      ;
 const TagModeManual = <IconManualMode    key="TagModeManual" title="Manual mode"                          className="w-5 h-5 ml-2" strokeWidth={.9} />;
