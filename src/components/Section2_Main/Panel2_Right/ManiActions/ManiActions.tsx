39d7bdbd732ec7582cc3a38d41bf4701039b4429
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-30 19:35:07 -0700

    WIP. ManiActions. fix null values check

--- src/components/Section2_Main/Panel2_Right/ManiActions/ManiActions.tsx
+++ src/components/Section2_Main/Panel2_Right/ManiActions/ManiActions.tsx
@@ -8,18 +8,19 @@ import { CardTitleMenu } from "./ManiMenu";
 
 function CardOpenUrl({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     const fileUs = useAtomValue(fileUsAtom);
-    const url = fileUs.mani?.forms[0]?.detection.web_ourl;
-    const domain = fileUs.meta?.[0]?.disp.domain;
+    const url = fileUs.mani?.forms[0]?.detection?.web_ourl;
+    const domain = fileUs.meta?.[0]?.disp?.domain;
     return (<>
-        {url && <a
-            href={url}
-            target="_blank"
-            rel="noopener"
-            title={`Open ${domain}`}
-            onClick={(event) => event.stopPropagation()}
-        >
-            <IconOpenLink className="w-4 h-4" />
-        </a>
+        {url &&
+            <a
+                href={url}
+                target="_blank"
+                rel="noopener"
+                title={`Open ${domain}`}
+                onClick={(event) => event.stopPropagation()}
+            >
+                <IconOpenLink className="w-4 h-4" />
+            </a>
         }
     </>);
 }
@@ -29,11 +30,15 @@ export function ManiActions({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
         <div className="flex items-center space-x-1">
             <CardOpenUrl fileUsAtom={fileUsAtom} />
 
-            <CardTitleMenu fileUsAtom={fileUsAtom} icon={
-                <div className="opacity-60 hover:opacity-100 active:scale-[.97]">
-                    <IconMenuHamburger className="w-6 h-6" />
-                </div>
-            } />
+            <CardTitleMenu
+                fileUsAtom={fileUsAtom}
+                icon={
+                    <div className="opacity-60 hover:opacity-100 active:scale-[.97]">
+                        <IconMenuHamburger className="w-6 h-6" />
+                    </div>
+                }
+            />
+            
             {/* <PopoverMenu /> */}
             {/* <CardMenu /> */}
         </div>
