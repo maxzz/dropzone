ce74e4c87fbf006ba84e31a54f44d6e837febc89
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-01 15:52:45 -0700

    WIP. UICardFormButton. update app tags definition

--- src/components/Section2_Main/Panel1_FilesList/Card/Part4_CardUI/UICardFormButton.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part4_CardUI/UICardFormButton.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { IconAutoMode, IconFormChangePsw, IconFormLogin } from '@ui/UIIcons';
+import { IconAutoMode, IconBtnFormChangePsw, IconBtnFormLogin } from '@ui/UIIcons';
 import { IconAppWebChrome, IconAppWebIE, IconAppWebIESolid, IconAppWindows, IconManualMode } from '@ui/UIIconSymbols';
 
+/*
 const TagWinApp =     <IconAppWindows    key="TagWinApp"     title="Windows application"                  className="w-5 h-5 ml-2 opacity-75" />      ;
 // const TagWebIe =      <IconAppWebIE      key="TagWebIe"      title="Webiste trained with IE"              className="w-4 h-4 ml-2" strokeWidth={.9} />;
 const TagWebIe =      <IconAppWebIESolid key="TagWebIe"      title="Webiste trained with IE"              className="w-5 h-5 ml-2 fill-transparent stroke-primary-900" />;
 const TagWeb =        <IconAppWebChrome  key="TagWeb"        title="Webiste trained with Chrome/Firefox"  className="w-5 h-5 ml-2" strokeWidth={.9} />;
 const TagModeNormal = <IconAutoMode      key="TagModeNormal" title="Normal mode"                          className="w-5 h-5 ml-2 opacity-75" />      ;
 const TagModeManual = <IconManualMode    key="TagModeManual" title="Manual mode"                          className="w-5 h-5 ml-2" strokeWidth={.9} />;
-const TagLogin =      <IconFormLogin     key="TagLogin"      title="Login form"                           className="w-5 h-5 ml-2 opacity-75" />      ;
-const TagChgPsw =     <IconFormChangePsw key="TagChgPsw"     title="Password change form"                 className="w-5 h-5 ml-2 opacity-75" />      ;
+*/
+const TagLogin =      <IconBtnFormLogin     key="TagLogin"      title="Login form"                           className="w-5 h-5 ml-2 opacity-75" />      ;
+const TagChgPsw =     <IconBtnFormChangePsw key="TagChgPsw"     title="Password change form"                 className="w-5 h-5 ml-2 opacity-75" />      ;
+
+const tags = {
+    TagWinApp:
+        <IconAppWindows key="TagWinApp" title="Windows application" className="w-5 h-5 ml-2 opacity-75" />,
+    // TagWebIe:
+    //     <IconAppWebIE key="TagWebIe" title="Webiste trained with IE" className="w-4 h-4 ml-2" strokeWidth={.9} />,
+    TagWebIe:
+        <IconAppWebIESolid key="TagWebIe" title="Webiste trained with IE" className="w-5 h-5 ml-2 fill-transparent stroke-primary-900" />,
+    TagWeb:
+        <IconAppWebChrome key="TagWeb" title="Webiste trained with Chrome/Firefox" className="w-5 h-5 ml-2" strokeWidth={.9} />,
+    // TagModeNormal:
+    //     <IconAutoMode key="TagModeNormal" title="Normal mode" className="w-5 h-5 ml-2 opacity-75" />,
+    TagModeManual:
+        <IconManualMode key="TagModeManual" title="Manual mode" className="w-5 h-5 ml-2" strokeWidth={.9} />,
+    // TagLogin:
+    //     <IconBtnFormLogin key="TagLogin" title="Login form" className="w-5 h-5 ml-2 opacity-75" />,
+    // TagChgPsw:
+    //     <IconBtnFormChangePsw key="TagChgPsw" title="Password change form" className="w-5 h-5 ml-2 opacity-75" />,
+};
 
 type UICardFormButtonProps = {
     disp: Meta.Disp | undefined;
@@ -24,9 +45,9 @@ export function UICardFormButton({ disp, label, opened, onClick }: UICardFormBut
     const isWeb = !!disp?.domain;
 
     const icons = [
-        isWeb ? isIe ? TagWebIe : TagWeb : TagWinApp,
-        !isWeb && isIe && TagWebIe,
-        isScript && TagModeManual
+        isWeb ? isIe ? tags.TagWebIe : tags.TagWeb : tags.TagWinApp,
+        !isWeb && isIe && tags.TagWebIe,
+        isScript && tags.TagModeManual
     ];
 
     return (
@@ -36,6 +57,7 @@ export function UICardFormButton({ disp, label, opened, onClick }: UICardFormBut
         >
             <span className="">{label}</span>
             {icons}
+            {[TagLogin, TagChgPsw]}
         </button>
     );
 }
--- src/components/UI/UiIcons.tsx
+++ src/components/UI/UiIcons.tsx
@@ -104,7 +104,7 @@ export function IconAppWebIE({ title, ...rest }: SVGAttributes<SVGSVGElement> & 
 /**/
 
 
-export function IconFormLogin({ title, ...rest }: SVGAttributes<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
+export function IconBtnFormLogin({ title, ...rest }: SVGAttributes<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
     return (
         <svg viewBox="0 0 1025 1024" fill="currentColor" {...rest}>
             {title && <title>{title}</title>}
@@ -113,12 +113,11 @@ export function IconFormLogin({ title, ...rest }: SVGAttributes<SVGSVGElement> &
     );
 }
 
-export function IconFormChangePsw({ title, ...rest }: SVGAttributes<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
+export function IconBtnFormChangePsw({ title, ...rest }: SVGAttributes<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
     return (
         <svg viewBox="0 0 24 24" fill="currentColor" {...rest}>
             {title && <title>{title}</title>}
-            <path d="M17.66 9.53l-7.07 7.07l-4.24-4.24l1.41-1.41l2.83 2.83l5.66-5.66l1.41 1.41zM4 12c0-2.33 1.02-4.42 2.62-5.88L9 8.5v-6H3l2.2 2.2C3.24 6.52 2 9.11 2 12c0 5.19 3.95 9.45 9 9.95v-2.02c-3.94-.49-7-3.86-7-7.93zm18 0c0-5.19-3.95-9.45-9-9.95v2.02c3.94.49 7 3.86 7 7.93c0 2.33-1.02 4.42-2.62 5.88L15 15.5v6h6l-2.2-2.2c1.96-1.82 3.2-4.41 3.2-7.3z"
-            />
+            <path d="M17.66 9.53l-7.07 7.07l-4.24-4.24l1.41-1.41l2.83 2.83l5.66-5.66l1.41 1.41zM4 12c0-2.33 1.02-4.42 2.62-5.88L9 8.5v-6H3l2.2 2.2C3.24 6.52 2 9.11 2 12c0 5.19 3.95 9.45 9 9.95v-2.02c-3.94-.49-7-3.86-7-7.93zm18 0c0-5.19-3.95-9.45-9-9.95v2.02c3.94.49 7 3.86 7 7.93c0 2.33-1.02 4.42-2.62 5.88L15 15.5v6h6l-2.2-2.2c1.96-1.82 3.2-4.41 3.2-7.3z" />
         </svg>
     );
 }
