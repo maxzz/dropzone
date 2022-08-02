6ae52c41e686763a5b880014c647aaf7bf40a6fd
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-01 18:13:22 -0700

    WIP. Part1_CardTitle. React.memo version clean up

--- src/components/Section2_Main/Panel1_FilesList/Card/Part1_CardTitle/Part1_CardTitle.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part1_CardTitle/Part1_CardTitle.tsx
@@ -59,28 +59,3 @@ export function Part1_CardTitle({ fileUsAtom }: { fileUsAtom: FileUsAtomType; })
         </CardTitleSelect>
     );
 }
-
-//export const CardTitleTextMemo = React.memo(CardTitleText);
-
-// export function Part1_CardTitle({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
-//     const isCurrentCard = useAtomValue(useAtomValue(fileUsAtom).state.isCurrentAtom);
-//     const doSetCurrentCard = useSetAtom(doSetCurrentCardAtom);
-//     const setRightPanel = useSetAtom(rightPanelData.panelAtom);
-
-//     function toggleCardSelection() {
-//         doSetCurrentCard({ fileUsAtom, setCurrent: !isCurrentCard });
-//         setRightPanel(!isCurrentCard ? fileUsAtom : undefined);
-//     }
-
-//     return (
-//         <div
-//             className={classNames(
-//                 "relative p-2 text-primary-100 cursor-pointer select-none",
-//                 isCurrentCard ? "bg-blue-900" : "bg-primary-900",
-//             )}
-//             onClick={toggleCardSelection}
-//         >
-//             <CardTitleTextMemo fileUsAtom={fileUsAtom} />
-//         </div>
-//     );
-// }
