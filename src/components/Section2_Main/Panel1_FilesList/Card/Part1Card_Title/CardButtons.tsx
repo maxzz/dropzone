d4ed4f17a33282fe363b0dd22d508f5819a83bd5
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-02 17:35:50 -0700

    WIP. Part2Card_FormBody. clarify card buttons

--- src/components/Section2_Main/Panel1_FilesList/Card/Part1Card_Title/CardButtons.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part1Card_Title/CardButtons.tsx
@@ -1,11 +1,54 @@
-import React, { MouseEvent } from "react";
-import { FormIdx } from "@/store";
-import { UICardFormButton, UICardFormMediumButton } from "../Part4Card_UI/UICardFormButton";
+import React, { HTMLAttributes, MouseEvent } from "react";
 import { PrimitiveAtom, useAtom } from "jotai";
+import { FormIdx, formIdxName } from "@/store";
+import { classNames } from "@/utils/classnames";
+import { IconFormChange, IconFormLogin } from "@ui/UIIconSymbols";
+import { appBigIcons, appMediumIcons, dispToIcons } from "../Part4Card_UI/UICardFormButton";
+
+type UICardFormButtonProps = {
+    disp: Meta.Disp | undefined;
+    formIdx: FormIdx;
+    opened: boolean;
+};
+
+function UICardFormButton({ disp, formIdx, opened, ...rest }: UICardFormButtonProps & HTMLAttributes<HTMLButtonElement>) {
+    const icons = dispToIcons(disp, appBigIcons);
+    return (
+        <button
+            className={classNames("p-2 border border-primary-700 rounded flex items-center shadow-md active:scale-[.97] select-none", opened && 'bg-primary-800 text-primary-100')}
+            {...rest}
+        >
+            <span>{formIdxName(formIdx)}</span>
+            {icons}
+        </button>
+    );
+}
+
+function UICardFormMediumButton({ disp, formIdx, opened, ...rest }: UICardFormButtonProps & HTMLAttributes<HTMLButtonElement>) {
+    const icons = dispToIcons(disp, appMediumIcons);
+    return (
+        <button
+            className={classNames("p-0.5 border border-primary-700 rounded shadow-md active:scale-[.97] select-none", opened && 'bg-primary-800 text-primary-100')}
+            title={formIdxName(formIdx)}
+            {...rest}
+        >
+            <div className={classNames("p-px w-4 h-4", !opened && "bg-primary-700")}>
+                {formIdx === FormIdx.login
+                    ? <IconFormLogin className="w-full h-full" />
+                    : <IconFormChange className="w-full h-full" />
+                }
+            </div>
+
+            <div className="w-1 self-stretch border-r border-primary-700"></div>
+
+            <div className="-mt-1 ml-2">{icons}</div>
+        </button>
+    );
+}
 
 export function CardNormalButtons({ hasLogin, hasCpass, disp, openAtom }: { hasLogin: boolean; hasCpass: boolean; disp: Array<Meta.Disp | undefined>; openAtom: PrimitiveAtom<boolean>; }) {
     const [open, setOpen] = useAtom(openAtom);
-    const toogleOpen = () => setOpen((v) => !v);
+    const toogleOpen = (event: MouseEvent) => { event.stopPropagation(); setOpen((v) => !v); };
     return (
         <div className="py-2 flex items-center space-x-2 text-sm">
             {hasLogin && <UICardFormButton disp={disp[0]} opened={open} onClick={toogleOpen} formIdx={FormIdx.login} />}
--- src/components/Section2_Main/Panel1_FilesList/Card/Part4Card_UI/UICardFormButton.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part4Card_UI/UICardFormButton.tsx
@@ -1,7 +1,5 @@
-import React, { HTMLAttributes } from 'react';
-import { IconAppWebChrome as IconWebCho, IconAppWebIESolid as IconWebIe6, IconAppWindows as IconWinApp, IconFormChange, IconFormLogin, IconManualMode as IconManual } from '@ui/UIIconSymbols';
-import { FormIdx, formIdxName } from '@/store';
-import { classNames } from '@/utils/classnames';
+import React from 'react';
+import { IconAppWebChrome as IconWebCho, IconAppWebIESolid as IconWebIe6, IconAppWindows as IconWinApp, IconManualMode as IconManual } from '@ui/UIIconSymbols';
 
 const tips = {
     winApp: "Windows application",
@@ -10,21 +8,21 @@ const tips = {
     manual: "Manual mode",
 };
 
-const bigTags = {
+export const appBigIcons = {
     winApp: <IconWinApp key="winApp" title={tips.winApp} className="w-5 h-5 ml-2 opacity-75" />,
     webIe6: <IconWebIe6 key="webIe6" title={tips.webIe6} className="w-5 h-5 ml-2 fill-transparent stroke-current" />,
     webCho: <IconWebCho key="webCho" title={tips.webCho} className="w-5 h-5 ml-2" strokeWidth={.9} />,
     manual: <IconManual key="manual" title={tips.manual} className="w-5 h-5 ml-2" strokeWidth={.9} />,
 };
 
-const mediumTags = {
+export const appMediumIcons = {
     winApp: <IconWinApp key="winApp" title={tips.winApp} className="w-5 h-5 ml-2 opacity-75" />,
     webIe6: <IconWebIe6 key="webIe6" title={tips.webIe6} className="w-5 h-5 ml-2 fill-transparent stroke-current" />,
     webCho: <IconWebCho key="webCho" title={tips.webCho} className="w-5 h-5 ml-2" strokeWidth={.9} />,
     manual: <IconManual key="manual" title={tips.manual} className="w-5 h-5 ml-2" strokeWidth={.9} />,
 };
 
-function dispToIcons(disp: Meta.Disp | undefined, tags: Record<string, JSX.Element>): (false | JSX.Element | undefined)[] {
+export function dispToIcons(disp: Meta.Disp | undefined, tags: Record<string, JSX.Element>): (false | JSX.Element | undefined)[] {
     const isIe = disp?.isIe;
     const isScript = disp?.isScript;
     const isWeb = !!disp?.domain;
@@ -33,45 +31,4 @@ function dispToIcons(disp: Meta.Disp | undefined, tags: Record<string, JSX.Eleme
         !isWeb && isIe && tags.webIe6,
         isScript && tags.manual
     ];
-}
-
-type UICardFormButtonProps = {
-    disp: Meta.Disp | undefined;
-    formIdx: FormIdx;
-    opened: boolean;
-};
-
-export function UICardFormButton({ disp, formIdx, opened, ...rest }: UICardFormButtonProps & HTMLAttributes<HTMLButtonElement>) {
-    const icons = dispToIcons(disp, bigTags);
-    return (
-        <button
-            className={classNames("p-2 border border-primary-700 rounded flex items-center shadow-md active:scale-[.97] select-none", opened && 'bg-primary-800 text-primary-100')}
-            {...rest}
-        >
-            <span>{formIdxName(formIdx)}</span>
-            {icons}
-        </button>
-    );
-}
-
-export function UICardFormMediumButton({ disp, formIdx, opened, ...rest }: UICardFormButtonProps & HTMLAttributes<HTMLButtonElement>) {
-    const icons = dispToIcons(disp, mediumTags);
-    return (
-        <button
-            className={classNames("p-0.5 border border-primary-700 rounded shadow-md active:scale-[.97] select-none", opened && 'bg-primary-800 text-primary-100')}
-            title={formIdxName(formIdx)}
-            {...rest}
-        >
-            <div className={classNames("p-px w-4 h-4", !opened && "bg-primary-700")}>
-                {formIdx === FormIdx.login
-                    ? <IconFormLogin className="w-full h-full" />
-                    : <IconFormChange className="w-full h-full" />
-                }
-            </div>
-
-            <div className="w-1 self-stretch border-r border-primary-700"></div>
-
-            <div className="-mt-1 ml-2">{icons}</div>
-        </button>
-    );
 }
