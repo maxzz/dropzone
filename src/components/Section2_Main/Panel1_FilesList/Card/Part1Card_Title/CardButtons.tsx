722c4cece6c190612ee6c07efbfa2340f0af9249
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-04 02:39:13 -0700

    WIP. CardButtons. update CardNormalButtons

--- src/components/Section2_Main/Panel1_FilesList/Card/Part1Card_Title/CardButtons.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part1Card_Title/CardButtons.tsx
@@ -1,41 +1,54 @@
-import React, { Fragment, HTMLAttributes, MouseEvent, ReactNode } from "react";
+import React, { Fragment, HTMLAttributes, MouseEvent } from "react";
 import { PrimitiveAtom, useAtom, useAtomValue } from "jotai";
 import { FormIdx, formIdxName, UISize, uiSizeAtom } from "@/store";
-import { classNames } from "@/utils/classnames";
 import { IconFormChange, IconFormLogin } from "@ui/UIIconSymbols";
 import { appBigIcons, appMediumIcons, ButtonsDisp, dispToIcons } from "../Part4Card_UI/UICardFormButtonTypes";
+import { classNames } from "@/utils/classnames";
 
 type UICardFormButtonProps = {
     disp: Meta.Disp | undefined;
     formIdx: FormIdx;
-    opened: boolean;
+    open: boolean;
 };
 
-function UICardFormButton({ disp, formIdx, opened, ...rest }: UICardFormButtonProps & HTMLAttributes<HTMLButtonElement>) {
-    const icons = dispToIcons(disp, appBigIcons);
-    return (
-        <button
-            className={classNames(
-                "p-2 border border-primary-700 rounded flex items-center shadow-md active:scale-[.97] select-none",
-                opened && 'bg-primary-800 text-primary-100'
-            )}
-            {...rest}
-        >
-            <span>{formIdxName(formIdx)}</span>
-            {icons}
-        </button>
-    );
-}
+// function UICardFormButton({ disp, formIdx, open, ...rest }: UICardFormButtonProps & HTMLAttributes<HTMLButtonElement>) {
+//     const icons = dispToIcons(disp, appBigIcons);
+//     return (
+//         <button
+//             className={classNames(
+//                 "p-2 border border-primary-700 rounded flex items-center shadow-md active:scale-[.97] select-none",
+//                 open && 'bg-primary-800 text-primary-100'
+//             )}
+//             {...rest}
+//         >
+//             <span>{formIdxName(formIdx)}</span>
+//             {icons}
+//         </button>
+//     );
+// }
 
 export function CardNormalButtons({ buttonsDisp, openAtom }: { buttonsDisp: ButtonsDisp; openAtom: PrimitiveAtom<boolean>; }) {
     const [open, setOpen] = useAtom(openAtom);
+    const icons = buttonsDisp.map(([_, disp]) => dispToIcons(disp, appBigIcons));
+
     const toogleOpen = (event: MouseEvent) => { event.stopPropagation(); setOpen((v) => !v); };
     return (
         <div className="py-2 flex items-center space-x-2 text-sm">
             {buttonsDisp.map(([hasForm, disp], idx) => (
                 <Fragment key={idx}>
                     {hasForm &&
-                        <UICardFormButton disp={disp} opened={open} onClick={toogleOpen} formIdx={idx} />
+                        // <UICardFormButton disp={disp} open={open} onClick={toogleOpen} formIdx={idx} />
+
+                        <button
+                            className={classNames(
+                                "p-2 border border-primary-700 rounded flex items-center shadow-md active:scale-[.97] select-none",
+                                open && 'bg-primary-800 text-primary-100'
+                            )}
+                        >
+                            <span>{formIdxName(idx)}</span>
+                            {icons[idx]}
+                        </button>
+
                     }
                 </Fragment>
             ))}
@@ -43,12 +56,32 @@ export function CardNormalButtons({ buttonsDisp, openAtom }: { buttonsDisp: Butt
     );
 }
 
+/*
+export function CardNormalButtons({ buttonsDisp, openAtom }: { buttonsDisp: ButtonsDisp; openAtom: PrimitiveAtom<boolean>; }) {
+    const [open, setOpen] = useAtom(openAtom);
+    const icons = buttonsDisp.map(([_, disp]) => dispToIcons(disp, appMediumIcons));
+    
+    const toogleOpen = (event: MouseEvent) => { event.stopPropagation(); setOpen((v) => !v); };
+    return (
+        <div className="py-2 flex items-center space-x-2 text-sm">
+            {buttonsDisp.map(([hasForm, disp], idx) => (
+                <Fragment key={idx}>
+                    {hasForm &&
+                        <UICardFormButton disp={disp} open={open} onClick={toogleOpen} formIdx={idx} />
+                    }
+                </Fragment>
+            ))}
+        </div>
+    );
+}
+*/
+
 export function CardMediumButtons({ buttonsDisp, openAtom }: { buttonsDisp: ButtonsDisp; openAtom: PrimitiveAtom<boolean>; }) {
     const minimal = useAtomValue(uiSizeAtom) === UISize.minimal;
     const [open, setOpen] = useAtom(openAtom);
     const icons = buttonsDisp.map(([_, disp]) => dispToIcons(disp, appMediumIcons));
     const prefixClass = { className: minimal ? "self-start w-2 h-2" : "self-start w-3 h-3" };
-    const prefix = (idx: FormIdx) => <>{idx === FormIdx.login ? IconFormLogin(prefixClass) : IconFormChange(prefixClass)}</>;
+    const prefix = (idx: FormIdx) => idx === FormIdx.login ? IconFormLogin(prefixClass) : IconFormChange(prefixClass);
     return (
         <button
             className={classNames(
