2766eb90456c4d76f3e7f34c164e57f913f6d00f
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-04 15:48:49 -0700

    WIP. FieldRow. switch FormRowTypeIcon to direct component call. next step

--- src/components/Section2_Main/Panel1_FilesList/Card/Part2Card_FormBody/Part2Form_Fields/FieldRowTypeIcon.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part2Card_FormBody/Part2Form_Fields/FieldRowTypeIcon.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { IconFieldBtn, IconFieldTxt, IconFieldChk, IconFieldLst, IconFieldPsw, IconFieldEdt } from '@ui/UIIconSymbols';
 
 export function FormRowTypeIcon({ field, className }: { field: Mani.Field; className?: string; }) {
-    const hint = `Field type: ${field.type}`;
-    const props = { className, title: field.type !== "list" ? hint : `Field choices: ${field.choosevalue}` };
+    const type = field.type as keyof typeof icons;
+    const hint = `Field type: ${type}`;
+    const props = { className, title: type !== "list" ? hint : `Field choices: ${field.choosevalue}` };
     const icons = {
         "edit": field.password ? IconFieldPsw : IconFieldEdt,
+        "check": IconFieldChk,
+        "list": IconFieldLst,
+        "combo": IconFieldLst,
+        "text": IconFieldTxt,
+        "button": IconFieldBtn,
     }
+    const Icon = icons[type](props);
     return (<>
-        {icons["edit"](props)}
-        {/* {field.type === "edit" && (field.password ? <>{IconFieldPsw(props)}</> : <>{IconFieldEdt(props)}</>)}
-        {field.type === "check" && <>{IconFieldChk(props)}</>}
-        {(field.type === "list" || field.type === "combo") && <IconFieldLst {...props} />}
-        {field.type === "text" && <IconFieldTxt className={className} title={hint} />}
-        {field.type === "button" && <IconFieldBtn className={className} title={hint} />} */}
+        {Icon}
     </>);
 }
