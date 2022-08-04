497aca7ca0d446163fc52b5e1aef8e3666c87b6c
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-04 15:39:05 -0700

    WIP. FieldRow. switch FormRowTypeIcon to direct component call. next step

--- src/components/Section2_Main/Panel1_FilesList/Card/Part2Card_FormBody/Part2Form_Fields/FieldRowTypeIcon.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part2Card_FormBody/Part2Form_Fields/FieldRowTypeIcon.tsx
@@ -4,11 +4,15 @@ import { IconFieldBtn, IconFieldTxt, IconFieldChk, IconFieldLst, IconFieldPsw, I
 export function FormRowTypeIcon({ field, className }: { field: Mani.Field; className?: string; }) {
     const hint = `Field type: ${field.type}`;
     const props = { className, title: field.type !== "list" ? hint : `Field choices: ${field.choosevalue}` };
+    const icons = {
+        "edit": field.password ? IconFieldPsw : IconFieldEdt,
+    }
     return (<>
-        {field.type === "edit" && (field.password ? <>{IconFieldPsw(props)}</> : <>{IconFieldEdt(props)}</>)}
+        {icons["edit"](props)}
+        {/* {field.type === "edit" && (field.password ? <>{IconFieldPsw(props)}</> : <>{IconFieldEdt(props)}</>)}
         {field.type === "check" && <>{IconFieldChk(props)}</>}
         {(field.type === "list" || field.type === "combo") && <IconFieldLst {...props} />}
         {field.type === "text" && <IconFieldTxt className={className} title={hint} />}
-        {field.type === "button" && <IconFieldBtn className={className} title={hint} />}
+        {field.type === "button" && <IconFieldBtn className={className} title={hint} />} */}
     </>);
 }
