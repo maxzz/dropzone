d813d2c47681a8dec516cc93c9b7d1e685072bcb
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-05 00:06:28 -0700

    WIP. CardTitleIcon. re-work generation

--- src/components/Section2_Main/Panel1_FilesList/Card/Part1Card_Title/CardTitleIcon.tsx
+++ src/components/Section2_Main/Panel1_FilesList/Card/Part1Card_Title/CardTitleIcon.tsx
@@ -3,26 +3,40 @@ import { FileUsStats } from "@/store";
 import { IconAppWebChrome, IconAppWebIE, IconAppWebIESolid, IconAppWindows, IconCatalog } from "@ui/UIIconSymbols";
 
 export function CardTitleIcon({ stats: { isWeb, isChrome, isFCat, isCustomization } }: { stats: FileUsStats; }) {
-    if (isFCat) {
-        return (
-            <div className="w-6 h-6 flex items-center justify-center">
-                <IconCatalog className="w-5 h-5 fill-primary-300 stroke-primary-900" title="Field catalog" />
-                {/* <IconCatalog className="w-5 h-5 text-primary-300" title="Field catalog" /> */}
-            </div>
-        );
-    }
+    const title =
+        isWeb ? 'Webiste trained with IE'
+            : isChrome ? 'Webiste trained with Chrome'
+                : isFCat ? "Field catalog"
+                    : 'Windows application';
 
-    const title = isChrome ? 'Webiste trained with Chrome' : isWeb ? 'Webiste trained with IE' : 'Windows application';
+    const Icon =
+        isWeb ? <IconAppWebIESolid className="w-5 h-5 text-primary-300" title={`${title}`} />
+            : isChrome ? <IconAppWebChrome className="w-5 h-5 text-primary-300" title={`${title}`} />
+                : isFCat ? <IconCatalog className="w-5 h-5 fill-primary-300 stroke-primary-900" title="Field catalog" />
+                    : <IconAppWindows className="w-5 h-5 fill-primary-300 stroke-transparent" title={`${title}`} />;
 
-    const icon = isChrome
-        ? <IconAppWebChrome className="w-5 h-5 text-primary-300" title={`${title}`} />
-        : isWeb
-            ? <IconAppWebIESolid className="w-5 h-5 text-primary-300" title={`${title}`} /> // good to make outline: fill-transparent stroke-slate-200
-            : <IconAppWindows className="w-5 h-5 fill-primary-300 stroke-transparent" title={`${title}`} />;
-    // : <IconAppWindows className="w-5 h-5 text-primary-300" title={`${title}`} />;
-
-    return (<>
-        {icon}
-    </>);
+    return Icon;
 }
 
+// export function CardTitleIcon({ stats: { isWeb, isChrome, isFCat, isCustomization } }: { stats: FileUsStats; }) {
+//     if (isFCat) {
+//         return (
+//             <div className="w-6 h-6 flex items-center justify-center">
+//                 <IconCatalog className="w-5 h-5 fill-primary-300 stroke-primary-900" title="Field catalog" />
+//                 {/* <IconCatalog className="w-5 h-5 text-primary-300" title="Field catalog" /> */}
+//             </div>
+//         );
+//     }
+
+//     const title = isChrome ? 'Webiste trained with Chrome' : isWeb ? 'Webiste trained with IE' : 'Windows application';
+
+//     const Icon = isChrome
+//         ? <IconAppWebChrome className="w-5 h-5 text-primary-300" title={`${title}`} />
+//         : isWeb
+//             ? <IconAppWebIESolid className="w-5 h-5 text-primary-300" title={`${title}`} />
+//             : <IconAppWindows className="w-5 h-5 fill-primary-300 stroke-transparent" title={`${title}`} />;
+
+//     return Icon;
+// }
+
+//TODO: move (<IconAppWebIESolid, and ...) to functions instead of components
