88816ec3c49333675e42a2975a73ff30f63ff9de
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-12 16:09:01 -0700

    WIP. ManiActions. rework selected manifest actions

--- src/components/Section2_Main/Panel2_Right/ManiActions/ManiActions.tsx
+++ src/components/Section2_Main/Panel2_Right/ManiActions/ManiActions.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { useAtomValue } from "jotai";
-import { FileUsAtomType } from "@/store";
-import { IconMenuHamburger, IconOpenLink } from "@ui/UIIconSymbols";
+import { useAtomValue, useSetAtom } from "jotai";
+import { FileUsAtomType, formEditorDataAtom } from "@/store";
+import { IconGear, IconMenuHamburger, IconOpenLink } from "@ui/UIIconSymbols";
 import { CardTitleMenu } from "./ManiMenu";
 //import { PopoverMenu } from '@ui/nun/UIDropdownMenuLaag';
 //import { CardMenu } from './nun/CardMenu';
@@ -18,6 +18,7 @@ function CardOpenUrl({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
                 rel="noopener"
                 title={`Open ${domain}`}
                 onClick={(event) => event.stopPropagation()}
+                className="p-1 w-8 h-8 hover:bg-primary-700 rounded active:scale-[.97] flex items-center justify-center outline-none focus:ring-1 ring-primary-400"
             >
                 <IconOpenLink className="w-4 h-4" />
             </a>
@@ -25,20 +26,32 @@ function CardOpenUrl({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     </>);
 }
 
+export function CardEdit({ fileUsAtom, formIdx }: { fileUsAtom: FileUsAtomType; formIdx: number; }) {
+    const setFormEditorData = useSetAtom(formEditorDataAtom);
+    return (
+        <button
+            className={`p-1 w-8 h-8 hover:bg-primary-700 rounded active:scale-[.97] flex items-center justify-center outline-none focus:ring-1 ring-primary-400`}
+            onClick={() => setFormEditorData({ fileUsAtom, formIdx: formIdx })}
+            title="Edit detection options"
+        >
+            <IconGear className="w-4 h-4 stroke-[0.7] hover:stroke-[1]" />
+        </button>
+    );
+}
+
 export function ManiActions({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     return (
-        <div className="flex items-center space-x-1">
+        <div className="flex items-center">
+            <CardEdit fileUsAtom={fileUsAtom} formIdx={0} />
             <CardOpenUrl fileUsAtom={fileUsAtom} />
 
             <CardTitleMenu
                 fileUsAtom={fileUsAtom}
                 icon={
-                    <div className="opacity-60 hover:opacity-100 active:scale-[.97]">
-                        <IconMenuHamburger className="w-6 h-6" />
-                    </div>
+                    <IconMenuHamburger className="w-8 h-8 p-1 stroke-[0.8] hover:bg-primary-700 rounded opacity-60 hover:opacity-100 active:scale-[.97] outline-none focus:ring-1 ring-primary-400" />
                 }
             />
-            
+
             {/* <PopoverMenu /> */}
             {/* <CardMenu /> */}
         </div>
