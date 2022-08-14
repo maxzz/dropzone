81dad7d5a2a67fcd9eacedd6691edc8a4413cd3c
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-14 02:01:14 -0700

    WIP. Panel2_Right. switch SyntaxHighlighterXml to classes vs. inline styles

--- src/components/UI/Highlight/SyntaxHighlighter.tsx
+++ src/components/UI/Highlight/SyntaxHighlighter.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-//import { Light as SyntaxHighlighter, SyntaxHighlighterProps } from 'react-syntax-highlighter';
 import { SyntaxHighlighterProps } from 'react-syntax-highlighter';
 import { default as SyntaxHighlighter } from 'react-syntax-highlighter/dist/esm/light';
 import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
-//import theme from './highlight-vs2015';
-import theme from './highlight-night-owl';
+//import theme from './highlight-night-owl';
+import './highlight-inline.css';
 
 SyntaxHighlighter.registerLanguage('xml', xml); /* cannot use lazy load */
 
-export function SyntaxHighlighterXml({children, ...rest}: SyntaxHighlighterProps) {
+export function SyntaxHighlighterXml({ children, ...rest }: SyntaxHighlighterProps) {
     return (
-        <SyntaxHighlighter language='xml' style={theme} {...rest}>
+        // <SyntaxHighlighter language='xml' style={theme} {...rest}>
+        <SyntaxHighlighter language='xml' useInlineStyles={false} {...rest}>
             {children}
         </SyntaxHighlighter>
     );
