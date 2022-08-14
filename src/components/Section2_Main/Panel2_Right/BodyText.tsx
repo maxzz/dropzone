a34a9c945b0686a26e07be32552ef5a1dd74cf92
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-14 01:35:37 -0700

    WIP. Panel2_Right. optimize SyntaxHighlighterXml dependencies loading

--- src/components/Section2_Main/Panel2_Right/BodyText.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyText.tsx
@@ -15,7 +15,9 @@ export default function BodyText({ text }: { text: string; }) { /*lazy load*/
 
         <UISemiScrollbar className={`px-2 pt-1 pb-4 overflow-auto w-full h-full text-xs text-primary-100 bg-[#011627] opacity-60 cursor-default`}>
             <div className="font-mono">
-                <SyntaxHighlighterXml text={text} />
+                <SyntaxHighlighterXml>
+                    {text}
+                </SyntaxHighlighterXml>
             </div>
         </UISemiScrollbar>
     </>);
--- src/components/UI/Highlight/SyntaxHighlighter.tsx
+++ src/components/UI/Highlight/SyntaxHighlighter.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+//import { Light as SyntaxHighlighter, SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import { SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import { default as SyntaxHighlighter } from 'react-syntax-highlighter/dist/esm/light';
 import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
 //import theme from './highlight-vs2015';
 import theme from './highlight-night-owl';
 
 SyntaxHighlighter.registerLanguage('xml', xml); /* cannot use lazy load */
 
-export function SyntaxHighlighterXml({text}: {text: string}) {
+export function SyntaxHighlighterXml({children, ...rest}: SyntaxHighlighterProps) {
     return (
-        <SyntaxHighlighter language='xml' style={theme}>
-            {text}
+        <SyntaxHighlighter language='xml' style={theme} {...rest}>
+            {children}
         </SyntaxHighlighter>
     );
 }
