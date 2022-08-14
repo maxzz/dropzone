8e1339c95a31c48748cb80e3e977ccad82760968
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-13 18:44:06 -0700

    WIP. Panel2_Right. switch to highlight.js from prism.js

--- src/components/Section2_Main/Panel2_Right/BodyText.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyText.tsx
@@ -1,8 +1,13 @@
 import { UISemiScrollbar } from '@ui/UISemiScrollbar';
 import React from 'react';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import xml from 'react-syntax-highlighter/dist/esm/languages/prism/xml-doc';
-import st from 'react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus';
+
+// import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import xml from 'react-syntax-highlighter/dist/esm/languages/prism/xml-doc';
+// import st from 'react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus';
+
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
+import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco';
 
 SyntaxHighlighter.registerLanguage('xml-doc', xml); /* cannot use lazy load */
 
@@ -19,7 +24,7 @@ export default function BodyText({ text }: { text: string; }) { /*lazy load*/
         <UISemiScrollbar className={`px-2 pt-1 pb-4 overflow-auto w-full h-full text-xs text-primary-100 bg-primary-800 opacity-50 cursor-default`}>
             {/* <div className="font-mono whitespace-pre"> */}
             <div className="font-mono">
-                <SyntaxHighlighter language='xml' style={st}>
+                <SyntaxHighlighter language='xml' style={docco}>
                     {text}
                 </SyntaxHighlighter>
             </div>
