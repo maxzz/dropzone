e03a790414602813fa4b918443fe0d001cd5dda5
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-16 17:55:18 -0700

    WIP. Editor_Manifest. add forms content placeholders

--- src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest.tsx
+++ src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest.tsx
@@ -3,12 +3,50 @@ import { useAtom } from 'jotai';
 import { FileUsAtomType } from '@/store';
 import SelectDemo from './UISelect';
 
-export function Editor_Manifest({fileUsAtom}: {fileUsAtom: FileUsAtomType}) {
+function Form_Login() {
+    return (<>
+        <div className="text-lg border-red-500 border-b">Login</div>
+
+        <div className="">Fields</div>
+
+        <div className="">Submit options</div>
+
+        <div className="">Form options</div>
+        <div className="">
+            <div className="">General</div>
+            <div className="">Quick link</div>
+            <div className="">Screen detection</div>
+            <div className="">Authentication</div>
+        </div>
+    </>);
+}
+
+function Form_PChange() {
+    return (<>
+        <div className="text-lg border-red-500 border-b">Password change</div>
+
+        <div className="">Fields</div>
+
+        <div className="">Submit options</div>
+
+        <div className="">Form options</div>
+        <div className="">
+            <div className="">General</div>
+            <div className="">Quick link</div>
+            <div className="">Screen detection</div>
+            <div className="">Authentication</div>
+        </div>
+    </>);
+}
+
+export function Editor_Manifest({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     const [fileUs] = useAtom(fileUsAtom);
     return (
         <div>
-            List_Manifest
-            <SelectDemo/>
+            <Form_Login />
+            <Form_PChange />
+
+            <SelectDemo />
         </div>
     );
 }
