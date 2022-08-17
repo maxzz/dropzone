1973d581cd748c7e1097863c9573e6402e2eba96
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-16 19:03:38 -0700

    WIP. Editor_Manifest. add new login form prototype

--- src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest.tsx
+++ src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest.tsx
@@ -6,46 +6,22 @@ import { UIArrow } from '@ui/UIArrow';
 import { UIAccordion } from '@ui/UIAccordion';
 import { classNames } from '@/utils/classnames';
 
-function Separator({ label, tall = true, className, ...rest }: { label?: ReactNode; tall?: boolean; } & HTMLAttributes<HTMLDivElement>) {
-    return (
-        <div className={classNames("relative select-none", className)} {...rest}>
-
-            <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                {/* <div className="w-full border-t-primary-300 border-t"></div> */}{/* wiredjs.com */}
-                <svg className="stroke-transparent fill-primary-500" viewBox="0 0 600 6">
-                    <path d="M.72 2.21c142.84.73 287.01.51 598.74 1M.01 3.09C214.67 4.2 428.48 4.18 600 2.6" />
-                </svg>
-            </div>
-
-            <div className={tall ? "relative flex justify-center" : "absolute inset-0 flex items-center justify-center"}>
-                {label}
-            </div>
-        </div>
-    );
-}
-
 function SubSection({ label, openAtom, children }: { label: ReactNode; openAtom: PrimitiveAtom<boolean>; } & HTMLAttributes<HTMLDivElement>) {
     const [open, setOpen] = useAtom(openAtom);
     return (<>
-        <Separator
-            label={
-                <div className="flex items-center cursor-pointer px-2 pb-1 bg-primary-100">
-                    <div className="text-red-500">
-                        <span className="">
-                            {label}
-                        </span>
-                    </div>
-                    <UIArrow className="w-4 h-4 pt-1 text-primary-500" open={open} />
-                </div>
-            }
-            onClick={() => setOpen(v => !v)}
-        />
+        <div className="flex items-center cursor-pointer px-2 pb-1" onClick={() => setOpen(v => !v)}>
+            <UIArrow className="w-4 h-4 pt-1 text-red-500" open={open} />
+            <div className="text-base text-red-500">
+                {label}
+            </div>
+        </div>
         <UIAccordion open={open}>
-            {children}
+            <div className="ml-12">
+                {children}
+            </div>
         </UIAccordion>
     </>);
 }
-
 
 function LoginFields({ }: {}) {
     return (<>
@@ -116,30 +92,29 @@ function Form_Login() {
         loginAtom: atom<boolean>(false),
         fieldsAtom: atom<boolean>(false),
         submitAtom: atom<boolean>(false),
+        policyAtom: atom<boolean>(false),
         optionsAtom: atom<boolean>(false),
     });
     return (<>
         <SubSection label={<div className="text-lg border-red-500 border-b">Login</div>} openAtom={atoms.loginAtom}>
             <div className="pl-4">
                 <SubSection label="Fields" openAtom={atoms.fieldsAtom}>
-                    <div className="pl-4">
-                        <LoginFields />
-                    </div>
+                    <LoginFields />
                 </SubSection>
 
                 <SubSection label="Submit options" openAtom={atoms.submitAtom}>
-                    <div className="pl-4">
-                        <SubmitOptions />
-                    </div>
+                    <SubmitOptions />
+                </SubSection>
+
+                <SubSection label="Policy" openAtom={atoms.policyAtom}>
+                    <div className="">Policy</div>
                 </SubSection>
 
                 <SubSection label="Form options" openAtom={atoms.optionsAtom}>
-                    <div className="pl-4">
-                        <div className="">General</div>
-                        <div className="">Quick link</div>
-                        <div className="">Screen detection</div>
-                        <div className="">Authentication</div>
-                    </div>
+                    <div className="">General</div>
+                    <div className="">Quick link</div>
+                    <div className="">Screen detection</div>
+                    <div className="">Authentication</div>
                 </SubSection>
             </div>
 
