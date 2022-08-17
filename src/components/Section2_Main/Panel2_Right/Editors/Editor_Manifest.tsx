8650dcececaad95e8c0fcc0f57e7ab2a300015de
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-16 18:34:28 -0700

    WIP. Editor_Manifest. add SubSection component

--- src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest.tsx
+++ src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
-import { useAtom } from 'jotai';
+import React, { HTMLAttributes, ReactNode, useState } from 'react';
+import { atom, PrimitiveAtom, useAtom } from 'jotai';
 import { FileUsAtomType } from '@/store';
 import SelectDemo from './UISelect';
+import { UIArrow } from '@ui/UIArrow';
+import { UIAccordion } from '@ui/UIAccordion';
+import { classNames } from '@/utils/classnames';
+
+function Separator({ label, tall = true, className, ...rest }: { label?: ReactNode; tall?: boolean; } & HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div className={classNames("relative select-none", className)} {...rest}>
+            <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                {/* <div className="w-full border-t-primary-300 border-t"></div> */}{/* wiredjs.com */}
+                <svg className="stroke-transparent fill-primary-500" viewBox="0 0 600 6">
+                    <path d="M.72 2.21c142.84.73 287.01.51 598.74 1M.01 3.09C214.67 4.2 428.48 4.18 600 2.6" />
+                </svg>
+            </div>
+            <div className={tall ? "relative flex justify-center" : "absolute inset-0 flex items-center justify-center"}>
+                <span className="px-2 pb-1 bg-primary-100">
+                    {label}
+                </span>
+            </div>
+        </div>
+    );
+}
+
+function SubSection({ label, openAtom, children }: { label: string; openAtom: PrimitiveAtom<boolean>; } & HTMLAttributes<HTMLDivElement>) {
+    const [open, setOpen] = useAtom(openAtom);
+    return (<>
+        <Separator
+            label={
+                <div className="flex items-center cursor-pointer">
+                    <div className="">{label}</div>
+                    <UIArrow className="w-4 h-4 pt-1 text-primary-500" open={open} />
+                </div>
+            }
+            onClick={() => setOpen(v => !v)}
+        />
+        <UIAccordion open={open}>
+            {children}
+        </UIAccordion>
+    </>);
+}
+
 
 function Values({ isPsw }: { isPsw: boolean; }) {
     return (<>
@@ -61,6 +101,9 @@ function PasswordManagerIcon({ }: {}) {
 }
 
 function Form_Login() {
+    const [atoms] = useState({
+        optionsAtom: atom<boolean>(false),
+    })
     return (<>
         <div className="text-lg border-red-500 border-b">Login</div>
         <div className="pl-4">
@@ -68,13 +111,15 @@ function Form_Login() {
 
             <div className="">Submit options</div>
 
-            <div className="">Form options</div>
+            <div className="text-red-500">Form options</div>
+            <SubSection label="Form options" openAtom={atoms.optionsAtom}>
             <div className="pl-4">
                 <div className="">General</div>
                 <div className="">Quick link</div>
                 <div className="">Screen detection</div>
                 <div className="">Authentication</div>
             </div>
+            </SubSection>
         </div>
     </>);
 }
