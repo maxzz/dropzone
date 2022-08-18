ab02573c8d80c8bdc384d7d015a8ff56bb6e8e34
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-18 16:35:53 -0700

    WIP. tailwind-plugin-data-state. done. code clean up

--- tailwind/tailwind-plugin-data-state.js
+++ tailwind/tailwind-plugin-data-state.js
@@ -3,17 +3,15 @@ const plugin = require('tailwindcss/plugin');
 /**
  * @param {string} state 
  * @param {import('tailwindcss/types/config').PluginAPI} helpers 
- * 
- * * addVariant - for registering custom variants
- * * e - for manually escaping strings meant to be used in class names
  */
 function addDataStateVariant(state, { addVariant, e, }) {
 
+    // addVariant - for registering custom variants
+    // e - for manually escaping strings meant to be used in class names
+   
     addVariant(`data-state-${state}`,
         ({ modifySelectors, separator }) => {
-            modifySelectors(({ className }) => {
-                //console.log(`------------- className = '${className}' state = '${state}'`);
-
+            modifySelectors(({ className }) => { //console.log(`------------- className = '${className}' state = '${state}'`);
                 return `.${e(`data-state-${state}${separator}${className}`)}[data-state='${state}']`;
             });
         }
@@ -39,17 +37,12 @@ function addDataStateVariant(state, { addVariant, e, }) {
 /**
  * @param {string} state 
  * @param {import('tailwindcss/types/config').PluginAPI} helpers 
- * 
- * * addVariant - for registering custom variants
- * * e - for manually escaping strings meant to be used in class names
  */
 function addDataVariant(state, { addVariant, e, }) {
 
     addVariant(`data-${state}`,
         ({ modifySelectors, separator }) => {
             modifySelectors(({ className }) => {
-                console.log(`------place------- className = '${className}' state = '${state}'`);
-
                 return `.${e(`data-${state}${separator}${className}`)}[data-${state}]`;
             });
         }
@@ -58,8 +51,6 @@ function addDataVariant(state, { addVariant, e, }) {
     addVariant(`group-data-${state}`,
         ({ modifySelectors, separator }) => {
             modifySelectors(({ className }) => {
-                console.log(`------group------- className = '${className}' state = '${state}'`);
-
                 return `.group[data-${state}] .${e(`group-data-${state}${separator}${className}`)}`;
             });
         }
@@ -68,8 +59,6 @@ function addDataVariant(state, { addVariant, e, }) {
     addVariant(`peer-data-${state}`,
         ({ modifySelectors, separator }) => {
             modifySelectors(({ className }) => {
-                console.log(`------peer------- className = '${className}' state = '${state}'`);
-
                 return `.peer[data-${state}] ~ .${e(`peer-data-${state}${separator}${className}`)}`;
             });
         }
@@ -77,13 +66,14 @@ function addDataVariant(state, { addVariant, e, }) {
 }
 
 module.exports = plugin(function (helpers) {
-    // variants that help styling Radix-UI components
+    // variants that help styling data-state-* of radix-ui components
     addDataStateVariant('open', helpers);
     addDataStateVariant('closed', helpers);
     addDataStateVariant('on', helpers);
     addDataStateVariant('checked', helpers);
     addDataStateVariant('unchecked', helpers);
 
+    // variants that help styling data-* on radix-ui components
     addDataVariant('disabled', helpers);
     addDataVariant('highlighted', helpers);
     addDataVariant('placeholder', helpers);
