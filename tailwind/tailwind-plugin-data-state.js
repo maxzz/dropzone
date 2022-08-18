a1a447faa68cff9a553a36995380d5777e6e37ed
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-18 15:53:19 -0700

    WIP. tailwind-plugin-data-state. add trace from console and types

--- tailwind/tailwind-plugin-data-state.js
+++ tailwind/tailwind-plugin-data-state.js
@@ -1,39 +1,40 @@
-///<reference path='../node_modules/tailwindcss/types/config.d.ts'/>
-
 const plugin = require('tailwindcss/plugin');
 
 /**
  * @param {string} state 
  * @param {import('tailwindcss/types/config').PluginAPI} helpers 
+ * 
+ * * addVariant - for registering custom variants
+ * * e - for manually escaping strings meant to be used in class names
  */
-function dataStateVariant(state, {
-    // config.d.ts:
-    // addVariant(name: string, definition: string | string[] | (() => string) | (() => string)[]): void
-    // for registering custom variants
-    addVariant,
-    // e: (className: string) => string
-    // for manually escaping strings meant to be used in class names
-    e
-}) {
-    addVariant(`data-state-${state}`, ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-            console.log('------------- class', className, '-----------state', state);
-            
-            return `.${e(`data-state-${state}${separator}${className}`)}[data-state='${state}']`;
-        });
-    });
+function dataStateVariant(state, { addVariant, e, }) {
 
-    addVariant(`group-data-state-${state}`, ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-            return `.group[data-state='${state}'] .${e(`group-data-state-${state}${separator}${className}`)}`;
-        });
-    });
+    addVariant(`data-state-${state}`,
+        ({ modifySelectors, separator }) => {
+            modifySelectors(({ className }) => {
+                console.log(`------------- className = '${className}' state = '${state}'`);
+                
+                return `.${e(`data-state-${state}${separator}${className}`)}[data-state='${state}']`;
+            });
+        }
+    );
 
-    addVariant(`peer-data-state-${state}`, ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-            return `.peer[data-state='${state}'] ~ .${e(`peer-data-state-${state}${separator}${className}`)}`;
-        });
-    });
+    addVariant(`group-data-state-${state}`,
+        ({ modifySelectors, separator }) => {
+            modifySelectors(({ className }) => {
+                return `.group[data-state='${state}'] .${e(`group-data-state-${state}${separator}${className}`)}`;
+            });
+        }
+    );
+
+    addVariant(`peer-data-state-${state}`,
+        ({ modifySelectors, separator }) => {
+            modifySelectors(({ className }) => {
+                return `.peer[data-state='${state}'] ~ .${e(`peer-data-state-${state}${separator}${className}`)}`;
+            });
+        }
+    );
+
 }
 
 module.exports = plugin(function (helpers) {
