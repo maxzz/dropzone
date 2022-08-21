5596c9ba3fd1dd4435029ffcbe88422a6b25e942
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-21 02:08:31 -0700

    WIP. Editor_Manifest. update ValueDropdown. 10. compact view

--- src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest/Part1_Fields.tsx
+++ src/components/Section2_Main/Panel2_Right/Editors/Editor_Manifest/Part1_Fields.tsx
@@ -166,63 +166,63 @@ function FieldTypeOld({ value }: { value: number; }) {
 function FieldType({ field }: { field: Meta.Field; }) {
     const { password, type = 'NOTYPE' } = field.mani;
     return (
-        <div className="flex items-center space-x-0.5">
+        <div className="ml-0.5 flex items-center space-x-0.5">
             <FormRowTypeIcon field={field.mani} className="w-5 h-5 text-primary-500" />
             <div className="text-primary-500">{`${password ? 'psw' : type}`}</div>
         </div>
     );
 }
 
-function FieldValue({ isPsw, value }: { isPsw: boolean; value: number; }) {
-    return (
-        <>
-            <se.Select>
-                <se.SelectTrigger aria-label="Food">
-                    <se.SelectValue /> <se.SelectIcon> <ChevronDownIcon /> </se.SelectIcon>
-                </se.SelectTrigger>
+// function FieldValue({ isPsw, value }: { isPsw: boolean; value: number; }) {
+//     return (
+//         <>
+//             <se.Select>
+//                 <se.SelectTrigger aria-label="Food">
+//                     <se.SelectValue /> <se.SelectIcon> <ChevronDownIcon /> </se.SelectIcon>
+//                 </se.SelectTrigger>
 
-                <se.SelectContent>
-                    <se.SelectScrollUpButton>
-                        <ChevronUpIcon />
-                    </se.SelectScrollUpButton>
+//                 <se.SelectContent>
+//                     <se.SelectScrollUpButton>
+//                         <ChevronUpIcon />
+//                     </se.SelectScrollUpButton>
 
-                    <se.SelectViewport>
-                        <se.SelectGroup>
-                            <se.SelectLabel>Fruits</se.SelectLabel>
-                            <SeGroupItem label="Ask - Resuse" value="1" />
-                            <SeGroupItem label="Ask - Confirm" value="2" />
-                            <SeGroupItem label="Ask Always" value="3" />
-                        </se.SelectGroup>
+//                     <se.SelectViewport>
+//                         <se.SelectGroup>
+//                             <se.SelectLabel>Fruits</se.SelectLabel>
+//                             <SeGroupItem label="Ask - Resuse" value="1" />
+//                             <SeGroupItem label="Ask - Confirm" value="2" />
+//                             <SeGroupItem label="Ask Always" value="3" />
+//                         </se.SelectGroup>
 
-                        {/* <se.SelectSeparator />
+//                         {/* <se.SelectSeparator />
 
-                        <se.SelectGroup>
-                        </se.SelectGroup> */}
+//                         <se.SelectGroup>
+//                         </se.SelectGroup> */}
 
-                    </se.SelectViewport>
+//                     </se.SelectViewport>
 
-                    <se.SelectScrollDownButton>
-                        <ChevronDownIcon />
-                    </se.SelectScrollDownButton>
+//                     <se.SelectScrollDownButton>
+//                         <ChevronDownIcon />
+//                     </se.SelectScrollDownButton>
 
-                </se.SelectContent>
-            </se.Select>
+//                 </se.SelectContent>
+//             </se.Select>
 
-            {/* {isPsw
-                ? <>
-                    <div className="">Windows Password</div>
-                </>
-                : <>
-                    <div className="">Windows User Name</div>
-                    <div className="">Windows User Principal Name</div>
-                    <div className="">Windows Domain\User Name</div>
-                    <div className="">Windows Domain</div>
-                    <div className="">Windows E-mail Address</div>
-                </>
-            } */}
-        </>
-    );
-}
+//             {/* {isPsw
+//                 ? <>
+//                     <div className="">Windows Password</div>
+//                 </>
+//                 : <>
+//                     <div className="">Windows User Name</div>
+//                     <div className="">Windows User Principal Name</div>
+//                     <div className="">Windows Domain\User Name</div>
+//                     <div className="">Windows Domain</div>
+//                     <div className="">Windows E-mail Address</div>
+//                 </>
+//             } */}
+//         </>
+//     );
+// }
 
 function InputRow({ field }: { field: Meta.Field; }) {
     const { useit, displayname, type: typ, value: val } = field.mani;
@@ -242,7 +242,7 @@ function InputRow({ field }: { field: Meta.Field; }) {
     const [valueAs, setValueAs] = useAtom(state.valueAsAtom);
     return (<>
         <input
-            className="place-self-center w-5 h-5 form-checkbox text-primary-700 bg-primary-800
+            className="place-self-center w-4 h-4 form-checkbox text-primary-700 bg-primary-800
             ring-1
             focus:ring-1
             focus:ring-offset-primary-800 ring-primary-600 focus:ring-primary-400
@@ -251,18 +251,18 @@ function InputRow({ field }: { field: Meta.Field; }) {
             checked={useIt}
             onChange={() => setUseIt(v => !v)}
         />
+
         <InputField valueAtom={state.labelAtom} placeholder="Label" />
         <InputField valueAtom={state.labelAtom} placeholder="Catalog" />
 
-        {/* <InputField valueAtom={state.valueAtom} placeholder="Username" /> */}
         <ValueDropdown field={field} />
 
-        <FieldValue isPsw={false} value={2} />
-        <FieldType field={field} /> {/* <div className="px-2 border-primary-800 border-l border-r">text</div> */}
+        {/* <FieldValue isPsw={false} value={2} /> */}
+        <FieldType field={field} />
     </>);
 }
 
-const titles = ["Use it", "Label", "Catalog", "Value", "Value type", "Type"];
+const titles = ["Use it", "Label", "Catalog", "Value", "Type"];
 function TableHeader() {
     return (<>
         {titles.map((title, idx) => (
@@ -276,7 +276,7 @@ export function Part1_Fields({ fields }: { fields: Meta.Field[] | undefined; }) 
         {fields
             ? <>
                 <div className={classNames(
-                    "p-2 grid grid-cols-[max-content_minmax(0,1fr)_minmax(0,1fr)_minmax(0,1fr)_max-content_auto] items-center gap-x-2 gap-y-1",
+                    "p-2 grid grid-cols-[max-content_minmax(0,1fr)_minmax(0,1fr)_minmax(0,1fr)_auto] items-center gap-x-0.5 gap-y-0.5",
                     "bg-primary-800 text-primary-200 rounded-sm"
                 )}>
                     <TableHeader />
@@ -285,6 +285,6 @@ export function Part1_Fields({ fields }: { fields: Meta.Field[] | undefined; }) 
             </>
             : <div className="">no fields</div>
         }
-        <DropdownMenu />
+        {/* <DropdownMenu /> */}
     </>);
 }
