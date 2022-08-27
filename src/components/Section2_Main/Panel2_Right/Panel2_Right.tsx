f3aa727ac504a55d814f362c75b011ccd3e462f3
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-27 14:51:44 -0700

    WIP. Panel2_Right. move Scroller out

--- src/components/Section2_Main/Panel2_Right/Panel2_Right.tsx
+++ src/components/Section2_Main/Panel2_Right/Panel2_Right.tsx
@@ -1,32 +1,24 @@
 import React, { HTMLAttributes } from 'react';
 import { useAtomValue } from 'jotai';
-import { FileUsAtomType, rightPanelData, ViewMode } from '@/store';
+import { FileUs, FileUsAtomType, rightPanelData, ViewMode } from '@/store';
 import { classNames } from '@/utils/classnames';
 import { CardTitleTextNormal } from '../Panel1_FilesList/Card/Part1Card_Title/Part1Card_Title';
-import { UISemiScrollbar } from '@ui/UISemiScrollbar';
+import { Scroller } from './Scroller';
 import { ManiActions } from './ManiActions/ManiActions';
 import { BodyAsHighlightedText } from './BodyAsHighlightedText';
 import { BodyAsEditors } from './BodyAsEditors';
 
-function Scroller({ className, children, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    return (
-        <UISemiScrollbar className={classNames("px-2 pt-1 pb-4 overflow-auto w-full h-full", className)} {...rest}>
-            {children}
-        </UISemiScrollbar>
-    );
-}
-
 export function Panel2_Right({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    const fileUsAtom = useAtomValue(rightPanelData.panelAtom);
-    const rightPanelValue = useAtomValue(rightPanelData.valueAtom);
-    const rightPanelViewMode = useAtomValue(rightPanelData.viewModeAtom);
+    const fileUsAtom: FileUsAtomType | undefined = useAtomValue(rightPanelData.panelAtom);
+    const fileUs: FileUs | undefined = useAtomValue(rightPanelData.valueAtom);
+    const viewMode = useAtomValue(rightPanelData.viewModeAtom);
 
     const canEditManifest = !!fileUsAtom;
-    const showRaw = rightPanelViewMode === ViewMode.raw || (rightPanelViewMode === ViewMode.edit && !canEditManifest);
+    const showRaw = viewMode === ViewMode.raw || (viewMode === ViewMode.edit && !canEditManifest);
 
     return (
         <div className={classNames("flex-auto pt-2 pb-2 w-full h-full overflow-hidden bg-primary-900", className)} {...rest}>
-            {rightPanelValue &&
+            {fileUs &&
                 <div className="w-full h-full flex flex-col">
 
                     {/* Card title */}
@@ -43,7 +35,7 @@ export function Panel2_Right({ className, ...rest }: HTMLAttributes<HTMLDivEleme
                             : "text-xs text-primary-100"
                     }>
                         {showRaw
-                            ? <BodyAsHighlightedText text={rightPanelValue.raw || ''} />
+                            ? <BodyAsHighlightedText text={fileUs.raw || ''} />
                             : fileUsAtom && <BodyAsEditors fileUsAtom={fileUsAtom} />
                         }
                     </Scroller>
