ec85805356005f65b4b690982a1cdbb7db23b488
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-27 01:11:23 -0700

    WIP. Editor_FieldCatalog. initial render of real data

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_FieldCatalog/Editor_FieldCatalog.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_FieldCatalog/Editor_FieldCatalog.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useAtom } from 'jotai';
 import { FileUsAtomType } from '@/store';
 
 export function Editor_FieldCatalog({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     const [fileUs] = useAtom(fileUsAtom);
+    const names = fileUs.fcat?.names || [];
     return (
-        <div>
-            List_Manifest
+        <div className="grid grid-cols-2 text-primary-400">
+            {names.map((item, idx) => (
+                <Fragment key={idx}>
+                    <div className="">{item.dispname}</div>
+                    <div className="font-mono text-[.6rem]" key={idx}>{item.dbname}</div>
+                </Fragment>
+            ))}
         </div>
     );
 }
