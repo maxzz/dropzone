67c0492c812026192f8ac6793124746a40d63118
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-27 00:57:31 -0700

    WIP. Editor_FieldCatalog. initial clean up

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_FieldCatalog/Editor_FieldCatalog.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_FieldCatalog/Editor_FieldCatalog.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { useAtom } from 'jotai';
 import { FileUsAtomType } from '@/store';
-import { SelectDemo } from '../UISelect';
 
 export function Editor_FieldCatalog({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     const [fileUs] = useAtom(fileUsAtom);
     return (
         <div>
             List_Manifest
-            <SelectDemo />
         </div>
     );
 }
