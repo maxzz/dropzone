e3210b49842a016d7c872150902cd873e706da82
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-26 19:23:41 -0700

    WIP. Editors. add different render for manifests and field catalog

--- src/components/Section2_Main/Panel2_Right/Editors/index.tsx
+++ src/components/Section2_Main/Panel2_Right/Editors/index.tsx
@@ -1,13 +1,15 @@
 import { FileUsAtomType } from '@/store';
+import { useAtomValue } from 'jotai';
 import React from 'react';
 import { Editor_FieldCatalog } from './Editor_FieldCatalog/Editor_FieldCatalog';
 import { Editor_Manifest } from './Editor_Manifest/Editor_Manifest';
 
-export function Editors({ fileUsAtom }: { fileUsAtom: FileUsAtomType | undefined; }) {
-    return (<>
-        {fileUsAtom && <>
-            <Editor_Manifest fileUsAtom={fileUsAtom} />
-            {/* <Editor_FieldCatalog fileUsAtom={fileUsAtom} /> */}
-        </>}
-    </>);
+export function Editors({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
+    const fileUs = useAtomValue(fileUsAtom);
+    return (
+        !fileUs.meta
+            ? <></>
+            : fileUs.fcat
+                ? <Editor_FieldCatalog fileUsAtom={fileUsAtom} />
+                : <Editor_Manifest fileUsAtom={fileUsAtom} />);
 }
