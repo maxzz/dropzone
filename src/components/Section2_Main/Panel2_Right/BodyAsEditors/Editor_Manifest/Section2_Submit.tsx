90ee8e24cecb537f26beb8d666d1f1d9622d915a
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-27 17:21:57 -0700

    WIP. Section2_Submit. add fields differentiation by manifest type

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section2_Submit.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section2_Submit.tsx
@@ -1,16 +1,22 @@
-import { FieldTyp, Meta } from '@/store/manifest';
 import React from 'react';
+import { FieldTyp, Meta } from '@/store/manifest';
 
 export function Section2_Submit({ form }: { form: Meta.Form | undefined; }) {
+    const isWeb = !!form?.mani.detection.web_ourl;
     const ourFields = form?.fields?.filter((field) => field.ftyp === FieldTyp.button);
     return (<>
-        {ourFields?.map((field, idx) => (
-            <div className="" key={idx}>
-                {field.mani.displayname}
-            </div>
-        ))}
-        <hr />
         <div className="">Do Not Submit</div>
-        <div className="">Automatically submit login data</div>
+
+        {isWeb
+            ? <div className="">Automatically submit login data</div>
+            : <>
+                <hr />
+                {ourFields?.map((field, idx) => (
+                    <div className="" key={idx}>
+                        {field.mani.displayname}
+                    </div>
+                ))}
+            </>
+        }
     </>);
 }
