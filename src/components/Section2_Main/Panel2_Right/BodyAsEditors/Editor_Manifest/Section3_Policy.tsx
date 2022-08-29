a6e748dc7d58e2e2c3b2a6ac9db27215152ec3d2
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-29 14:58:25 -0700

    WIP. Section3_Policy. add initial implementation

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
+import { PrimitiveAtom, useAtom, useAtomValue } from 'jotai';
+import { FileUsAtomType, FormIdx } from '@/store';
+import { classNames } from '@/utils/classnames';
 
-export function Section3_Policy() {
-    return (<>
-        <div className="">Policy</div>
-    </>);
+
+export function Section3_Policy({ fileUsAtom, formType }: { fileUsAtom: FileUsAtomType; formType: FormIdx; }) {
+    const fileUs = useAtomValue(fileUsAtom);
+    const metaForm = fileUs.meta?.[formType];
+
+    const policies = metaForm?.fields?.filter((field) => field.mani.policy || field.mani.policy2);
+
+    /* // No, keep it simple, just check one at time
+
+    if (formType === FormIdx.login) {
+        // check in this form (new addition) and cpass
+    } else {
+        // check only in this form
+    }
+    */
+
+    return (
+        <>
+            {!policies?.length
+                ? <div className="">Policy not specified</div>
+                : policies.length === 1
+                    ? <>
+                        <div className="">{policies[0].mani.policy}</div>
+                        {policies[0].mani.policy2 && <div className="">{policies[0].mani.policy2}</div>}
+                    </>
+                    : <>
+                        Multiple policies
+                    </>
+            }
+            {/* <hr className="mt-4" />
+            <Popup /> */}
+        </>
+    );
 }
