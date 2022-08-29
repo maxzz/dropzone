5637f0c13748c65304997078544f3efb5e2dc64e
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-29 15:52:05 -0700

    WIP. PolicyEditor. add FieldWithPolicy to handle multiple policies

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy/Section3_Policy.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy/Section3_Policy.tsx
@@ -3,6 +3,7 @@ import { PrimitiveAtom, useAtom, useAtomValue } from 'jotai';
 import { FileUsAtomType, FormIdx } from '@/store';
 import { classNames } from '@/utils/classnames';
 import { PolicyEditor } from './PolicyEditor';
+import { Meta } from '@/store/manifest';
 
 function Input({ valueAtom, className, ...rest }: { valueAtom: PrimitiveAtom<string>; } & InputHTMLAttributes<HTMLInputElement>) {
     const [value, setValue] = useAtom(valueAtom);
@@ -21,6 +22,13 @@ function Input({ valueAtom, className, ...rest }: { valueAtom: PrimitiveAtom<str
             {...rest}
         />
     );
+}
+
+function FieldWithPolicy({ field }: { field: Meta.Field; }) {
+    return (<>
+        <div className="">{field.mani.policy}</div>
+        {field.mani.policy2 && <div className="">{field.mani.policy2}</div>}
+    </>);
 }
 
 export function Section3_Policy({ fileUsAtom, formType }: { fileUsAtom: FileUsAtomType; formType: FormIdx; }) {
@@ -42,14 +50,19 @@ export function Section3_Policy({ fileUsAtom, formType }: { fileUsAtom: FileUsAt
         <>
             {!policies?.length
                 ? <div className="">Policy not specified</div>
-                : policies.length === 1
-                    ? <>
-                        <div className="">{policies[0].mani.policy}</div>
-                        {policies[0].mani.policy2 && <div className="">{policies[0].mani.policy2}</div>}
-                    </>
-                    : <>
-                        Multiple policies
-                    </>
+                :
+                <div className="">
+                    {policies.map((field, idx) => (
+                        <FieldWithPolicy field={field} key={idx} />
+                    ))}
+                </div>
+
+                // policies.length === 1
+                //     ?
+                //     <FieldWithPolicy field={policies[0]} />
+                //     : <>
+                //         Multiple policies
+                //     </>
             }
             <hr className="mt-4" />
             <PolicyEditor />
