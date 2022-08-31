3d4d27ccdf33e2d833c682ed77b3a576caf1121c
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-31 15:35:52 -0700

    WIP. PolicyEditor. separate UI components into new file

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy/PolicyEditor.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy/PolicyEditor.tsx
@@ -1,11 +1,10 @@
-import React, { HTMLAttributes, useState } from "react";
+import React, { useState } from "react";
 import { atom, PrimitiveAtom, useAtom } from "jotai";
 import { a, config, useTransition } from "@react-spring/web";
 import * as Dialog from '@radix-ui/react-dialog';
-import * as Select from '@radix-ui/react-select';
+import { Check, Dropdown, Input, Radio } from "./PolicyeditorUi";
 import { classNames } from "@/utils/classnames";
-import { IconChevronDown, IconCross } from "@ui/UIIconSymbols";
-import { CheckIcon } from "@radix-ui/react-icons";
+import { IconCross } from "@ui/UIIconSymbols";
 import { ConstrainPsw, ConstrainSet, namesConstrainPsw, namesConstrainSet, UseAs } from "@/store/policy";
 
 type PolicyUi = {
@@ -25,91 +24,6 @@ type PolicyUi = {
 
     useAs: UseAs;       // by user / by system
 };
-
-function Check({ children, className, ...rest }: HTMLAttributes<HTMLElement>) {
-    return (
-        <label className={classNames("w-max inline-flex items-center gap-x-2 select-none cursor-pointer", className)} {...rest}>
-            <input className="place-self-center w-4 h-4 dark-checkbox" type="checkbox" />
-            {children}
-        </label>
-    );
-}
-
-function Radio({ children, name, checked, onChange, className, ...rest }: { name: string; checked: boolean; } & HTMLAttributes<HTMLElement>) {
-    return (
-        <label className={classNames("w-max inline-flex items-center gap-x-2 select-none cursor-pointer", className)} {...rest}>
-            <input className="w-4 h-4 dark-radio" name={name} checked={checked} onChange={onChange} type="radio" />
-            {children}
-        </label>
-    );
-}
-
-function Input({ className, ...rest }: HTMLAttributes<HTMLInputElement>) {
-    return (
-        <input className={classNames("p-2 h-9 text-primary-300 bg-primary-700 rounded", className)} {...rest} />
-    );
-}
-
-const itemsRule = [
-    { value: "1", name: "Letters and numbers" },
-    { value: "2", name: "Numbers only" },
-    { value: "3", name: "Letters only" },
-    { value: "4", name: "Letters or numbers with special characters" },
-    { value: "5", name: "Letters or numbers with at least one number" },
-];
-
-const itemsHistory = [
-    { value: "1", name: "None" },
-    { value: "2", name: "Different than the Windows password" },
-    { value: "3", name: "Unique within Password Manager logons" },
-    { value: "4", name: "Different than the current password" },
-];
-
-function Dropdown({ items, valueAtom, className }: { items: string[]; valueAtom: PrimitiveAtom<string>; } & HTMLAttributes<HTMLButtonElement>) {
-    const [val, setVal] = useAtom(valueAtom);
-    return (<>
-        <Select.Root value={val} onValueChange={(v: string) => setVal(v)}>
-            <Select.Trigger className={className}>
-                <div className="p-2 flex items-center justify-between space-x-1 text-primary-300 bg-primary-700 rounded">
-                    <Select.Value />
-                    <Select.Icon><IconChevronDown className="w-4 h-4" /></Select.Icon>
-                </div>
-            </Select.Trigger>
-
-            <Select.Portal container={document.getElementById('portal')}>
-                <Select.Content>
-                    <Select.Viewport className={
-                        classNames(
-                            "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
-                            "px-1.5 py-1 grid grid-cols-1 rounded shadow-md",
-                            "bg-primary-100 dark:bg-gray-800 rounded",
-                        )}
-                    >
-                        {items.map((item, idx) => (
-                            <Select.Item className={
-                                classNames(
-                                    "relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded outline-none",
-                                    "text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100",
-                                    "focus:bg-primary-100",
-                                    "radix-disabled:opacity-50",
-                                    "focus:outline-none select-none"
-                                )}
-                                value={`${idx}`}
-                                key={idx}
-                            >
-                                <Select.ItemText>{item}</Select.ItemText>
-                                <Select.ItemIndicator className="absolute left-2 inline-flex items-center">
-                                    <CheckIcon />
-                                </Select.ItemIndicator>
-                            </Select.Item>
-                        ))}
-                    </Select.Viewport>
-                </Select.Content>
-            </Select.Portal>
-
-        </Select.Root>
-    </>);
-}
 
 function EditorBody() {
     const ruleAtom = useState(atom('1'))[0];
