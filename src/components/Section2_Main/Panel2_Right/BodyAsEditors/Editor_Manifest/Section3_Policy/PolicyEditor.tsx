6507855a7806b4d1b85a913f0e07a4976b727553
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-30 19:23:23 -0700

    WIP. PolicyEditor. test appear duration and Select width

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy/PolicyEditor.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section3_Policy/PolicyEditor.tsx
@@ -38,54 +38,51 @@ const itemsPolicy = [
     { value: "4", name: "Different than the current password" },
 ];
 
-function Dropdown({ items }: {items: {value: string; name: string}[]} & HTMLAttributes<HTMLInputElement>) {
+function Dropdown({ items }: { items: { value: string; name: string; }[]; } & HTMLAttributes<HTMLInputElement>) {
     const ourAtom = useState(atom('1'))[0];
     const [val, setVal] = useAtom(ourAtom);
     return (<>
-        {/* <div className="relative"> */}
-            <Select.Root value={val} onValueChange={(v: string) => setVal(v)}>
-                <Select.Trigger>
-                    <div className="p-2 flex items-center space-x-1 text-primary-300 bg-primary-700 rounded">
-                        <Select.Value />
-                        <Select.Icon><IconChevronDown className="w-4 h-4" /></Select.Icon>
-                    </div>
-                </Select.Trigger>
+        <Select.Root value={val} onValueChange={(v: string) => setVal(v)}>
+            <Select.Trigger>
+                <div className="p-2 flex items-center justify-between space-x-1 text-primary-300 bg-primary-700 rounded">
+                    <Select.Value />
+                    <Select.Icon><IconChevronDown className="w-4 h-4" /></Select.Icon>
+                </div>
+            </Select.Trigger>
 
-                <Select.Portal container={document.getElementById('portal')}>
-                    <Select.Content>
-                        <Select.Viewport className={
-                            classNames(
-                                "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
-                                "px-1.5 py-1 grid grid-cols-1 rounded shadow-md",
-                                "bg-primary-100 dark:bg-gray-800 rounded",
-                            )}
-                        >
-                            {items.map((item, idx) => (
-                                <Select.Item className={
-                                    classNames(
-                                        "relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded outline-none",
-                                        "text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100",
-                                        "focus:bg-primary-100",
-                                        "radix-disabled:opacity-50",
-                                        "focus:outline-none select-none"
-                                    )}
-                                    value={item.value}
-                                    key={idx}
-                                >
-                                    <Select.ItemText>{item.name}</Select.ItemText>
-                                    <Select.ItemIndicator className="absolute left-2 inline-flex items-center">
-                                        <CheckIcon />
-                                    </Select.ItemIndicator>
-                                </Select.Item>
-                            ))}
-                        </Select.Viewport>
-                    </Select.Content>
-                </Select.Portal>
+            <Select.Portal container={document.getElementById('portal')}>
+                <Select.Content>
+                    <Select.Viewport className={
+                        classNames(
+                            "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
+                            "px-1.5 py-1 grid grid-cols-1 rounded shadow-md",
+                            "bg-primary-100 dark:bg-gray-800 rounded",
+                        )}
+                    >
+                        {items.map((item, idx) => (
+                            <Select.Item className={
+                                classNames(
+                                    "relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded outline-none",
+                                    "text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100",
+                                    "focus:bg-primary-100",
+                                    "radix-disabled:opacity-50",
+                                    "focus:outline-none select-none"
+                                )}
+                                value={item.value}
+                                key={idx}
+                            >
+                                <Select.ItemText>{item.name}</Select.ItemText>
+                                <Select.ItemIndicator className="absolute left-2 inline-flex items-center">
+                                    <CheckIcon />
+                                </Select.ItemIndicator>
+                            </Select.Item>
+                        ))}
+                    </Select.Viewport>
+                </Select.Content>
+            </Select.Portal>
 
-            </Select.Root>
-        {/* </div> */}
-    </>
-    );
+        </Select.Root>
+    </>);
 }
 
 function EditorBody() {
@@ -183,7 +180,8 @@ export function PolicyEditor() {
         from: { opacity: 0, y: -10, scale: 0.97 },
         enter: { opacity: 1, y: 0, scale: 1 },
         leave: { opacity: 0, y: 10, scale: 0.97 },
-        config: config.stiff,
+        // config: config.stiff,
+        config: {duration: 3000},
     });
     return (
         <Dialog.Root open={open} onOpenChange={setOpen}>
