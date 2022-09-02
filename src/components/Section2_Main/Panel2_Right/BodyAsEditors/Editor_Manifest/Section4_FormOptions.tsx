c86e05bccddfeaf1f84451311f6fe2954c202a8f
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-02 02:37:30 -0700

    WIP. Section4_FormOptions. add RowBoolean component

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section4_FormOptions.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section4_FormOptions.tsx
@@ -3,7 +3,7 @@ import { Atomize } from '@/hooks/atomsX';
 import { FileUsAtomType, FormIdx } from '@/store';
 import { Meta } from '@/store/manifest';
 import { classNames } from '@/utils/classnames';
-import { atom, useAtom, useAtomValue } from 'jotai';
+import { atom, PrimitiveAtom, useAtom, useAtomValue } from 'jotai';
 
 type UiPart1General = {
     name: string;       // login name
@@ -82,16 +82,29 @@ function Section({ label }: { label: string; }) {
     );
 }
 
-function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
+function RowInput({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
     return (
         <input
             className={classNames(
                 "px-2 py-1 h-6",
-                "bg-primary-800 text-primary-300 focus:ring-offset-primary-800 ring-primary-600 focus:ring-primary-400",
+                "bg-primary-800 text-primary-300 focus:ring-offset-primary-800 ring-primary-600 focus:ring-primary-500",
                 "focus:ring-1 focus:ring-offset-1",
                 "outline-none rounded-sm",
                 className,
             )}
+            {...rest}
+        />
+    );
+}
+
+function RowBoolean({ useItAtom, className, ...rest }: { useItAtom: PrimitiveAtom<boolean>; } & InputHTMLAttributes<HTMLInputElement>) {
+    const [useIt, setUseIt] = useAtom(useItAtom);
+    return (
+        <input
+            className={classNames("place-self-center w-4 h-4 dark-checkbox", className,)}
+            type="checkbox"
+            checked={useIt}
+            onChange={() => setUseIt(v => !v)}
             {...rest}
         />
     );
@@ -106,16 +119,16 @@ function Part1General({ atoms }: { atoms: FormAtoms; }) {
     const [balloon, setBalloon] = useAtom(atoms.uiPart1General.balloonAtom);
     return (<>
         <div className="">Managed login name</div>
-        <Input value={name} onChange={(e) => setName(e.target.value)} />
+        <RowInput value={name} onChange={(e) => setName(e.target.value)} />
 
         <div className="">Description</div>
-        <Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+        <RowInput value={desc} onChange={(e) => setDesc(e.target.value)} />
 
         <div className="">User hint</div>
-        <Input value={hint} onChange={(e) => setHint(e.target.value)} />
+        <RowInput value={hint} onChange={(e) => setHint(e.target.value)} />
 
         <div className="">Show balloon</div>
-        <Input value={balloon} onChange={(e) => setBalloon(+e.target.value)} /> {/* TODO: add validation */}
+        <RowInput value={balloon} onChange={(e) => setBalloon(+e.target.value)} /> {/* TODO: add validation */}
     </>);
 }
 
@@ -130,43 +143,40 @@ function Part2ScreenDetection({ atoms }: { atoms: FormAtoms; }) {
         isWeb ?
             <>
                 <div className="">URL</div>
-                <Input value={url} onChange={(e) => setUrl(e.target.value)} />
+                <RowInput value={url} onChange={(e) => setUrl(e.target.value)} />
             </>
             : <>
                 <div className="">Windows Caption</div>
-                <Input value={caption} onChange={(e) => setCaption(e.target.value)} />
+                <RowInput value={caption} onChange={(e) => setCaption(e.target.value)} />
 
                 <div className="">Monitor screen changes</div>
-                <Input value={monitor ? '1' : '0'} onChange={(e) => setMonitor(e.target.value === '1')} />
+                <RowInput value={monitor ? '1' : '0'} onChange={(e) => setMonitor(e.target.value === '1')} />
 
             </>);
 }
 
 function Part3Authentication({ atoms }: { atoms: FormAtoms; }) {
-    const [aim, setAim] = useAtom(atoms.uiPart4Authentication.aimAtom);
-    const [lock, setLock] = useAtom(atoms.uiPart4Authentication.lockAtom);
     return (<>
         <div className="" title="Start authentication immediately">Authenticate immediately</div>
-        <Input value={aim ? '1' : '0'} onChange={(e) => setAim(e.target.value === '1')} />
+        <RowBoolean className="justify-self-start" useItAtom={atoms.uiPart4Authentication.aimAtom} />
 
-        <div className="">Lock out login fields</div>
-        <Input value={lock ? '1' : '0'} onChange={(e) => setLock(e.target.value === '1')} />
+        <div className="mt-1.5">Lock out login fields</div>
+        <RowBoolean className="justify-self-start" useItAtom={atoms.uiPart4Authentication.lockAtom} />
     </>);
 }
 
 function Part4QL({ atoms }: { atoms: FormAtoms; }) {
-    const [dashboard, setDashboard] = useAtom(atoms.uiPart2QL.dashboardAtom);
     const [name, setName] = useAtom(atoms.uiPart2QL.nameAtom);
     const [url, setUrl] = useAtom(atoms.uiPart2QL.urlAtom);
     return (<>
         <div className="">Display on mini-dashboard</div>
-        <Input value={dashboard ? '1' : '0'} onChange={(e) => setDashboard(e.target.value === '1')} />
+        <RowBoolean className="mb-1.5 justify-self-start" useItAtom={atoms.uiPart2QL.dashboardAtom} />
 
         <div className="">Quick Link Name</div>
-        <Input value={name} onChange={(e) => setName(e.target.value)} />
+        <RowInput value={name} onChange={(e) => setName(e.target.value)} />
 
         <div className="">Quick Link URL</div>
-        <Input value={url} onChange={(e) => setUrl(e.target.value)} />
+        <RowInput value={url} onChange={(e) => setUrl(e.target.value)} />
     </>);
 }
 
@@ -175,10 +185,10 @@ function Part5PasswordManagerIcon({ atoms }: { atoms: FormAtoms; }) {
     const [loc, setLoc] = useAtom(atoms.uiPart5PasswordManagerIcon.locAtom);
     return (<>
         <div className="">Location ID</div>
-        <Input value={id} onChange={(e) => setId(e.target.value)} />
+        <RowInput value={id} onChange={(e) => setId(e.target.value)} />
 
         <div className="">Location</div>
-        <Input value={loc} onChange={(e) => setLoc(e.target.value)} />
+        <RowInput value={loc} onChange={(e) => setLoc(e.target.value)} />
     </>);
 }
 
