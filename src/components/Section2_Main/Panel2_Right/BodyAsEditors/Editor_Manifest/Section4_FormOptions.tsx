116b53105283a7c5b6d7b87c9bf7925707bd518e
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-01 18:45:15 -0700

    WIP. Section4_FormOptions. add use of local atoms

--- src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section4_FormOptions.tsx
+++ src/components/Section2_Main/Panel2_Right/BodyAsEditors/Editor_Manifest/Section4_FormOptions.tsx
@@ -3,7 +3,7 @@ import { Atomize } from '@/hooks/atomsX';
 import { FileUsAtomType, FormIdx } from '@/store';
 import { Meta } from '@/store/manifest';
 import { classNames } from '@/utils/classnames';
-import { atom } from 'jotai';
+import { atom, useAtom } from 'jotai';
 
 type UiPart1General = {
     name: string;       // login name
@@ -34,7 +34,7 @@ type UiPart5PasswordManagerIcon = {
     loc: string;        // Location
 };
 
-type FormUi = {
+type FormAtoms = {
     uiPart1General: Atomize<UiPart1General>;
     uiPart2QL: Atomize<UiPart2QL>;
     uiPart3ScreenDetection: Atomize<UiPart3ScreenDetection>;
@@ -42,7 +42,7 @@ type FormUi = {
     uiPart5PasswordManagerIcon: Atomize<UiPart5PasswordManagerIcon>;
 };
 
-function createAtoms(v: string, callback: () => void): FormUi {
+function createAtoms(v: string, callback: () => void): FormAtoms {
     return {
         uiPart1General: {
             nameAtom: atom(''),
@@ -59,7 +59,7 @@ function createAtoms(v: string, callback: () => void): FormUi {
             captionAtom: atom(''),
             monitorAtom: atom<boolean>(false),
             urlAtom: atom(''),
-                },
+        },
         uiPart4Authentication: {
             aimAtom: atom<boolean>(false),
             lockAtom: atom<boolean>(false),
@@ -68,7 +68,7 @@ function createAtoms(v: string, callback: () => void): FormUi {
             idAtom: atom(''),
             locAtom: atom(''),
         },
-    }
+    };
 }
 
 function Section({ label }: { label: string; }) {
@@ -83,51 +83,90 @@ function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
     );
 }
 
-function Part1General({ }: {}) {
+//TODO: add RowString, RowBoolean, RowNumber, SectionBody: ml-0
+
+function Part1General({ atoms }: { atoms: FormAtoms; }) {
+    const [name, setName] = useAtom(atoms.uiPart1General.nameAtom);
+    const [desc, setDesc] = useAtom(atoms.uiPart1General.descAtom);
+    const [hint, setHint] = useAtom(atoms.uiPart1General.hintAtom);
+    const [balloon, setBalloon] = useAtom(atoms.uiPart1General.balloonAtom);
     return (
         <div className="ml-4">
             <div className="">Managed login name</div>
+            <Input value={name} onChange={(e) => setName(e.target.value)} />
+
             <div className="">Description</div>
+            <Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+
             <div className="">User hint</div>
+            <Input value={hint} onChange={(e) => setHint(e.target.value)} />
+
             <div className="">Show balloon</div>
+            <Input value={balloon} onChange={(e) => setBalloon(+e.target.value)} /> {/* TODO: add validation */}
         </div>
     );
 }
 
-function Part2QL({ }: {}) {
+function Part2QL({ atoms }: { atoms: FormAtoms; }) {
+    const [dashboard, setDashboard] = useAtom(atoms.uiPart2QL.dashboardAtom);
+    const [name, setName] = useAtom(atoms.uiPart2QL.nameAtom);
+    const [url, setUrl] = useAtom(atoms.uiPart2QL.urlAtom);
     return (
         <div className="ml-4">
             <div className="">Display on mini-dashboard</div>
+            <Input value={dashboard ? '1' : '0'} onChange={(e) => setDashboard(e.target.value === '1')} />
+
             <div className="">Quick Link Name</div>
+            <Input value={name} onChange={(e) => setName(e.target.value)} />
+
             <div className="">Quick Link URL</div>
+            <Input value={url} onChange={(e) => setUrl(e.target.value)} />
         </div>
     );
 }
 
-function Part3ScreenDetection({ }: {}) {
+function Part3ScreenDetection({ atoms }: { atoms: FormAtoms; }) {
+    const [caption, setCaption] = useAtom(atoms.uiPart3ScreenDetection.captionAtom);
+    const [monitor, setMonitor] = useAtom(atoms.uiPart3ScreenDetection.monitorAtom);
+    const [url, setUrl] = useAtom(atoms.uiPart3ScreenDetection.urlAtom);
     return (
         <div className="ml-4">
             <div className="">Windows Caption</div>
+            <Input value={caption} onChange={(e) => setCaption(e.target.value)} />
+
             <div className="">Monitor screen changes</div>
+            <Input value={monitor ? '1' : '0'} onChange={(e) => setMonitor(e.target.value === '1')} />
+
             <div className="">URL</div>
+            <Input value={url} onChange={(e) => setUrl(e.target.value)} />
         </div>
     );
 }
 
-function Part4Authentication({ }: {}) {
+function Part4Authentication({ atoms }: { atoms: FormAtoms; }) {
+    const [aim, setAim] = useAtom(atoms.uiPart4Authentication.aimAtom);
+    const [lock, setLock] = useAtom(atoms.uiPart4Authentication.lockAtom);
     return (
         <div className="ml-4">
             <div className="">Start authentication immediately</div>
+            <Input value={aim ? '1' : '0'} onChange={(e) => setAim(e.target.value === '1')} />
+            
             <div className="">Lock out login fields</div>
+            <Input value={lock ? '1' : '0'} onChange={(e) => setLock(e.target.value === '1')} />
         </div>
     );
 }
 
-function Part5PasswordManagerIcon({ }: {}) {
+function Part5PasswordManagerIcon({ atoms }: { atoms: FormAtoms; }) {
+    const [id, setId] = useAtom(atoms.uiPart5PasswordManagerIcon.idAtom);
+    const [loc, setLoc] = useAtom(atoms.uiPart5PasswordManagerIcon.locAtom);
     return (
         <div className="ml-4">
             <div className="">Location ID</div>
+            <Input value={id} onChange={(e) => setId(e.target.value)} />
+
             <div className="">Location</div>
+            <Input value={loc} onChange={(e) => setLoc(e.target.value)} />
         </div>
     );
 }
@@ -135,17 +174,17 @@ function Part5PasswordManagerIcon({ }: {}) {
 export function Section4_FormOptions({ fileUsAtom, formType }: { fileUsAtom: FileUsAtomType; formType: FormIdx; }) {
     const atoms = createAtoms('', () => {
         console.log('changed');
-    })
+    });
     return (<>
         <Section label="General" />
-        <Part1General />
+        <Part1General atoms={atoms} />
         <Section label="Quick link" />
-        <Part2QL />
+        <Part2QL atoms={atoms} />
         <Section label="Screen detection" />
-        <Part3ScreenDetection />
+        <Part3ScreenDetection atoms={atoms} />
         <Section label="Authentication" />
-        <Part4Authentication />
+        <Part4Authentication atoms={atoms} />
         <Section label="Password Manager Icon" />
-        <Part5PasswordManagerIcon />
+        <Part5PasswordManagerIcon atoms={atoms} />
     </>);
 }
