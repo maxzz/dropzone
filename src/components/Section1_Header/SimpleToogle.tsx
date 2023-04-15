261a5a58d993fa5066521cd854f718baf83b2d1f
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-14 18:27:47 -0700

    WIP. app. SimpleToogle. 4 fix zoom scale

--- src/components/Section1_Header/SimpleToogle.tsx
+++ src/components/Section1_Header/SimpleToogle.tsx
@@ -4,16 +4,16 @@ import React, { useState } from "react";
 export function SimpleToogle() {
     const [on, setOn] = useState(false);
     return (
-        <div
+        <button
             className={classNames(
-                "p-1 h-6 w-10 ring-1 ring-inset rounded-full transition duration-150 ease-in-out pointer-events-auto",
+                "p-1 h-6 w-10 ring-1 ring-inset rounded-full transition duration-100 ease-in-out pointer-events-auto select-none",
                 "ring-primary-100",
                 on ? "bg-primary-300/50" : "bg-primary-700",
             )}
             onClick={() => setOn(v => !v)}
         >
-            <div className={classNames("h-4 w-4 rounded-full bg-white ring-1 ring-primary-700/40 shadow-sm transition duration-150 ease-out", on && " translate-x-4")}>
+            <div className={classNames("h-4 w-4 rounded-full bg-white ring-1 ring-primary-500/50 shadow-[0px_3px_3px_#0005] transition duration-100 ease", on && " translate-x-[100%]")}>
             </div>
-        </div>
+        </button>
     );
 }
