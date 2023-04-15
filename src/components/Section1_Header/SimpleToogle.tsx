84a6fe9b7b44de45987c89a9158d6d272de8bc61
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-14 18:12:03 -0700

    WIP. app. SimpleToogle. 3 w/ state

--- src/components/Section1_Header/SimpleToogle.tsx
+++ src/components/Section1_Header/SimpleToogle.tsx
@@ -1,13 +1,18 @@
+import { classNames } from "@/utils";
 import React, { useState } from "react";
 
 export function SimpleToogle() {
     const [on, setOn] = useState(false);
     return (
         <div
-            className="ml-auto p-1 h-6 w-10 bg-primary-300/50 ring-primary-100 ring-1 ring-inset rounded-full transition duration-150 ease-in-out pointer-events-auto"
+            className={classNames(
+                "p-1 h-6 w-10 ring-1 ring-inset rounded-full transition duration-150 ease-in-out pointer-events-auto",
+                "ring-primary-100",
+                on ? "bg-primary-300/50" : "bg-primary-700",
+            )}
             onClick={() => setOn(v => !v)}
         >
-            <div className={`h-4 w-4 rounded-full bg-white ring-1 ring-primary-700/40 shadow-sm transition duration-150 ease-out${on ? ' translate-x-4' : ''}`}>
+            <div className={classNames("h-4 w-4 rounded-full bg-white ring-1 ring-primary-700/40 shadow-sm transition duration-150 ease-out", on && " translate-x-4")}>
             </div>
         </div>
     );
