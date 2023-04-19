42df3bd7cc36d2a502ac7a416b14021d28a64de2
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-19 15:52:39 -0700

    WIP. FldCat. add FldCatDlg components

---
+++ src/components/Section2_Main/Panel2_Right/Body_Manifest/ManiSection1_Fields/Column4_Catalog/FldCatDlg/Body.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useAtom } from "jotai";
+import { Atomize } from "@/hooks/atomsX";
+import { Check } from "../../../ManiSection3_Policy/PolicyEditorDlg/ui-controls";
+import { DialogButtons, DialogHeader, SectionGenerationBy, SectionHistory, SectionMinMaxLength, SectionRuleTypes, SectionTestRoom } from "../../../ManiSection3_Policy/PolicyEditorDlg/ui-sections";
+import { classNames } from "@/utils";
+
+const frameClasses = "p-4 text-sm text-primary-400 bg-primary-800 border-primary-600/20 shadow-primary-700/30 border shadow rounded flex flex-col space-y-4";
+const sectionClasses = "text-sm font-bold border-primary-700 border-b";
+
+export function FldCatDlgBody() {
+    return (
+        <div className={frameClasses}>
+            {/* Header */}
+            <DialogHeader />
+
+            <div className={classNames("flex flex-col space-y-4")}>
+                {/* Predefined or Custom rule */}
+                <h2 className={sectionClasses}>Password complexity</h2>
+                <div className="">something</div>
+            </div>
+
+            {/* Buttons */}
+            <DialogButtons />
+        </div>
+    );
+}
---
+++ src/components/Section2_Main/Panel2_Right/Body_Manifest/ManiSection1_Fields/Column4_Catalog/FldCatDlg/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import * as Dialog from '@radix-ui/react-dialog';
+import { config, useTransition, a } from "@react-spring/web";
+import { FldCatDlgBody } from "./Body";
+
+export function FldCatDlg() {
+    const [open, setOpen] = React.useState(false);
+    
+    const transitions = useTransition(Number(open), {
+        from: { opacity: 0, y: -10, scale: 0.97 },
+        enter: { opacity: 1, y: 0, scale: 1 },
+        leave: { opacity: 0, y: 10, scale: 0.97 },
+        config: config.stiff,
+    });
+
+    return (
+        <Dialog.Root open={open} onOpenChange={setOpen}>
+            <Dialog.Trigger className="px-4 py-3 text-primary-300 border-primary-500 active:scale-[.97] border rounded select-none">
+                Edit
+            </Dialog.Trigger>
+
+            {transitions((styles, item) => (
+                !item
+                    ? null
+                    : <>
+                        <Dialog.Portal container={document.getElementById('portal')}>
+                            <a.div className="fixed inset-0 bg-primary-900/80" style={{ opacity: styles.opacity, }} />
+
+                            <Dialog.Content forceMount asChild className="fixed inset-0 flex justify-center items-center">
+                                <a.div style={styles}>
+                                    <FldCatDlgBody />
+                                </a.div>
+                            </Dialog.Content>
+
+                        </Dialog.Portal>
+                    </>
+            ))}
+        </Dialog.Root>
+    );
+}
