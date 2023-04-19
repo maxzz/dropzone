adeef262505010681728637a58632abf940d4e7f
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-19 16:42:54 -0700

    WIP. FldCatDlg. add real FldCatItemsGrid component

--- src/components/Section2_Main/Panel2_Right/Body_Manifest/ManiSection1_Fields/Column4_Catalog/FldCatDlg/Body.tsx
+++ src/components/Section2_Main/Panel2_Right/Body_Manifest/ManiSection1_Fields/Column4_Catalog/FldCatDlg/Body.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { useAtom } from "jotai";
-import { Atomize } from "@/hooks/atomsX";
-import { Check } from "../../../ManiSection3_Policy/PolicyEditorDlg/ui-controls";
-import { DialogButtons, DialogHeader, SectionGenerationBy, SectionHistory, SectionMinMaxLength, SectionRuleTypes, SectionTestRoom } from "../../../ManiSection3_Policy/PolicyEditorDlg/ui-sections";
+import { DialogButtons, DialogHeader } from "../../../ManiSection3_Policy/PolicyEditorDlg/ui-sections";
 import { classNames } from "@/utils";
+import { FldCatItemsGrid } from "./FldCatItemsGrid";
 
 const frameClasses = "p-4 text-sm text-primary-400 bg-primary-800 border-primary-600/20 shadow-primary-700/30 border shadow rounded flex flex-col space-y-4";
 const sectionClasses = "text-sm font-bold border-primary-700 border-b";
@@ -14,10 +12,12 @@ export function FldCatDlgBody() {
             {/* Header */}
             <DialogHeader header="Field Catalog" />
 
-            <div className={classNames("flex flex-col space-y-4")}>
-                {/* Predefined or Custom rule */}
+            <div>
                 <h2 className={sectionClasses}>Catalog items</h2>
-                <div className="">something</div>
+                
+                <div className="max-h-[60vh] min-w-[500px] overflow-y-auto">
+                    <FldCatItemsGrid />
+                </div>
             </div>
 
             {/* Buttons */}
---
+++ src/components/Section2_Main/Panel2_Right/Body_Manifest/ManiSection1_Fields/Column4_Catalog/FldCatDlg/FldCatItemsGrid.tsx
@@ -0,0 +1,57 @@
+import React, { Fragment } from "react";
+import { useAtomValue } from "jotai";
+import { CatalogItem, FldCatItemsAtom } from "@/store";
+import { fieldIcons } from "@/components/Section2_Main/Panel1_FilesList/Card/Card2_FormBody/CardFormBody2_Fields/FieldRowTypeIcon";
+import { Scroller } from "@/components/Section2_Main/Panel2_Right/Scroller";
+
+function FieldIcon(isPsw: boolean | undefined, className: string) {
+    const type = isPsw ? 'psw' : 'edit';
+    const Icon = fieldIcons[type]?.({ className, title: `Field type: ${type}`, }) || <div className="text-red-500">nan</div>;
+    return Icon;
+}
+
+function TableHeader() {
+    return (<>
+        <div className="col-start-2 mb-2 text-[.65rem] text-right text-primary-400 border-primary-100 border-b select-none">#</div>
+        {/* <div className="col-start-3 mb-2 text-[.65rem] text-primary-400 border-primary-100 border-b select-none">Type</div> */}
+        <div className="col-start-3 mb-2 text-[.65rem] text-primary-400 border-primary-100 border-b select-none">Name</div>
+        <div className="col-start-4 mb-2 text-[.65rem] text-primary-400 border-primary-100 border-b select-none">ID</div>
+    </>);
+}
+
+export function FldCatItemsGrid() {
+    const names = useAtomValue(FldCatItemsAtom);
+    return (
+        <div className="grid grid-rows-[auto_minmax(0,1fr)] overflow-hidden">
+
+            <div className="px-4 py-3 text-xs text-primary-400 bg-primary-800 border-primary-700 border-b">
+                {/* filters */}
+                {names.length} item{names.length === 1 ? '' : 's'}
+            </div>
+
+            <Scroller className="pt-2 text-xs text-primary-100">
+                <div className="grid grid-cols-[1fr_auto_auto_auto_1fr] gap-x-4 text-primary-400">
+                    <TableHeader />
+                    {names.map(mapItem)}
+                </div>
+            </Scroller>
+        </div>
+    );
+
+    function mapItem(item: CatalogItem, idx: number) {
+        return (
+            <Fragment key={idx}>
+                <div className="col-start-2 text-right">{idx + 1}</div>
+
+                <div className="col-start-3 flex items-center gap-x-2 leading-[18px]">
+                    {FieldIcon(item.password, "w-4 h-4 opacity-25")}
+                    <div className="">{item.dispname}</div>
+                </div>
+
+                <div className="col-start-4 font-mono text-[.6rem]">
+                    {item.dbname}
+                </div>
+            </Fragment>
+        );
+    }
+}
