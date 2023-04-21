cd7ca7df4f244c14271044d768aad76c93fcf90f
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-21 14:39:14 -0700

    WIP. menu. update actions

--- src/components/Section2_Main/Panel2_Right/Body_CaptionActions/index.tsx
+++ src/components/Section2_Main/Panel2_Right/Body_CaptionActions/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { IconMenuHamburger } from '@ui/icons';
 import { FileUsAtomType } from '@/store';
 import { ButtonCardEdit, ButtonCardOpenUrl } from './caption-buttons';
 import { CardTitleMenu } from './menu';
-
-const iconMenuHamburgerClasses = 'w-8 h-8 p-1 stroke-[0.8] hover:bg-primary-700 rounded opacity-60 hover:opacity-100 active:scale-[.97] outline-none focus:ring-1 ring-primary-400';
 
 export function Body_CaptionActions({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     return (
         <div className="flex items-center">
             <ButtonCardEdit fileUsAtom={fileUsAtom} formIdx={0} />
             <ButtonCardOpenUrl fileUsAtom={fileUsAtom} />
-
-            <CardTitleMenu fileUsAtom={fileUsAtom} icon={<IconMenuHamburger className={iconMenuHamburgerClasses} />} />
+            <CardTitleMenu fileUsAtom={fileUsAtom} />
         </div>
     );
 }
--- src/components/Section2_Main/Panel2_Right/Body_CaptionActions/menu/index.tsx
+++ src/components/Section2_Main/Panel2_Right/Body_CaptionActions/menu/index.tsx
@@ -2,44 +2,39 @@ import React from 'react';
 import { useAtomValue } from 'jotai';
 import { OldOverlay_Dialog } from '@ui/UIDialog';
 import { MenuTrigger, MenuPortal, MenuContent, MenuItem, MenuSeparator, Menu } from '@ui/UiDropdownMenu';
+import { IconMenuHamburger } from '@ui/icons';
 import toast from 'react-hot-toast';
 import { FileUsAtomType } from '@/store';
 import { TestDialogContent, saveXmlFile } from './menu-commands';
 
-export function CardTitleMenu({ fileUsAtom, icon }: { fileUsAtom: FileUsAtomType; icon: React.ReactNode; }) {
+const iconMenuHamburgerClasses = 'w-8 h-8 p-1 stroke-[0.8] hover:bg-primary-700 rounded opacity-60 hover:opacity-100 active:scale-[.97] outline-none focus:ring-1 ring-primary-400';
+
+export function CardTitleMenu({ fileUsAtom }: { fileUsAtom: FileUsAtomType; }) {
     const fileUs = useAtomValue(fileUsAtom);
     return (
         <Menu>
             <MenuTrigger>
-                {icon}
+                <IconMenuHamburger className={iconMenuHamburgerClasses} />
             </MenuTrigger>
 
             <MenuPortal container={document.getElementById('portal')}>
                 <MenuContent sideOffset={5}>
 
-                    <OldOverlay_Dialog trigger={
-                        <MenuItem className="!text-sm">
-                            Menu item as dialog trigger
-                        </MenuItem>
-                    }>
+                    <OldOverlay_Dialog
+                        trigger={
+                            <MenuItem className="!text-sm">
+                                Menu item as dialog trigger
+                            </MenuItem>
+                        }
+                    >
                         <TestDialogContent />
                     </OldOverlay_Dialog>
 
-                    <MenuItem className="!text-sm"
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            saveXmlFile(fileUs);
-                        }}
-                    >
+                    <MenuItem className="!text-sm" onClick={commandSaveXml}>
                         Convert manual to normal
                     </MenuItem>
 
-                    <MenuItem className="!text-sm"
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            toast('Not imlemented yet', { style: { backgroundColor: 'tomato' } });
-                        }}
-                    >
+                    <MenuItem className="!text-sm" onClick={commandMerge}>
                         Merge two manifests
                     </MenuItem>
 
@@ -49,4 +44,14 @@ export function CardTitleMenu({ fileUsAtom, icon }: { fileUsAtom: FileUsAtomType
             </MenuPortal>
         </Menu>
     );
+
+    function commandSaveXml(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        event.stopPropagation();
+        saveXmlFile(fileUs);
+    }
+
+    function commandMerge(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        event.stopPropagation();
+        toast('Not imlemented yet', { style: { backgroundColor: 'tomato' } });
+    }
 }
