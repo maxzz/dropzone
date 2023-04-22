490d93a2427be89afa49bb4df5b95c528c37d068
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-22 15:47:23 -0700

    WIP. store. clean up store indices

--- src/store/atoms/index.tsx
+++ src/store/atoms/index.tsx
@@ -1,47 +1,6 @@
 export * from './atoms-files';
 export * from './atoms-file-catalog';
-
-export {
-    showManiAtoms,
-    totalManiAtoms,
-
-    hasFilesAtom,
-
-    busyAtom,
-
-    allCards,
-
-    selected4ActionAtom,
-
-    splitPaneAtom,
-
-    searchFilterData,
-    sortByAtom,
-    orderAtom,
-
-    uiSizeAtom,
-
-} from './atoms-ui-state';
-
-export type {
-    ManiOpenSections,
-} from './atoms-ui-right-panel';
-
-export {
-    ViewMode,
-    rightPanelData,
-    maniOpenSections,
-} from './atoms-ui-right-panel';
-
-export {
-    doSetCurrentCardAtom,
-} from './atoms-selections';
-
-export type {
-    EditorData,
-} from './atoms-dialog-manifest';
-
-export {
-    formEditorDataAtom,
-} from './atoms-dialog-manifest';
-
+export * from './atoms-ui-state';
+export * from './atoms-ui-right-panel';
+export * from './atoms-selections';
+export * from './atoms-dialog-manifest';
