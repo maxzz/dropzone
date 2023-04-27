d1a6fe179323d5c35938c3cd21ad58b315fc35b4
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-26 18:13:16 -0700

    WIP. FldCatDlgBody. switch selected item layout to grid

--- src/components/Section4_Dialogs/FldCatDlg/Body.tsx
+++ src/components/Section4_Dialogs/FldCatDlg/Body.tsx
@@ -7,10 +7,9 @@ import { classNames, turnOffAutoComplete } from "@/utils";
 
 const gridFrameClasses = 'p-4 text-sm text-primary-400 bg-primary-800 border-primary-600/20 shadow-primary-700/30 border shadow rounded flex flex-col space-y-4';
 const dlgHeaderClasses = 'my-1 text-xs font-thin text-primary-400 bg-primary-800';
-const subSectionClasses = 'mt-4 pb-1 text-xs text-primary-400 bg-primary-800 border-primary-700 border-b';
+const subSectionClasses = 'mt-4 pb-0.5 text-sm text-primary-400 bg-primary-800 border-primary-700 border-b';
 
 function SubTitleA() {
-    const totalItems = useAtomValue(fldCatItemsAtom).length;
     return (
         <div className={subSectionClasses}>
             Catlog items
@@ -19,7 +18,6 @@ function SubTitleA() {
 }
 
 function SubTitleB() {
-    const totalItems = useAtomValue(fldCatItemsAtom).length;
     return (
         <div className={subSectionClasses}>
             Selected item
@@ -53,7 +51,7 @@ const inputFocusClasses = "focus:outline-none focus:ring-1 focus:ring-primary-40
 
 export function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
     return (
-        <input className={classNames("px-2 py-1.5 text-primary-300 bg-primary-700 rounded", inputFocusClasses, className,)} {...rest} />
+        <input className={classNames("px-2 py-1.5 w-full text-primary-300 bg-primary-700 rounded", inputFocusClasses, className,)} {...rest} />
     );
 }
 
@@ -79,28 +77,28 @@ function SelectedItemData({ selectedItemAtom }: { selectedItemAtom: PrimitiveAto
         setOwnernote(selectedItem?.ownernote || JSON.stringify(selectedItem || {}));
     }, [selectedItem]);
 
+    const itemClasses = "flex flex-col items-start";
+
     return (
-        <div className="text-xs grid">
+        <div className="mt-2 text-xs grid grid-cols-[min-content_auto_10rem] gap-x-2">
 
-            <div className="text-xs flex space-x-2">
-                <div className="flex flex-col items-start">
-                    <div className="">Type</div>
-                    <Input className="w-[3rem]" value={localType} onChange={(e) => setLocalType(e.target.value)} {...turnOffAutoComplete} />
-                </div>
-
-                <div className="flex flex-col items-start">
-                    <div className="">Name</div>
-                    <Input value={localName} onChange={(e) => setLocalName(e.target.value)} {...turnOffAutoComplete} />
-                </div>
-
-                <div className="flex flex-col items-start">
-                    <div className="">Value</div>
-                    <Input value={localValue} onChange={(e) => setLocalValue(e.target.value)} {...turnOffAutoComplete} />
-                </div>
+            <div className={itemClasses}>
+                <div>Type</div>
+                <Input className="w-[3rem]" value={localType} onChange={(e) => setLocalType(e.target.value)} {...turnOffAutoComplete} />
             </div>
 
-            <div className="mt-2 flex flex-col items-start">
-                <div className="">Description</div>
+            <div className={itemClasses}>
+                <div>Name</div>
+                <Input value={localName} onChange={(e) => setLocalName(e.target.value)} {...turnOffAutoComplete} />
+            </div>
+
+            <div className={itemClasses}>
+                <div>Value</div>
+                <Input value={localValue} onChange={(e) => setLocalValue(e.target.value)} {...turnOffAutoComplete} />
+            </div>
+
+            <div className={`mt-2 col-span-full ${itemClasses}`}>
+                <div>Description</div>
                 <textarea
                     className="p-1 w-full min-h-[3rem] text-[.65rem] leading-3 bg-primary-700 rounded" rows={3}
                     value={ownernote}
