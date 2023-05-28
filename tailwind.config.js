73e8110c1add3a3de4f200510710435f708ccc8f
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-27 18:16:32 -0700

    WIP. add tailwind customAdditionsPlugin()

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,33 @@
 const plugin = require("tailwindcss/plugin");
 const colors = require('tailwindcss/colors');
 
+const customAdditionsPlugin = plugin(function ({ addComponents }) {
+    const newComponents = {
+        ".scrollbar-w-2::-webkit-scrollbar": {
+            width: ".5rem !important",
+            height: ".5rem !important",
+        },
+        ".scrollbar-thumb-rounded::-webkit-scrollbar-thumb": {
+            borderRadius: ".25rem !important",
+        },
+        ".scrollbar-thumb-gray::-webkit-scrollbar-thumb": {
+            backgroundColor: "rgba(156, 163, 175, 1) !important",
+        },
+        ".scrollbar-track-gray-lighter::-webkit-scrollbar-track": {
+            backgroundColor: "rgba(209, 213, 219, 1) !important",
+        },
+        ".scrollbar-thumb-transparent::-webkit-scrollbar-thumb": {
+            backgroundColor: "transparent",
+        },
+        ".scrollbar-track-transparent::-webkit-scrollbar-track": {
+            backgroundColor: "transparent",
+        },
+    };
+    addComponents(newComponents, {
+        variants: ["responsive"],
+    });
+});
+
 module.exports = {
     content: ['./index.html', './src/**/*.{tsx,ts,jsx,js}'],
     //darkMode: false, // or 'media' or 'class'
@@ -20,32 +47,7 @@ module.exports = {
         extend: {},
     },
     plugins: [
-        plugin(function ({ addComponents }) {
-            const newComponents = {
-                ".scrollbar-w-2::-webkit-scrollbar": {
-                    width: ".5rem !important",
-                    height: ".5rem !important",
-                },
-                ".scrollbar-thumb-rounded::-webkit-scrollbar-thumb": {
-                    borderRadius: ".25rem !important",
-                },
-                ".scrollbar-thumb-gray::-webkit-scrollbar-thumb": {
-                    backgroundColor: "rgba(156, 163, 175, 1) !important",
-                },
-                ".scrollbar-track-gray-lighter::-webkit-scrollbar-track": {
-                    backgroundColor: "rgba(209, 213, 219, 1) !important",
-                },
-                ".scrollbar-thumb-transparent::-webkit-scrollbar-thumb": {
-                    backgroundColor: "transparent",
-                },
-                ".scrollbar-track-transparent::-webkit-scrollbar-track": {
-                    backgroundColor: "transparent",
-                },
-            };
-            addComponents(newComponents, {
-                variants: ["responsive"],
-            });
-        }),
+        customAdditionsPlugin,
         require('./tailwind/tailwnid-plugin-debug-styles'),
         require('./tailwind/tailwind-plugin-debug-screens'),
         require('./tailwind/tailwind-plugin-colors-bridge')([
