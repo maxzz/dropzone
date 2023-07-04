9cbb2235b407271a25589e5302f4ad1a51a530e2
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-03 20:18:10 -0700

    WIP. ---------- switch to pm-manifest npm package. move away old defs

---
+++ src/store/manifest_/index.ts
@@ -0,0 +1,7 @@
+export * from './mani-types';
+export * from './transforms';
+export * from './io/mani-i';
+export * from './io/mani-o';
+export * from './io/xml-to-js';
+export * from './io/file-to-meta';
+export * from './io/mani-show-error';
