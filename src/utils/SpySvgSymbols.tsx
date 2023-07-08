127d8a14ce20d01551bab68ce9c5410b994eb0a3
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-07 18:37:01 -0700

    WIP. icons. massage SpySvgSymbols styles

--- src/utils/SpySvgSymbols.tsx
+++ src/utils/SpySvgSymbols.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function SpySvgSymbols({ fontID = 'svgfont' }: { fontID?: string; }) {
     const [ids, setIds] = useState<string[]>([]);
@@ -9,21 +9,23 @@ export function SpySvgSymbols({ fontID = 'svgfont' }: { fontID?: string; }) {
         setIds(ids);
     }, []);
 
-    return (<>
-        {!!ids.length &&
-            <div className="w-[80vw] mx-auto mt-4 grid grid-cols-[repeat(auto-fill,minmax(0,64px))] gap-4">
-                {ids.map((id, idx) => (
-                    <div className="">
-                        <div className="w-16 h-16 border-4 border-gray-100 bg-gray-300 shadow-lg" key={idx}>
-                            <svg fill="#c3b2d3" stroke="black" strokeWidth={.5} className={`w-full h-full`}>
-                                <title>{`${id}`}</title>
-                                <use xlinkHref={`#${id}`} />
-                            </svg>
-                        </div>
-                        <div className="text-primary-100 text-[.65rem]">{id}</div>
+    if (!ids.length) {
+        return null;
+    }
+
+    return (
+        <div className="mx-auto mt-4 py-4 w-[80vw] grid grid-cols-[repeat(auto-fill,minmax(0,64px))] gap-2">
+            {ids.map((id, idx) => (
+                <div key={idx}>
+                    <div className="w-16 h-16 bg-primary-300 border-primary-700 border-4 rounded">
+                        <svg className="w-full h-full fill-[#c3b2d3] stroke-black stroke-[.5]">
+                            <title>{`${id}`}</title>
+                            <use xlinkHref={`#${id}`} />
+                        </svg>
                     </div>
-                ))}
-            </div >
-        }
-    </>);
+                    <div className="min-h-[2rem] text-[.65rem] text-primary-100 text-center">{id}</div>
+                </div>
+            ))}
+        </div>
+    );
 }
