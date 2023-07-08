0d86749c56e4f8742700a697829e5ef096c66a51
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-07 18:15:50 -0700

    WIP. icons. split monolith. 12. add labels to SpySvgSymbols

--- src/utils/SpySvgSymbols.tsx
+++ src/utils/SpySvgSymbols.tsx
@@ -2,22 +2,27 @@ import React, { useEffect, useState } from 'react';
 
 export function SpySvgSymbols({ fontID = 'svgfont' }: { fontID?: string; }) {
     const [ids, setIds] = useState<string[]>([]);
+
     useEffect(() => {
-        const font = document.querySelector(`#${fontID} > defs`)?.children;
-        const ids = (font ? [...font] : []).map(item => item.id);
+        const fontElChildren = document.querySelector(`#${fontID} > defs`)?.children;
+        const ids = (fontElChildren ? [...fontElChildren] : []).map(item => item.id);
         setIds(ids);
     }, []);
+
     return (<>
         {!!ids.length &&
             <div className="w-[80vw] mx-auto mt-4 grid grid-cols-[repeat(auto-fill,minmax(0,64px))] gap-4">
-                {ids.map((id, idx) =>
-                    <div className="w-16 h-16 border-4 border-gray-100 bg-gray-300 shadow-lg" key={idx}>
-                        <svg fill="#c3b2d3" stroke="black" strokeWidth={.5} className={`w-full h-full`}>
-                            <title>{`${id}`}</title>
-                            <use xlinkHref={`#${id}`} />
-                        </svg>
+                {ids.map((id, idx) => (
+                    <div className="">
+                        <div className="w-16 h-16 border-4 border-gray-100 bg-gray-300 shadow-lg" key={idx}>
+                            <svg fill="#c3b2d3" stroke="black" strokeWidth={.5} className={`w-full h-full`}>
+                                <title>{`${id}`}</title>
+                                <use xlinkHref={`#${id}`} />
+                            </svg>
+                        </div>
+                        <div className="text-primary-100 text-[.65rem]">{id}</div>
                     </div>
-                )}
+                ))}
             </div >
         }
     </>);
