3b366bc8bf94b7438b4bb368a31acf6b24cef779
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-13 18:53:19 -0700

    WIP. doSetFilesAtom code review

--- src/store/atoms/0-files-atom/1-do-set-files.ts
+++ src/store/atoms/0-files-atom/1-do-set-files.ts
@@ -6,32 +6,37 @@ import { _foldAllCardsAtom } from '../9-ui-state';
 import { rightPanelData } from '../2-right-panel';
 import { uuid } from '@/utils';
 
+function pathWoFilename(file: File): string {
+    const rv = ((file as any).path as string || '')
+        .replace(/^\//, '')
+        .split(/[\\\/]/);
+    rv.pop(); // remove filename as the last item
+    return rv.join('/');
+}
+
 export const doSetFilesAtom = atom(
     null,
     (get, set, accepterFiles: File[]) => {
         const droppedIn: FileUsAtomType[] = accepterFiles
             .filter((file) => file.size)
             .map(
-                (file, idx) => {
-                    const path = ((file as any).path as string || '').replace(/^\//, '').split(/[\\\/]/);
-                    path.pop(); // remove file name as the last item
-                    
-                    const newItem: FileUs = {
+                (fileHandle, idx) => {
+                    const newFileUs: FileUs = {
                         id: uuid(),
                         idx,
-                        fname: file.name,
-                        fpath: path.join('/'),
-                        fmodi: (file as any).lastModified || 0,
-                        modified: file.lastModified,
-                        size: file.size,
-                        file: file,
+                        fname: fileHandle.name,
+                        fpath: pathWoFilename(fileHandle),
+                        fmodi: (fileHandle as any).lastModified || 0,
+                        modified: fileHandle.lastModified,
+                        size: fileHandle.size,
+                        file: fileHandle,
                         state: {
                             isGroupAtom: atom<boolean>(false),
                             isCurrentAtom: atom<boolean>(false),
                         },
                         stats: {} as FileUsStats, // the real one will be assigned after caching content
                     };
-                    return atom<FileUs>(newItem);
+                    return atom<FileUs>(newFileUs);
                 }
             );
 
