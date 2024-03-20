5a4aecff9599e4d7d23041d992728d101849ba2b
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-20 16:19:11 -0700

    WIP. search. FilterSearch code review

--- src/components/1-header/fiters/1-filter-search/1-filter-search.tsx
+++ src/components/1-header/fiters/1-filter-search/1-filter-search.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { useState, useRef } from 'react';
 import { useAtom } from 'jotai';
 import { useKey } from 'react-use';
-import { turnOffAutoComplete } from '@/utils';
-import { searchFilterData } from '@/store';
-import { IconCaseSensitive, IconClose, SymbolDot, IconSearch } from '@ui/icons';
 import { UiTip } from '@ui/ui-tooltip';
 import { Config } from 'react-popper-tooltip';
+import { searchFilterData } from '@/store';
+import { IconCaseSensitive, IconClose, SymbolDot, IconSearch } from '@ui/icons';
+import { classNames, turnOffAutoComplete } from '@/utils';
 
 function ToggleCaseSensitive() {
     const [cs, setCs] = useAtom(searchFilterData.caseSensitiveAtom);
@@ -20,54 +20,52 @@ function ToggleCaseSensitive() {
     );
 }
 
-function TipTrigger() {
+function InputAndTrigger() {
     const [filterTxt, setFilterTxt] = useAtom(searchFilterData.textAtom);
     const isEmpty = !filterTxt;
 
-    const [isActive, setIsActive] = React.useState(false);
-    const keyboardRef = React.useRef<HTMLInputElement>(null);
+    const [isActive, setIsActive] = useState(false);
+    const keyboardRef = useRef<HTMLInputElement>(null);
 
     useKey('Escape', () => setFilterTxt(''), { target: keyboardRef.current });
     useKey((event) => event.ctrlKey && event.key === 'd', (event) => { event.preventDefault(); keyboardRef.current && keyboardRef.current.focus(); });
-    return (
-        <div className={``}>
-            <div
-                className={`${isActive ? 'h-8' : 'h-7 py-0.5'} px-2 flex items-center bg-gray-700 focus-within:bg-gray-600 border-2 ${isEmpty ? 'w-12 rounded-full' : 'w-full rounded-md'}`}
-                onFocus={() => setIsActive(true)}
-                onBlur={() => setIsActive(false)}
-                onClick={() => keyboardRef.current && keyboardRef.current.focus()}
-            >
-                <input
-                    ref={keyboardRef}
-                    className="w-full h-6 text-sm text-gray-200 bg-transparent focus:outline-none"
-                    value={filterTxt}
-                    onChange={(event) => setFilterTxt(event.target.value)}
-                    {...turnOffAutoComplete}
-                />
 
-                {isEmpty
-                    ? (
-                        // Ctrl+D and Search icon
-                        <div className="flex-none relative">
-                            {/* Ctrl+D */}
-                            {/* 
-                            {!active && (
+    return (
+        <div
+            className={classNames(isActive ? "h-8" : "h-7 py-0.5", isEmpty ? "w-12 rounded-full" : "w-full rounded-md", "px-2 flex items-center bg-gray-700 focus-within:bg-gray-600 border-2")}
+            onFocus={() => setIsActive(true)}
+            onBlur={() => setIsActive(false)}
+            onClick={() => keyboardRef.current && keyboardRef.current.focus()}
+        >
+            <input
+                ref={keyboardRef}
+                className="w-full h-6 text-sm text-gray-200 bg-transparent focus:outline-none"
+                value={filterTxt}
+                onChange={(event) => setFilterTxt(event.target.value)}
+                {...turnOffAutoComplete}
+            />
+
+            {isEmpty
+                ? (
+                    // Ctrl+D and Search icon
+                    <div className="flex-none relative">
+                        {/* shortcut icon small 'Ctrl' and text 'D' */}
+                        {/* 
+                            {!isActive && (
                                 <div className="absolute -left-3.5 -top-0.5 flex flex-col items-center text-gray-400 pointer-events-none">
-                                    <IconCtrl className="w-3 h-3" />
+                                    <IconCtrl className="size-3" />
                                     <div className="text-[.5rem] leading-[.5rem]">D</div>
                                 </div>
                             )}
-                            */}
-                            <IconSearch className="w-4 h-4" />
-                        </div>
-                    )
-                    : (<>
-                        <ToggleCaseSensitive />
-                        <IconClose onClick={() => setFilterTxt('')} className="w-6 h-6 p-0.5 cursor-pointer" />
-                    </>)
-
-                }
-            </div>
+                        */}
+                        <IconSearch className="size-4" />
+                    </div>
+                )
+                : (<>
+                    <ToggleCaseSensitive />
+                    <IconClose onClick={() => setFilterTxt('')} className="size-6 p-0.5 cursor-pointer" />
+                </>)
+            }
         </div>
     );
 }
@@ -98,7 +96,7 @@ const popperOptions: Config = {
 export function FilterSearch() {
     return (
         <div className="flex-1 min-h-[32px] max-w-[40rem] ml-2 md:ml-4 sm:self-stretch md:self-end md:pb-2 lg:pb-0 lg:self-auto flex justify-end items-center">
-            <UiTip trigger={<TipTrigger />} arrow popperOptions={popperOptions}>
+            <UiTip trigger={<InputAndTrigger />} popperOptions={popperOptions} arrow>
                 <PopupContent />
             </UiTip>
         </div>
