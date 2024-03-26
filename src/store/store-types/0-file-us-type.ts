b2293ea33fb9aede96736077c13c0e132025c1a2
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-26 16:56:40 -0700

    WIP. store. update FileUsStats description

--- src/store/store-types/0-file-us-type.ts
+++ src/store/store-types/0-file-us-type.ts
@@ -7,42 +7,42 @@ export type FileUsState = {
 };
 
 export type FileUsStats = {
-    domain?: string;
+    domain?: string;                    // fileUs.meta?.[0]?.disp.domain
 
-    isWeb: boolean;
-    isChrome: boolean;
-    isFCat: boolean;
-    isCustomization: boolean;
+    isWeb: boolean;                     // is web apploication
+    isChrome: boolean;                  // is web apploication and not IE; isWeb && !fileUs.meta?.[0]?.disp.isIe
+    isFCat: boolean;                    // is field catalog
+    isCustomization: boolean;           // !fileUs.meta?.length && !!fileUs.mani?.options
 
-    url?: string;
-    title?: string;
+    url?: string;                       // loginForm?.detection.web_ourl
+    title?: string;                     // loginForm?.options.choosename
     
-    isSubFolder?: boolean;      // Now it's a simple check to see if the path is in front of the filename.
-    subFolder?: string;         // This is now the full path available from the browser, i.e. not a subfolder.
+    isSubFolder?: boolean;              // Now it's a simple check to see if the path is in front of the filename.
+    subFolder?: string;                 // This is now the full path available from the browser, i.e. not a subfolder.
     
-    dateCreated?: string;
-    dateModified?: string;
+    dateCreated?: string;               // TimeUtils.dpTimeToShow(fileUs.mani?.descriptor?.created)
+    dateModified?: string;              // TimeUtils.dpTimeToShow(fileUs.mani?.descriptor?.modified)
 };
 
 export type FileUs = {
-    id: string;
-    idx: number;                // index in the loaded list wo/ counting on filters, i.e. absolute index
+    id: string;                         // unique id
+    idx: number;                        // index in the loaded list wo/ counting on filters, i.e. absolute index
 
-    fname: string;              // filename
-    fpath: string;              // file relative path to the dropped folder
-    fmodi: number;              // file.lastModified
-    size: number;               // file size
+    fname: string;                      // filename
+    fpath: string;                      // file relative path to the dropped folder
+    fmodi: number;                      // file.lastModified
+    size: number;                       // file size
 
-    raw?: string;               // raw manifest as it was loaded
-    mani?: Mani.Manifest;       // json raw manifest
-    meta?: Meta.Form[];         // meta data on manifest
+    raw?: string;                       // raw manifest as it was loaded
+    mani?: Mani.Manifest;               // json raw manifest
+    meta?: Meta.Form[];                 // meta data on manifest
 
-    fcat?: CatalogFile.Root;    // field catalog
+    fcat?: CatalogFile.Root;            // field catalog
 
-    file?: File;                // file OS handle
+    file?: File;                        // file OS handle
 
-    state: FileUsState;         // local state atoms: is currnet; is selected
-    stats: FileUsStats;         // quick access statistics
+    state: FileUsState;                 // local state atoms: is currnet; is selected
+    stats: FileUsStats;                 // quick access statistics
 };
 
 export type FileUsAtomType = WritableAtom<FileUs, [FileUs], void>;
