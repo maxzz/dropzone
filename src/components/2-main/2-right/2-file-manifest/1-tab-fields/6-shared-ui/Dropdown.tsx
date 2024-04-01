9a9cbdca2f9b45c7bb7b08a1739bd89a0a30b102
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-31 19:20:10 -0700

    WIP. mani view. Dropdown code review

--- src/components/2-main/2-right/2-file-manifest/1-tab-fields/6-shared-ui/Dropdown.tsx
+++ src/components/2-main/2-right/2-file-manifest/1-tab-fields/6-shared-ui/Dropdown.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
 import { PrimitiveAtom } from 'jotai';
 import { classNames } from '@/utils';
-import { SymbolChevronDown, SymbolDot } from '@ui/icons';
 import * as menu from '@radix-ui/react-dropdown-menu';
-
-export function isKeyToClearDefault(key: string) {
-    return key === 'Backspace' || /^[a-z0-9]$/i.test(key);
-}
+import { SymbolChevronDown, SymbolDot } from '@ui/icons';
 
 const menuContentClasses = "\
-px-1.5 py-1 \
-max-h-[50vh] \
-grid grid-cols-1 \
+px-1.5 py-1 max-h-[50vh] \
+\
+bg-primary-100 \
+dark:bg-gray-800 \
+\
+rounded-lg shadow-md \
+overflow-auto smallscroll smallscroll-light \
 \
 radix-side-top:animate-slide-up \
 radix-side-bottom:animate-slide-down \
 \
-bg-primary-100 dark:bg-gray-800 \
-rounded-lg shadow-md \
-overflow-auto smallscroll smallscroll-light";
+grid grid-cols-1";
 //TODO: maybe have a separate popop for big list and add search; or simplescroll; more fields.. put on top?; scroll to view;
 
 const menuItemClasses = "\
-relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded-md outline-none \
-text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100 \
-";
+relative pl-8 pr-4 py-2 text-xs \
+text-primary-700 \
+data-highlighted:bg-primary-700 \
+data-highlighted:text-primary-100 \
+rounded-md outline-none select-none cursor-default \
+flex items-center";
 
 export function Dropdown(useItAtom: PrimitiveAtom<boolean>, items: string[], selectedIndex: number, onSetIndex: (idx: number) => void) {
     return (
         <menu.Root>
             <menu.Trigger asChild>
                 <button className="px-2 border-l border-primary-800 outline-none group">
-                    <SymbolChevronDown className="w-4 h-4 border-primary-500 rounded group-focus-within:border" />
+                    <SymbolChevronDown className="size-4 border-primary-500 rounded group-focus-within:border" />
                 </button>
             </menu.Trigger>
 
@@ -52,7 +52,7 @@ export function Dropdown(useItAtom: PrimitiveAtom<boolean>, items: string[], sel
                                     key={idx}
                                 >
                                     {isSelected && (
-                                        <SymbolDot className="absolute left-2 w-5 h-5 fill-primary-700" />
+                                        <SymbolDot className="absolute left-2 size-5 fill-primary-700" />
                                     )}
 
                                     <span className="flex-grow">
@@ -67,4 +67,8 @@ export function Dropdown(useItAtom: PrimitiveAtom<boolean>, items: string[], sel
             </menu.Portal>
         </menu.Root>
     );
+}
+
+export function isKeyToClearDefault(key: string) {
+    return key === 'Backspace' || /^[a-z0-9]$/i.test(key);
 }
