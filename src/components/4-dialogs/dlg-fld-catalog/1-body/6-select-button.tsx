9723eb3f6aa487e1d51a48536cf3683f22c5d002
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-01 15:27:12 -0700

    WIP. field catalog. split FldCatDlgBody monolith

---
+++ src/components/4-dialogs/dlg-fld-catalog/1-body/6-select-button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from "react";
+import { PrimitiveAtom, useAtomValue, useSetAtom } from "jotai";
+import { CatalogItem } from "@/store/manifest";
+import { closeFldCatDialogAtom } from "@/store";
+import { BottomButton } from "@/components/2-main/2-right/2-file-manifest/3-tab-policy/dlg-policy-editor/3-sections";
+
+export function SelectButton({ selectedItemAtom, ...rest }: { selectedItemAtom: PrimitiveAtom<CatalogItem | null>; } & ButtonHTMLAttributes<HTMLButtonElement>) {
+    const closeFldCatDialog = useSetAtom(closeFldCatDialogAtom);
+    const selectedItem = useAtomValue(selectedItemAtom);
+    return (
+        <BottomButton disabled={!selectedItem} onClick={() => closeFldCatDialog({ fldCatItem: selectedItem })} {...rest}>
+            Select
+        </BottomButton>
+    );
+}
