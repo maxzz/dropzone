6706f89324901a2fc7d4b1486804805d02651480
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-31 19:16:30 -0700

    WIP. mani view. Dropdown code review

--- src/components/2-main/2-right/2-file-manifest/1-tab-fields/6-shared-ui/Dropdown.tsx
+++ src/components/2-main/2-right/2-file-manifest/1-tab-fields/6-shared-ui/Dropdown.tsx
@@ -8,6 +8,24 @@ export function isKeyToClearDefault(key: string) {
     return key === 'Backspace' || /^[a-z0-9]$/i.test(key);
 }
 
+const menuContentClasses = "\
+px-1.5 py-1 \
+max-h-[50vh] \
+grid grid-cols-1 \
+\
+radix-side-top:animate-slide-up \
+radix-side-bottom:animate-slide-down \
+\
+bg-primary-100 dark:bg-gray-800 \
+rounded-lg shadow-md \
+overflow-auto smallscroll smallscroll-light";
+//TODO: maybe have a separate popop for big list and add search; or simplescroll; more fields.. put on top?; scroll to view;
+
+const menuItemClasses = "\
+relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded-md outline-none \
+text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100 \
+";
+
 export function Dropdown(useItAtom: PrimitiveAtom<boolean>, items: string[], selectedIndex: number, onSetIndex: (idx: number) => void) {
     return (
         <menu.Root>
@@ -18,33 +36,33 @@ export function Dropdown(useItAtom: PrimitiveAtom<boolean>, items: string[], sel
             </menu.Trigger>
 
             <menu.Portal container={document.getElementById('portal')}>
-                <menu.Content
-                    className={classNames(
-                        "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
-                        "px-1.5 py-1 grid grid-cols-1 rounded-lg shadow-md",
-                        "bg-primary-100 dark:bg-gray-800",
-                        "overflow-auto max-h-[50vh] smallscroll smallscroll-light" //TODO: maybe have a separate popop for big list and add search; or simplescroll; more fields.. put on top?; scroll to view;
-                    )}
-                >
-                    {items.map((item, idx) => {
-                        const isSelected = selectedIndex === idx;
-                        const isSeparator = item === '-';
-                        return isSeparator
-                            ? <menu.Separator className="my-1 h-px bg-gray-200 dark:bg-gray-700" key={idx} />
-                            :
-                            <menu.Item
-                                className={classNames(
-                                    "relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded-md outline-none",
-                                    "text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100",
-                                    isSelected && "bg-primary-300"
-                                )}
-                                onSelect={() => onSetIndex(idx)}
-                                key={idx}
-                            >
-                                {isSelected && <SymbolDot className="absolute left-2 w-5 h-5 fill-primary-700" />}
-                                <span className="flex-grow">{item}</span>
-                            </menu.Item>;
-                    })}
+                <menu.Content className={menuContentClasses}>
+
+                    {items.map(
+                        (item, idx) => {
+                            const isSelected = selectedIndex === idx;
+                            const isSeparator = item === '-';
+                            if (isSeparator) {
+                                return <menu.Separator className="my-1 h-px bg-gray-200 dark:bg-gray-700" key={idx} />;
+                            }
+                            return (
+                                <menu.Item
+                                    className={classNames(menuItemClasses, isSelected && "bg-primary-300")}
+                                    onSelect={() => onSetIndex(idx)}
+                                    key={idx}
+                                >
+                                    {isSelected && (
+                                        <SymbolDot className="absolute left-2 w-5 h-5 fill-primary-700" />
+                                    )}
+
+                                    <span className="flex-grow">
+                                        {item}
+                                    </span>
+                                </menu.Item>
+                            );
+                        })
+                    }
+
                 </menu.Content>
             </menu.Portal>
         </menu.Root>
