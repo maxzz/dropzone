72b21b89d1d9199bfd34a4b3537ccfe17f7177f1
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-31 19:01:19 -0700

    WIP. mani view. Column2_Label code review

--- src/components/2-main/2-right/2-file-manifest/1-tab-fields/2-column-label/index.tsx
+++ src/components/2-main/2-right/2-file-manifest/1-tab-fields/2-column-label/index.tsx
@@ -1,20 +1,34 @@
-import React, { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes } from 'react';
 import { PrimitiveAtom, useAtom } from 'jotai';
 import { classNames } from '@/utils';
 
-export function Column2_Label({ useItAtom, valueAtom, className, ...rest }: { useItAtom: PrimitiveAtom<boolean>; valueAtom: PrimitiveAtom<string>; } & InputHTMLAttributes<HTMLInputElement>) {
+type Column2_LabelProps =
+    & {
+        useItAtom: PrimitiveAtom<boolean>;
+        valueAtom: PrimitiveAtom<string>;
+    }
+    & InputHTMLAttributes<HTMLInputElement>;
+
+const Column2_LabelClasses = "\
+px-2 py-3 h-8 \
+\
+bg-primary-700 text-primary-200 \
+\
+ring-primary-600 \
+focus:ring-offset-primary-800 \
+focus:ring-primary-400 \
+focus:ring-1 \
+focus:ring-offset-1 \
+outline-none \
+rounded \
+";
+
+export function Column2_Label({ useItAtom, valueAtom, className, ...rest }: Column2_LabelProps) {
     const [value, setValue] = useAtom(valueAtom);
     const [useIt, setUseIt] = useAtom(useItAtom);
     return (
         <input
-            className={classNames(
-                "px-2 py-3 h-8",
-                "bg-primary-700 text-primary-200 focus:ring-offset-primary-800 ring-primary-600 focus:ring-primary-400",
-                "focus:ring-1 focus:ring-offset-1",
-                "outline-none rounded",
-                !useIt && "opacity-30 cursor-pointer",
-                className,
-            )}
+            className={classNames(Column2_LabelClasses, !useIt && "opacity-30 cursor-pointer", className)}
             value={value}
             onChange={(event) => setValue(event.target.value)}
             title={useIt ? "The label is shown to the user next to\nthe field for entering a value." : undefined}
