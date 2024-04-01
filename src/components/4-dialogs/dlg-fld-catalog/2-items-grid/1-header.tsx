62c60b81ee9f0922b532a2a85b4eed651d6e04e3
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-01 16:55:51 -0700

    WIP. field catalog. FldCatItemsGrid code clean up

--- src/components/4-dialogs/dlg-fld-catalog/2-items-grid/1-header.tsx
+++ src/components/4-dialogs/dlg-fld-catalog/2-items-grid/1-header.tsx
@@ -1,11 +1,17 @@
-export const rowClasses = 'px-2 py-px col-start-2 flex items-center space-x-2';
+export const rowClasses = "px-2 py-px col-start-2 flex items-center space-x-2";
 
-export const col1Classes = 'w-[4ch] text-right';
-export const col2Classes = 'w-[1.5rem] flex items-center justify-center gap-x-2 leading-[18px]';
-export const col3Classes = 'w-[41%] flex items-center gap-x-2 leading-[18px]';
-export const col4Classes = 'w-[44%] whitespace-nowrap font-mono text-[.6rem]';
+export const col1Classes = "w-[4ch] text-right";
+export const col2Classes = "w-[1.5rem] flex items-center justify-center gap-x-2 leading-[18px]";
+export const col3Classes = "w-[41%] flex items-center gap-x-2 leading-[18px]";
+export const col4Classes = "w-[44%] whitespace-nowrap font-mono text-[.6rem]";
 
-const tableHeaderClasses = 'mb-2 ml-1 text-[.65rem] text-primary-400 border-primary-100 border-b select-none';
+const tableHeaderClasses = "\
+ml-1 mb-2 text-[.65rem] \
+\
+text-primary-400 \
+border-primary-100 \
+border-b \
+select-none";
 
 export function TableHeader() {
     return (
