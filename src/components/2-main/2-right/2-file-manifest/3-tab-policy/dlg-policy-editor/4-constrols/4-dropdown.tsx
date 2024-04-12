521cacb18bf41cf7e52b42e9c8e8658db1f5999e
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-11 17:02:59 -0700

    WIP. mani-editor. update PolicyEditorDlg Dropdown

--- src/components/2-main/2-right/2-file-manifest/3-tab-policy/dlg-policy-editor/4-constrols/4-dropdown.tsx
+++ src/components/2-main/2-right/2-file-manifest/3-tab-policy/dlg-policy-editor/4-constrols/4-dropdown.tsx
@@ -1,17 +1,43 @@
 import { HTMLAttributes } from "react";
 import { PrimitiveAtom, useAtom } from "jotai";
 import * as Select from '@radix-ui/react-select';
-import { classNames } from '@/utils';
 import { SymbolChevronDown } from "@ui/icons";
 import { CheckIcon } from "@radix-ui/react-icons";
 
+const viewportClasses = "\
+px-1.5 py-1 \
+\
+bg-primary-100 dark:bg-gray-800 \
+\
+radix-side-top:animate-slide-up \
+radix-side-bottom:animate-slide-down \
+\
+rounded shadow-md \
+grid grid-cols-1";
+
+const selectItemClasses = "\
+relative pl-8 pr-4 py-2 text-xs \
+\
+text-primary-700 \
+\
+outline-none \
+focus:outline-none \
+focus:bg-primary-100 \
+\
+data-highlighted:bg-primary-700 \
+data-highlighted:text-primary-100 \
+\
+radix-disabled:opacity-50 \
+\
+rounded select-none cursor-default \
+flex items-center";
 
 export function Dropdown({ items, valueAtom, className }: { items: string[]; valueAtom: PrimitiveAtom<string>; } & HTMLAttributes<HTMLButtonElement>) {
     const [val, setVal] = useAtom(valueAtom);
     return (
         <Select.Root value={val} onValueChange={(v: string) => setVal(v)}>
             <Select.Trigger className={className}>
-                <div className="p-2 flex items-center justify-between space-x-1 text-primary-300 bg-primary-700 rounded">
+                <div className="p-2 text-primary-300 bg-primary-700 rounded flex items-center justify-between space-x-1">
                     <Select.Value />
                     <Select.Icon><SymbolChevronDown className="size-4" /></Select.Icon>
                 </div>
@@ -19,29 +45,18 @@ export function Dropdown({ items, valueAtom, className }: { items: string[]; val
 
             <Select.Portal container={document.getElementById('portal')}>
                 <Select.Content>
-                    <Select.Viewport className={classNames(
-                        "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
-                        "px-1.5 py-1 grid grid-cols-1 rounded shadow-md",
-                        "bg-primary-100 dark:bg-gray-800 rounded"
-                    )}
-                    >
-                        {items.map((item, idx) => (
-                            <Select.Item className={classNames(
-                                "relative pl-8 pr-4 py-2 text-xs flex items-center cursor-default select-none rounded outline-none",
-                                "text-primary-700 data-highlighted:bg-primary-700 data-highlighted:text-primary-100",
-                                "focus:bg-primary-100",
-                                "radix-disabled:opacity-50",
-                                "focus:outline-none select-none"
-                            )}
-                                value={`${idx}`}
-                                key={idx}
-                            >
-                                <Select.ItemText>{item}</Select.ItemText>
-                                <Select.ItemIndicator className="absolute left-2 inline-flex items-center">
-                                    <CheckIcon />
-                                </Select.ItemIndicator>
-                            </Select.Item>
-                        ))}
+                    <Select.Viewport className={viewportClasses}>
+                        {items.map(
+                            (item, idx) => (
+                                <Select.Item className={selectItemClasses} value={`${idx}`} key={idx}>
+                                    <Select.ItemText>{item}</Select.ItemText>
+
+                                    <Select.ItemIndicator className="absolute left-2 inline-flex items-center">
+                                        <CheckIcon />
+                                    </Select.ItemIndicator>
+                                </Select.Item>
+                            ))
+                        }
                     </Select.Viewport>
                 </Select.Content>
             </Select.Portal>
