3997510d7785b10e32ae81430f37f1b1f429cba6
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-17 15:27:58 -0700

    WIP. file list. update ManiFilenameParts params. code review

--- src/components/2-main/1-files-list/1-inline-card/1-title/4-filename/1-mani-filename-parts.tsx
+++ src/components/2-main/1-files-list/1-inline-card/1-title/4-filename/1-mani-filename-parts.tsx
@@ -4,35 +4,38 @@ type ParsedFnameParams = {
 };
 
 const partClasses = {
-    classAll: "text-[0.7rem] overflow-hidden whitespace-nowrap overflow-ellipsis", //text-primary-300/80
-    classSm: "opacity-50 font-sans text-[0.5rem]",
-    classXs: "opacity-30",
-    classLg: "px-px text-[0.72rem] text-primary-400 opacity-100 border-b border-dotted border-primary-500",
-    classL2: "px-1 text-[.65rem] font-bold text-gray-600 opacity-100", // large for ManiInfoTooltip
+    container: "text-[0.7rem] overflow-hidden whitespace-nowrap overflow-ellipsis", //text-primary-300/80
+    sm: "opacity-50 font-sans text-[0.5rem]",
+    xs: "opacity-30",
+    lg: "px-px text-[0.72rem] text-primary-400 opacity-100 border-b border-dotted border-primary-500",
+    l2: "px-1 text-[.65rem] font-bold text-gray-600 opacity-100", // large for ManiInfoTooltip
 };
 
 const reFilenameMatch = /^\{([0-9A-Za-z]{3,3})(.*)([0-9A-Za-z]{3,3})\}\.dpm$/; //TODO: handle '{guid} + extra.dpm' filenames
 
 export function ManiFilenameParts({ fname, large }: ParsedFnameParams) {
+    const { container, sm, xs, lg: large1, l2: large2 } = partClasses;
 
     const match = (fname || '').match(reFilenameMatch);
     if (!match) {
         return (
-            <div className={partClasses.classAll}>
-                <span className={partClasses.classSm}>{fname}</span>
+            <div className={container}>
+                <span className={sm}>
+                    {fname}
+                </span>
             </div>
         );
     }
 
-    const useLarge = large ? partClasses.classL2 : partClasses.classLg;
+    const lg = large ? large2 : large1;
 
     return (
-        <div className={partClasses.classAll}>
-            <span className={partClasses.classXs}>{'{'}</span>
-            <span className={useLarge}>{match[1]}</span>
-            <span className={partClasses.classSm}>{match[2]}</span>
-            <span className={useLarge}>{match[3]}</span>
-            <span className={partClasses.classXs}>{'}.dpm'}</span>
+        <div className={container}>
+            <span className={xs}>{'{'}</span>
+            <span className={lg}>{match[1]}</span>
+            <span className={sm}>{match[2]}</span>
+            <span className={lg}>{match[3]}</span>
+            <span className={xs}>{'}.dpm'}</span>
         </div>
     );
 }
