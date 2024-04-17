51bff224fc49dc8e771e0d777e8d7eb7ef5d4a69
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-17 16:32:10 -0700

    WIP. file list. update header controls. BtnPopupPool

--- src/components/2-main/1-files-list/1-inline-card/2-form-body/1-header/2-btn-popup-pool/index.tsx
+++ src/components/2-main/1-files-list/1-inline-card/2-form-body/1-header/2-btn-popup-pool/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { TransformEncoding } from '@/store/manifest';
 import { ToggleWithPortal } from '../4-shared-ui/2-toggle-w-portal';
 import { HeaderButton } from '../4-shared-ui/1-header-button';
@@ -7,8 +7,16 @@ export function BtnPopupPool({ names_ext }: { names_ext: string | undefined; }) 
     if (!names_ext) {
         return <HeaderButton text={"pool"} />;
     }
-    names_ext && (names_ext = TransformEncoding.persentRemove(TransformEncoding.xmlRestore(TransformEncoding.cppRestore(names_ext.replace(/:/g, '●'))))); // fix packed names
-    let items = (names_ext || '').split('●');
+
+    const prettifyPackedNames =
+        TransformEncoding.persentRemove(
+            TransformEncoding.xmlRestore(
+                TransformEncoding.cppRestore(names_ext.replace(/:/g, '●'))
+            )
+        );
+
+    let items = (prettifyPackedNames || '').split('●');
+
     return (
         <ToggleWithPortal text={"pool"}>
 
@@ -16,12 +24,21 @@ export function BtnPopupPool({ names_ext }: { names_ext: string | undefined; }) 
             <div className="mt-1 pl-4 pr-1 pb-1 pt-2 bg-primary-100 ring-1 ring-primary-400 rounded shadow-2xl">
                 <div className="max-w-sm max-h-[40vh] text-xs overflow-auto smallscroll">
                     <div className="grid grid-cols-[auto,1fr] gap-x-2 ">
-                        {items.map((item, idx) =>
-                            <Fragment key={idx}>
-                                <div className="px-1 text-right text-primary-400 border-r-primary-400 border-r">{idx}</div>
-                                <div>{item}</div>
-                            </Fragment>)
-                        }
+
+                        {items.map(
+                            (item, idx) => (
+                                <Fragment key={idx}>
+                                    <div className="px-1 text-right text-primary-400 border-r-primary-400 border-r">
+                                        {idx}
+                                    </div>
+
+                                    <div>
+                                        {item}
+                                    </div>
+                                </Fragment>
+                            )
+                        )}
+
                     </div>
                 </div>
             </div>
