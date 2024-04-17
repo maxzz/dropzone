8597aa58bba24108098639da3be057f200764e27
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-16 19:28:10 -0700

    WIP. file list. update CardTitleFilename

--- src/components/2-main/1-files-list/1-inline-card/1-title/4-filename/2-card-title-filename.tsx
+++ src/components/2-main/1-files-list/1-inline-card/1-title/4-filename/2-card-title-filename.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from "react";
+import { HTMLAttributes, useMemo } from "react";
 import { FileUs } from "@/store";
 import { UiTip, tipSmall } from "@ui/ui-tooltip";
 import { SymbolFolder } from "@ui/icons";
@@ -8,7 +8,7 @@ import { ManiFilenameParts } from "./1-mani-filename-parts";
 export function CardTitleFilename({ fileUs, className, ...rest }: { fileUs: FileUs; } & HTMLAttributes<HTMLDivElement>) {
     const stats = fileUs.stats;
 
-    const FilenameMemo = React.useMemo(() => {
+    const FilenameMemo = useMemo(() => {
         return (
             <UiTip
                 trigger={<ManiFilenameParts fname={fileUs.fname} />}
@@ -17,23 +17,34 @@ export function CardTitleFilename({ fileUs, className, ...rest }: { fileUs: File
                 {/* Popup content */}
                 <div className="p-1 text-xs grid grid-cols-[auto,1fr] gap-x-1 gap-y-1">
 
-                    {fileUs.fpath && <>
-                        <div className="font-bold">Sub-folder</div>
-                        <div>{fileUs.fpath}</div>
-                    </>}
+                    {fileUs.fpath && (
+                        <>
+                            <div className="font-bold">Sub-folder</div>
+                            <div>{fileUs.fpath}</div>
+                        </>
+                    )}
 
-                    <div className="font-bold">Filename</div>
-                    <div>{fileUs.fname}</div>
+                    <div className="font-bold">
+                        Filename
+                    </div>
 
-                    {stats.dateCreated && <>
-                        <div className="font-bold">Created</div>
-                        <div>{stats.dateCreated}</div>
-                    </>}
+                    <div>
+                        {fileUs.fname}
+                    </div>
 
-                    {stats.dateModified && <>
-                        <div className="font-bold">Modified</div>
-                        <div>{stats.dateModified}</div>
-                    </>}
+                    {stats.dateCreated && (
+                        <>
+                            <div className="font-bold">Created</div>
+                            <div>{stats.dateCreated}</div>
+                        </>
+                    )}
+
+                    {stats.dateModified && (
+                        <>
+                            <div className="font-bold">Modified</div>
+                            <div>{stats.dateModified}</div>
+                        </>
+                    )}
                 </div>
             </UiTip>
         );
@@ -42,7 +53,10 @@ export function CardTitleFilename({ fileUs, className, ...rest }: { fileUs: File
     return (
         <div className={classNames("text-sm font-light font-mono grid grid-cols-[minmax(0,min-content)_auto] items-center gap-x-1", className)} {...rest}>
             {FilenameMemo}
-            {stats.isSubFolder && <SymbolFolder className=" size-4 text-gray-500" title={`Folder: "${stats.subFolder}"`} />}
+
+            {stats.isSubFolder && (
+                <SymbolFolder className=" size-4 text-gray-500" title={`Folder: "${stats.subFolder}"`} />
+            )}
         </div>
     );
 }
