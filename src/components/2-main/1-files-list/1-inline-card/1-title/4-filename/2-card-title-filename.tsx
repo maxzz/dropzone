5cc98cc0562428d1f5d81735bb7610a16ee7d4a5
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-16 19:32:25 -0700

    WIP. file list. add PopupBody to CardTitleFilename

--- src/components/2-main/1-files-list/1-inline-card/1-title/4-filename/2-card-title-filename.tsx
+++ src/components/2-main/1-files-list/1-inline-card/1-title/4-filename/2-card-title-filename.tsx
@@ -5,54 +5,62 @@ import { SymbolFolder } from "@ui/icons";
 import { classNames } from "@/utils";
 import { ManiFilenameParts } from "./1-mani-filename-parts";
 
+function PopupBody({ fileUs }: { fileUs: FileUs; }) {
+    const stats = fileUs.stats;
+    return (
+        <div className="p-1 text-xs grid grid-cols-[auto,1fr] gap-x-1 gap-y-1">
+
+            {fileUs.fpath && (
+                <>
+                    <div className="font-bold">Sub-folder</div>
+                    <div>{fileUs.fpath}</div>
+                </>
+            )}
+
+            <div className="font-bold">
+                Filename
+            </div>
+
+            <div>
+                {fileUs.fname}
+            </div>
+
+            {stats.dateCreated && (
+                <>
+                    <div className="font-bold">Created</div>
+                    <div>{stats.dateCreated}</div>
+                </>
+            )}
+
+            {stats.dateModified && (
+                <>
+                    <div className="font-bold">Modified</div>
+                    <div>{stats.dateModified}</div>
+                </>
+            )}
+        </div>
+    );
+}
+
 export function CardTitleFilename({ fileUs, className, ...rest }: { fileUs: FileUs; } & HTMLAttributes<HTMLDivElement>) {
     const stats = fileUs.stats;
 
-    const FilenameMemo = useMemo(() => {
-        return (
-            <UiTip
-                trigger={<ManiFilenameParts fname={fileUs.fname} />}
-                {...tipSmall()}
-            >
-                {/* Popup content */}
-                <div className="p-1 text-xs grid grid-cols-[auto,1fr] gap-x-1 gap-y-1">
-
-                    {fileUs.fpath && (
-                        <>
-                            <div className="font-bold">Sub-folder</div>
-                            <div>{fileUs.fpath}</div>
-                        </>
-                    )}
-
-                    <div className="font-bold">
-                        Filename
-                    </div>
-
-                    <div>
-                        {fileUs.fname}
-                    </div>
-
-                    {stats.dateCreated && (
-                        <>
-                            <div className="font-bold">Created</div>
-                            <div>{stats.dateCreated}</div>
-                        </>
-                    )}
-
-                    {stats.dateModified && (
-                        <>
-                            <div className="font-bold">Modified</div>
-                            <div>{stats.dateModified}</div>
-                        </>
-                    )}
-                </div>
-            </UiTip>
-        );
-    }, [fileUs.fname, fileUs.stats.dateCreated, fileUs.stats.dateModified, fileUs.fpath, fileUs.fname]);
+    const FilenameTooltipMemo = useMemo(
+        () => {
+            return (
+                <UiTip
+                    trigger={<ManiFilenameParts fname={fileUs.fname} />}
+                    {...tipSmall()}
+                >
+                    <PopupBody fileUs={fileUs} />
+                </UiTip>
+            );
+        }, [fileUs.fname, fileUs.stats.dateCreated, fileUs.stats.dateModified, fileUs.fpath, fileUs.fname]
+    );
 
     return (
         <div className={classNames("text-sm font-light font-mono grid grid-cols-[minmax(0,min-content)_auto] items-center gap-x-1", className)} {...rest}>
-            {FilenameMemo}
+            {FilenameTooltipMemo}
 
             {stats.isSubFolder && (
                 <SymbolFolder className=" size-4 text-gray-500" title={`Folder: "${stats.subFolder}"`} />
