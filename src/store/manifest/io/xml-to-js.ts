b8850bf1a18cb0afe3ec6077f8b7897de3551dae
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-16 19:01:06 -0800

    WIP. update package dependencies. manifest v1.2.67. add prepare...() to export

--- src/store/manifest/io/xml-to-js.ts
+++ src/store/manifest/io/xml-to-js.ts
@@ -1,16 +1,18 @@
 import { type FileUs } from "@/store/store-types";
-import { makeXML, showError } from '..';
+import { convertJsToXml, type Mani, prepareNewMani4Xml, showError } from 'pm-manifest';
 //import { fileDownload } from '@/utils/file-download';
 
 export function convertToXml(fileUs: FileUs): { error: string; xml?: undefined; } | { xml: string; error?: undefined; } {
     if (!fileUs.fileCnt.raw) {
         return { error: 'empty file' };
     }
-    let xml = '';
+    
     try {
         // 1.
-        xml = makeXML(fileUs.parsedSrc.mani) || '';
+        const mani = prepareNewMani4Xml(fileUs.parsedSrc.mani as Mani.Manifest);
+        const xml = convertJsToXml(mani) || '';
         //console.log('%c---------new xml from converted---------', 'color: green', `\n${xml}`);
+
         // 2.
         //fileDownload({ data: xml, filename: fileUs.fname, mime: 'text/plain;charset=utf-8' });
         return { xml };
