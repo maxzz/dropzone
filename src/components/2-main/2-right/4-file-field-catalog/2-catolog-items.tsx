d791b91a95a2f85aad31fc91f86e705030b56270
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-14 19:14:59 -0700

    WIP. update package dependencies. react-19. 7 remove FieldIcon duplicates

--- src/components/2-main/2-right/4-file-field-catalog/2-catolog-items.tsx
+++ src/components/2-main/2-right/4-file-field-catalog/2-catolog-items.tsx
@@ -1,38 +1,29 @@
-import { type JSX, Fragment } from 'react';
+import { Fragment } from 'react';
 import { CatalogFile } from 'pm-manifest';
-import { fieldIcons } from '@/store/manifest/manifest-field-icons';
+import { FieldIcon } from '@/components/4-dialogs/dlg-fld-catalog';
 
 export function CatologItems({ names }: { names: CatalogFile.ItemInFile[]; }) {
     return (<>
-        {names.map((item, idx) => (
-            <Fragment key={idx}>
-                <div className="col-start-2 text-right">
-                    {idx + 1}
-                </div>
-
-                <div className="col-start-3 flex items-center gap-x-2 leading-[18px]">
-                    {FieldIcon(item.password, "size-4 opacity-25")}
-
-                    <div>
-                        {item.dispname}
+        {names.map(
+            (item, idx) => (
+                <Fragment key={idx}>
+                    <div className="col-start-2 text-right">
+                        {idx + 1}
                     </div>
-                </div>
 
-                <div className="col-start-4 font-mono text-[.6rem]">
-                    {item.dbname}
-                </div>
-            </Fragment>
-        ))}
+                    <div className="col-start-3 flex items-center gap-x-2 leading-[18px]">
+                        {FieldIcon(item.password, "size-4 opacity-25")}
+
+                        <div>
+                            {item.dispname}
+                        </div>
+                    </div>
+
+                    <div className="col-start-4 font-mono text-[.6rem]">
+                        {item.dbname}
+                    </div>
+                </Fragment>
+            ))
+        }
     </>);
-}
-
-function FieldIcon(isPsw: boolean | undefined, className: string) {
-    const type = isPsw ? 'psw' : 'edit';
-    const icon = fieldIcons[type]?.({ className, title: `Field type: ${type}`, });
-    const rv = icon || (
-        <div className="text-red-500">
-            nan
-        </div>
-    );
-    return rv;
 }
