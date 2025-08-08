b80ac4a6877d969a437446183f76c14630a9ac91
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-08 14:45:59 -0700

    WIP. load. doUpdateCacheAtom code review

--- src/store/atoms/9-ui-state/3-filters.ts
+++ src/store/atoms/9-ui-state/3-filters.ts
@@ -1,4 +1,6 @@
 import { atom } from "jotai";
+import { type FileUs } from "@/store/store-types";
+import { isAnyEmpty, isAnyManual } from "@/store/manifest";
 
 // Filters state
 
@@ -12,4 +14,26 @@ export const totalManiAtoms = {
     manualAtom: atom(0),
     normalAtom: atom(0),
     emptyAtom: atom(0),
+    fcAtom: atom(0),             // field catalogs
 };
+
+export function clearTotalManis(set: Setter) {
+    set(totalManiAtoms.normalAtom, 0);
+    set(totalManiAtoms.manualAtom, 0);
+    set(totalManiAtoms.emptyAtom, 0);
+    set(totalManiAtoms.fcAtom, 0);
+}
+
+export function addToTotalManis(fileUs: FileUs, set: Setter) {
+    const { fcat, meta } = fileUs.parsedSrc;
+
+    if (fcat) {
+        set(totalManiAtoms.fcAtom, v => ++v);
+    } else if (isAnyEmpty(meta)) {
+        set(totalManiAtoms.emptyAtom, v => ++v);
+    } else if (isAnyManual(meta)) {
+        set(totalManiAtoms.manualAtom, v => ++v);
+    } else {
+        set(totalManiAtoms.normalAtom, v => ++v);
+    }
+}
